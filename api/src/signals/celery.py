3e450dc9dc08ed78558b9bd5e1be10b5aa09de82
Niels van Dijk <niels@differentstep.com>
2018-07-31 15:57:12 +0200

    Added first celery tasks (empty).

--- api/src/signals/celery.py
+++ api/src/signals/celery.py
@@ -5,7 +5,7 @@ from celery import Celery
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'signals.settings')
 
-app = Celery('proj')
+app = Celery('signals')
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
@@ -15,8 +15,3 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
-
-
-@app.task(bind=True)
-def debug_task(self):
-    print('Request: {0!r}'.format(self.request))
--- api/src/signals/django_signals.py
+++ api/src/signals/django_signals.py
@@ -1,9 +1,12 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+from signals import tasks
 from signals.models import Signal
 
 
 @receiver(post_save, sender=Signal)
-def create_signal(sender, instance, created, **kwargs):
-    print("HERE")
+def post_save_signal(sender, instance, created, **kwargs):
+    if created:
+        tasks.push_to_sigmax.delay(id=instance.id)
+    tasks.email_to_apptimize.delay(id=instance.id)
