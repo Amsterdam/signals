55fd9731c07d69704b05fbcfde2e4df03fe17ca3
Thijs Coenen <t.coenen@amsterdam.nl>
2018-08-14 17:50:46 +0200

    SIG-417 (SIG-438) filters in eigen module

--- api/app/signals/apps/signals/views.py
+++ api/app/signals/apps/signals/views.py
@@ -3,26 +3,20 @@ import re
 
 from datapunt_api import bbox
 from datapunt_api.rest import DatapuntViewSetWritable
-from django.contrib.gis.geos import Polygon
 from django.http import JsonResponse
 from django_filters.rest_framework import DjangoFilterBackend
-from django_filters.rest_framework import FilterSet
-from django_filters.rest_framework import filters
 from rest_framework import viewsets, mixins
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
-from rest_framework.serializers import ValidationError
 from rest_framework.settings import api_settings
 from rest_framework.status import HTTP_202_ACCEPTED
 from rest_framework.views import APIView
 
 from signals import settings
 from signals.auth.backend import JWTAuthBackend
-from signals.messaging.categories import ALL_SUB_CATEGORIES
-from signals.apps.signals.models import Buurt
 from signals.apps.signals.models import Category
 from signals.apps.signals.models import Location
-from signals.apps.signals.models import Signal, STATUS_OPTIONS
+from signals.apps.signals.models import Signal
 from signals.apps.signals.models import Status
 from signals.apps.signals.permissions import (
     StatusPermission, CategoryPermission, LocationPermission)
@@ -35,119 +29,10 @@ from signals.apps.signals.serializers import (
 from signals.apps.signals.serializers import StatusSerializer
 from signals.throttling import NoUserRateThrottle
 
+from signals.apps.signals.filters import (
+    SignalFilter, StatusFilter, LocationFilter, STADSDELEN)
+
 LOGGER = logging.getLogger()
-
-STADSDELEN = (
-    ("B", "Westpoort (B)"),
-    ("M", "Oost (M)"),
-    ("N", "Noord (N)"),
-    ("A", "Centrum (A)"),
-    ("E", "West (E)"),
-    ("F", "Nieuw-West (F)"),
-    ("K", "Zuid (K)"),
-    ("T", "Zuidoost (T)"),
-)
-
-
-def buurt_choices():
-    options = Buurt.objects.values_list('vollcode', 'naam')
-    return [(c, f'{n} ({c})') for c, n in options]
-
-
-def status_choices():
-    return [(c, f'{n} ({c})') for c, n in STATUS_OPTIONS]
-
-
-def category_sub_choices():
-    # fixme: use the distinct query again but also supply the list to frontend
-    # options = Category.objects.values_list("sub").distinct()
-    return [(subcat, f'{subcat}') for _, _, subcat, _, _ in ALL_SUB_CATEGORIES]
-
-
-class SignalFilter(FilterSet):
-    id = filters.CharFilter()
-    in_bbox = filters.CharFilter(method='in_bbox_filter', label='bbox')
-    geo = filters.CharFilter(method="locatie_filter", label='x,y,r')
-
-    location__stadsdeel = filters.MultipleChoiceFilter(choices=STADSDELEN)
-    location__buurt_code = filters.MultipleChoiceFilter(choices=buurt_choices)
-    location__address_text = filters.CharFilter(lookup_expr='icontains')
-
-    extra = filters.CharFilter(method='in_extra', label='extra')
-
-    created_at = filters.DateFilter(field_name='created_at', lookup_expr='date')
-    created_at__gte = filters.DateFilter(field_name='created_at',
-                                         lookup_expr='date__gte')
-    created_at__lte = filters.DateFilter(field_name='created_at',
-                                         lookup_expr='date__lte')
-
-    updated_at = filters.DateFilter(field_name='updated_at', lookup_expr='date')
-    updated_at__gte = filters.DateFilter(field_name='updated_at',
-                                         lookup_expr='date__gte')
-    updated_at__lte = filters.DateFilter(field_name='updated_at',
-                                         lookup_expr='date__lte')
-
-    incident_date_start = filters.DateFilter(field_name='incident_date_start',
-                                             lookup_expr='date')
-    incident_date_start__gte = filters.DateFilter(field_name='incident_date_start',
-                                                  lookup_expr='date__gte')
-    incident_date_start__lte = filters.DateFilter(field_name='incident_date_start',
-                                                  lookup_expr='date__lte')
-
-    incident_date_end = filters.DateFilter(field_name='incident_date_end',
-                                           lookup_expr='date')
-
-    operational_date = filters.DateFilter(field_name='operational_date',
-                                          lookup_expr='date')
-    expire_date = filters.DateFilter(field_name='expire_date', lookup_expr='date')
-    expire_date__gte = filters.DateFilter(field_name='expire_date',
-                                          lookup_expr='date__gte')
-    expire_date__lte = filters.DateFilter(field_name='expire_date',
-                                          lookup_expr='date__lte')
-
-    status__state = filters.MultipleChoiceFilter(choices=status_choices)
-    category__sub = filters.MultipleChoiceFilter(choices=category_sub_choices)
-
-    class Meta(object):
-        model = Signal
-        fields = (
-            "id",
-            "signal_id",
-            "status__state",
-            "category__main",
-            "category__sub",
-            "updated_at",
-            "location__buurt_code",
-            "location__stadsdeel",
-            "location__address_text",
-            "reporter__email",
-            "in_bbox",
-            "extra",
-            "geo",
-        )
-
-    def in_bbox_filter(self, qs, name, value):
-        bbox_values, err = bbox.valid_bbox(value)
-        lat1, lon1, lat2, lon2 = bbox_values
-        poly_bbox = Polygon.from_bbox((lon1, lat1, lon2, lat2))
-
-        if err:
-            raise ValidationError(
-                f"bbox invalid {err}:{bbox_values}")
-        return qs.filter(
-            location__geometrie__bboverlaps=poly_bbox)
-
-    def in_extra(self, qs, name, value):
-        """
-        Filter in extra json field
-        """
-        # TODO
-        return qs
-
-    def locatie_filter(self, qs, name, value):
-        point, radius = bbox.parse_xyr(value)
-        return qs.filter(
-            location__geometrie__dwithin=(point, radius))
 
 
 class AuthViewSet:
@@ -250,80 +135,6 @@ class SignalAuthView(AuthViewSet, DatapuntViewSetWritable):
     serializer_class = SignalAuthSerializer
     filter_backends = (DjangoFilterBackend,)
     filter_class = SignalFilter
-
-
-class StatusFilter(FilterSet):
-    id = filters.CharFilter()
-    in_bbox = filters.CharFilter(method='in_bbox_filter', label='bbox')
-    location = filters.CharFilter(method="locatie_filter", label='x,y,r')
-
-    stadsdeel = filters.ChoiceFilter(choices=STADSDELEN)
-    buurt_code = filters.ChoiceFilter(choices=buurt_choices)
-
-    class Meta(object):
-        model = Status
-        fields = (
-            "id",
-            "_signal__id",
-            "buurt_code",
-            "signal__location__stadsdeel",
-            "signal__location__buurt_code",
-            "created_at",
-            # "operational_date",
-            "in_bbox",
-            "location",
-        )
-
-    def locatie_filter(self, qs, name, value):
-        point, radius = bbox.parse_xyr(value)
-        return qs.filter(signal__location__geometrie__dwithin=(point, radius))
-
-    def in_bbox_filter(self, qs, name, value):
-        bbox_values, err = bbox.valid_bbox(value)
-        lat1, lon1, lat2, lon2 = bbox_values
-        poly_bbox = Polygon.from_bbox((lon1, lat1, lon2, lat2))
-
-        if err:
-            raise ValidationError(f"bbox invalid {err}:{bbox_values}")
-        return qs.filter(signal__location__geometrie__bboverlaps=poly_bbox)
-
-
-class LocationFilter(FilterSet):
-    """
-    """
-    id = filters.CharFilter()
-    in_bbox = filters.CharFilter(method='in_bbox_filter', label='bbox')
-    location = filters.CharFilter(
-        method="locatie_filter", label='x,y,r')
-
-    stadsdeel = filters.ChoiceFilter(choices=STADSDELEN)
-    buurt_code = filters.ChoiceFilter(choices=buurt_choices)
-
-    class Meta(object):
-        model = Location
-        fields = (
-            "id",
-            "_signal__id",
-            "buurt_code",
-            "signal__location__stadsdeel",
-            "signal__location__buurt_code",
-            "created_at",
-            "in_bbox",
-            "location",
-        )
-
-    def locatie_filter(self, qs, name, value):
-        point, radius = bbox.parse_xyr(value)
-        return qs.filter(geometrie__dwithin=(point, radius))
-
-    def in_bbox_filter(self, qs, name, value):
-        bbox_values, err = bbox.valid_bbox(value)
-        lat1, lon1, lat2, lon2 = bbox_values
-        poly_bbox = Polygon.from_bbox((lon1, lat1, lon2, lat2))
-
-        if err:
-            raise ValidationError(f"bbox invalid {err}:{bbox_values}")
-        return qs.filter(geometrie__bboverlaps=poly_bbox)
 
 
 class LocationAuthView(AuthViewSet, DatapuntViewSetWritable):
