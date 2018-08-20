282a317fb1f779d8ef89d6504e1ff636fb0c3b56
Thijs Coenen <t.coenen@amsterdam.nl>
2018-08-20 11:52:12 +0200

    SIG-442: tests for Location serializer

--- api/app/tests/test_coordinates.py
+++ api/app/tests/test_coordinates.py
@@ -6,61 +6,56 @@ from django.test import TestCase
 from rest_framework.test import APITestCase
 from django.core.exceptions import ValidationError
 
-
 from signals.apps.signals.serializers import NearAmsterdamValidatorMixin
 from django.contrib.gis.geos import Point
 
+IN_AMSTERDAM = (4.898466, 52.361585)
+OUTSIDE_AMSTERDAM = tuple(reversed(IN_AMSTERDAM))
 
-class NearAmsterdamValidatorMixin(TestCase):
+
+class TestNearAmsterdamValidatorMixin(TestCase):
     def test_validate_geometrie(self):
         # note this test bypasses the API
-        in_amsterdam = (4.898466, 52.361585)
-
-        correct = Point(in_amsterdam)
-        wrong = Point(tuple(reversed(in_amsterdam)))
+        correct = Point(IN_AMSTERDAM)
+        wrong = Point(OUTSIDE_AMSTERDAM)
 
         # check that valid data is returned, and wrpng data rejected
-        validator = NearAmsterdamValidatorMixin()
-        self.assertEquals(correct, validator.validate_geometrie(correct))
+        v = NearAmsterdamValidatorMixin()
+        self.assertEquals(correct, v.validate_geometrie(correct))
 
         with self.assertRaises(ValidationError):
-            validator.validate_geometrie(wrong)
+            v.validate_geometrie(wrong)
 
 
 class TestLocationSerializer(APITestCase):
-    fixture_files = {
-        "post_signal": "signal_post.json",
-        "post_status": "status_auth_post.json",
-        "post_category": "category_auth_post.json",
-        "post_location": "location_auth_post.json",
-    }
-
-    def _get_fixture(self, name):
-
-        filename = self.fixture_files[name]
+    def _get_fixture(self):
         path = os.path.join(
             settings.BASE_DIR,
             'signals',
             'apps',
             'signals',
             'fixtures',
-            filename
+            'signal_post.json'
         )
 
         with open(path) as fixture_file:
             postjson = json.loads(fixture_file.read())
 
         return postjson
+   
+    def test_swapped_lon_lat(self):
+        """Post een compleet signaal."""
+        url = '/signals/signal/'
+        payload = self._get_fixture()
+        payload['location']['geometrie']['coordinates'] = OUTSIDE_AMSTERDAM
 
-    def test_wrong_side_of_planet(self):
-        pass
-#        url = '/signals/auth/location/'
-#        
-#        payload = self._get_fixture('post_signal')['location']
-#        payload['geometrie']['coordinates'] = (4.898466, 52.361585)
-#
-#        response = self.client.post(url, payload, format='json')
-#        self.assertEquals(response.status_code, 201)
-    
+        response = self.client.post(url, payload, format='json')
+        self.assertEqual(response.status_code, 400)
 
+    def test_correct_lon_lat(self):
+        url = '/signals/signal/'
+        payload = self._get_fixture()
+        payload['location']['geometrie']['coordinates'] = IN_AMSTERDAM
 
+        response = self.client.post(url, payload, format='json')
+        self.assertEqual(response.status_code, 201)
