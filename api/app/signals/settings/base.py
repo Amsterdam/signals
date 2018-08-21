1324409a98bcf0b4a85bfb0c2f5fa18f705b62bc
Niels van Dijk <niels@differentstep.com>
2018-08-21 15:43:46 +0200

    SIG-411 Hard environment get on SECRET_KEY value.

--- api/app/signals/settings/base.py
+++ api/app/signals/settings/base.py
@@ -15,7 +15,7 @@ from signals.settings.settings_databases import (
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Django settings
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.environ['SECRET_KEY']
 DEBUG = False
 ALLOWED_HOSTS = [
     'api.data.amsterdam.nl',
