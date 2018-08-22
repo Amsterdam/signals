2184296f27dbcbec89ee0f0cac15630c9089b22e
Niels van Dijk <niels@differentstep.com>
2018-08-22 10:06:15 +0200

    SIG-411 Temporary fix for current settings structure.

---
+++ api/app/signals/settings/__init__.py
@@ -0,0 +1,3 @@
+# Temporary fix to make the settings restructure work with current deployment settings. Can be
+# removed after the Ansible playbooks for deployment are updated with the production settings.
+from signals.settings.base import *  # noqa
