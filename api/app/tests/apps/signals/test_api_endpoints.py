9b041a58bf3ece891a50ebf2a7fdf6f6ae3663ee
Niels van Dijk <niels@differentstep.com>
2018-08-29 14:48:55 +0200

    SIG-418 Updated api tests.

--- api/app/tests/apps/signals/test_api_endpoints.py
+++ api/app/tests/apps/signals/test_api_endpoints.py
@@ -3,7 +3,6 @@ import os
 
 from django.conf import settings
 from django.core.files.uploadedfile import SimpleUploadedFile
-from rest_framework.status import HTTP_200_OK
 from rest_framework.test import APITestCase
 
 from signals.apps.signals.models import (
@@ -19,12 +18,12 @@ from tests.apps.signals import factories
 from tests.apps.users.factories import SuperUserFacotry
 
 
-class BrowseDatasetsTestCase(APITestCase):
-    datasets = [
-        "signals/auth/signal",
-        "signals/auth/status",
-        "signals/auth/category",
-        "signals/auth/location",
+class TestAPIEndpoints(APITestCase):
+    endpoints = [
+        '/signals/auth/signal/',
+        '/signals/auth/status/',
+        '/signals/auth/category/',
+        '/signals/auth/location/',
     ]
 
     def setUp(self):
@@ -41,89 +40,47 @@ class BrowseDatasetsTestCase(APITestCase):
         self.s.reporter = self.reporter
         self.s.save()
 
-    def valid_html_response(self, url, response):
-        """
-        Helper method to check common status/json
-        """
+    def test_signals_index(self):
+        response = self.client.get('/signals/')
 
-        self.assertEqual(
-            200, response.status_code, "Wrong response code for {}".format(url)
-        )
+        self.assertEqual(response.status_code, 200)
 
-        self.assertEqual(
-            "text/html; charset=utf-8",
-            response["Content-Type"],
-            "Wrong Content-Type for {}".format(url),
-        )
-
-    def test_index_pages(self):
-        url = "signals"
-
-        response = self.client.get("/{}/".format(url))
-
-        self.assertEqual(
-            response.status_code, 200, "Wrong response code for {}".format(url)
-        )
-
-    def test_lists(self):
+    def test_auth_lists(self):
         # Forcing authentication
         superuser = SuperUserFacotry.create()
         self.client.force_authenticate(user=superuser)
 
-        for url in self.datasets:
-            response = self.client.get("/{}/".format(url))
+        for url in self.endpoints:
+            response = self.client.get(url)
 
-            self.assertEqual(
-                response.status_code, 200,
-                "Wrong response code for {}".format(url)
-            )
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response['Content-Type'], 'application/json')
+            self.assertIn('count', response.data)
 
-            self.assertEqual(
-                response["Content-Type"],
-                "application/json",
-                "Wrong Content-Type for {}".format(url),
-            )
-
-            self.assertIn(
-                "count", response.data, "No count attribute in {}".format(url)
-            )
-
-    def test_lists_html(self):
+    def test_auth_lists_html(self):
         # Forcing authentication
         superuser = SuperUserFacotry.create()
         self.client.force_authenticate(user=superuser)
 
-        for url in self.datasets:
-            response = self.client.get("/{}/?format=api".format(url))
+        for url in self.endpoints:
+            response = self.client.get('{}?format=api'.format(url))
 
-            self.valid_html_response(url, response)
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response['Content-Type'], 'text/html; charset=utf-8')
+            self.assertIn('count', response.data)
 
-            self.assertIn(
-                "count", response.data, "No count attribute in {}".format(url)
-            )
+    def test_signal_detail(self):
+        signal = Signal.objects.all().first()
+        response = self.client.get(f'/signals/signal/{signal.signal_id}/')
 
-    def test_signal_unauthenticated(self):
-        some_signal = Signal.objects.all().first()
-
-        response = self.client.get(f"/signals/signal/{some_signal.signal_id}/")
-        self.assertEqual(response.status_code, HTTP_200_OK,
-                         "Probelem retrieving signal status")
-        self.assertEqual(response.data.get('signal_id'),
-                         str(some_signal.signal_id),
-                         "Signaal komt niet overeen")
-        self.assertEqual(response.data.get('status').get('id'),
-                         some_signal.status.id, "Status id komt niet overeen")
-        self.assertEqual(response.data.get('status').get('state'),
-                         str(some_signal.status.state),
-                         "Signaal status komt niet overeen")
-        self.assertEqual(response.data.get('text'), None,
-                         "Signaal lekt informatie naar publiek endpoint")
-        self.assertEqual(response.data.get('category'), None,
-                         "Signaal lekt informatie naar publiek endpoint")
-        self.assertEqual(response.data.get('location'), None,
-                         "Signaal lekt informatie naar publiek endpoint")
-        self.assertEqual(response.data.get('id'), None,
-                         "Signaal lekt informatie naar publiek endpoint")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data.get('signal_id'), str(signal.signal_id))
+        self.assertEqual(response.data.get('status').get('id'), signal.status.id)
+        self.assertEqual(response.data.get('status').get('state'), str(signal.status.state))
+        self.assertEqual(response.data.get('text'), None)
+        self.assertEqual(response.data.get('category'), None)
+        self.assertEqual(response.data.get('location'), None)
+        self.assertEqual(response.data.get('id'), None)
 
 
 class TestEnpointsBase(APITestCase):
