d37b0c8ae2d9441601f07271e9638101437b244e
Niels van Dijk <niels@differentstep.com>
2018-08-30 09:51:31 +0200

    SIG-418 isort.

--- api/app/signals/apps/signals/urls.py
+++ api/app/signals/apps/signals/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import include, path
 from rest_framework import routers
 
 from signals.apps.signals import views as api_views
--- api/app/signals/urls.py
+++ api/app/signals/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
