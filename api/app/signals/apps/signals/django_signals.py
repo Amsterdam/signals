158cb877ae4f6995b66ec7a5a0bff81cd0b273d2
Niels van Dijk <niels@differentstep.com>
2018-09-03 17:29:30 +0200

    SIG-417 Added tests for Django signals handlers.

--- api/app/signals/apps/signals/django_signals.py
+++ api/app/signals/apps/signals/django_signals.py
@@ -1,9 +1,8 @@
-from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from signals.apps.signals import tasks
 from signals.apps.signals.models import (
-    Signal, Status, create_initial, update_location,
+    Status, create_initial, update_location,
     update_status, update_category, update_reporter)
 from signals.messaging.send_emails import handle_create_signal, handle_status_change
 
@@ -11,32 +10,28 @@ from signals.messaging.send_emails import handle_create_signal, handle_status_ch
 @receiver(create_initial, dispatch_uid='create_initial')
 def create_initial_handler(sender, signal_obj, **kwargs):
     handle_create_signal(signal_obj)
+    tasks.push_to_sigmax.delay(pk=signal_obj.id)
+    tasks.send_mail_flex_horeca.delay(pk=signal_obj.id)
+    tasks.send_mail_apptimize.delay(pk=signal_obj.id)
 
 
 @receiver(update_location, dispatch_uid='update_location')
 def update_location_handler(sender, location, **kwargs):
-    pass
+    tasks.send_mail_apptimize.delay(pk=location.signal.id)
 
 
 @receiver(update_status, dispatch_uid='update_status')
 def update_status_handler(sender, status, **kwargs):
     previous_status = Status.objects.exclude(id=status.id).last()
     handle_status_change(status, previous_status)
+    tasks.send_mail_apptimize.delay(pk=status.signal.id)
 
 
 @receiver(update_category, dispatch_uid='update_category')
 def update_category_handler(sender, category, **kwargs):
-    pass
+    tasks.send_mail_apptimize.delay(pk=category.signal.id)
 
 
 @receiver(update_reporter, dispatch_uid='update_reporter')
 def update_reporter_handler(sender, reporter, **kwargs):
-    pass
-
-
-@receiver(post_save, sender=Signal, dispatch_uid='post_save_signal')
-def post_save_signal(sender, instance, created, **kwargs):
-    if created:
-        tasks.push_to_sigmax.delay(pk=instance.id)
-        tasks.send_mail_flex_horeca.delay(pk=instance.id)
-    tasks.send_mail_apptimize.delay(pk=instance.id)
+    tasks.send_mail_apptimize.delay(pk=reporter.signal.id)
--- api/app/tests/apps/signals/test_django_signals.py
+++ api/app/tests/apps/signals/test_django_signals.py
@@ -2,33 +2,55 @@ from unittest import mock
 
 from django.test import testcases
 
-from tests.apps.signals.factories import SignalFactory
+from signals.apps.signals.models import create_initial, update_location, update_status, \
+    update_category, update_reporter
+from tests.apps.signals.factories import SignalFactory, StatusFactory
 
 
 class TestDjangoSignals(testcases.TestCase):
 
+    @mock.patch('signals.apps.signals.django_signals.handle_create_signal')
     @mock.patch('signals.apps.signals.django_signals.tasks')
-    def test_post_save_signal_created(self, mocked_tasks):
-        signal = SignalFactory.build()
-        signal.location.save()
-        signal.status.save()
-        signal.reporter.save()
-        signal.category.save()
-        signal.save()  # Triggers Django `post_save` signal.
-
-        mocked_tasks.push_to_sigmax.delay.assert_called_once_with(pk=signal.id)
-        mocked_tasks.send_mail_flex_horeca.delay.assert_called_once_with(
-            pk=signal.id)
-        mocked_tasks.send_mail_apptimize.delay.assert_called_once_with(
-            pk=signal.id)
-
-    @mock.patch('signals.apps.signals.django_signals.tasks')
-    def test_post_save_signal_updated(self, mocked_tasks):
+    def test_create_initial_handler(self, mocked_tasks, mocked_handle_create_signal):
         signal = SignalFactory.create()
+        create_initial.send(sender=self.__class__, signal_obj=signal)
 
-        signal.text = 'Signal is updated'
+        mocked_handle_create_signal.assert_called_once_with(signal)
+        mocked_tasks.push_to_sigmax.delay.assert_called_once_with(pk=signal.id)
+        mocked_tasks.send_mail_flex_horeca.delay.assert_called_once_with(pk=signal.id)
+        mocked_tasks.send_mail_apptimize.delay.assert_called_once_with(pk=signal.id)
+
+    @mock.patch('signals.apps.signals.django_signals.tasks')
+    def test_update_location_handler(self, mocked_tasks):
+        signal = SignalFactory.create()
+        update_location.send(sender=self.__class__, location=signal.location)
+
+        mocked_tasks.send_mail_apptimize.delay.assert_called_once_with(pk=signal.id)
+
+    @mock.patch('signals.apps.signals.django_signals.handle_status_change')
+    @mock.patch('signals.apps.signals.django_signals.tasks')
+    def test_update_status_handler(self, mocked_tasks, mocked_handle_status_change):
+        signal = SignalFactory.create()
+        previous_status = signal.status
+        new_status = StatusFactory.create(_signal=signal)
+        signal.status = new_status
         signal.save()
-        mocked_tasks.push_to_sigmax.delay.assert_not_called()
-        mocked_tasks.send_mail_flex_horeca.delay.assert_not_called()
-        mocked_tasks.send_mail_apptimize.delay.assert_called_once_with(
-            pk=signal.id)
+
+        update_status.send(sender=self.__class__, status=new_status)
+
+        mocked_handle_status_change.assert_called_once_with(new_status, previous_status)
+        mocked_tasks.send_mail_apptimize.delay.assert_called_once_with(pk=signal.id)
+
+    @mock.patch('signals.apps.signals.django_signals.tasks')
+    def test_update_category_handler(self, mocked_tasks):
+        signal = SignalFactory.create()
+        update_category.send(sender=self.__class__, category=signal.category)
+
+        mocked_tasks.send_mail_apptimize.delay.assert_called_once_with(pk=signal.id)
+
+    @mock.patch('signals.apps.signals.django_signals.tasks')
+    def test_update_reporter_handler(self, mocked_tasks):
+        signal = SignalFactory.create()
+        update_reporter.send(sender=self.__class__, reporter=signal.reporter)
+
+        mocked_tasks.send_mail_apptimize.delay.assert_called_once_with(pk=signal.id)
