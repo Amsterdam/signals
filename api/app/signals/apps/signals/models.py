ea97a2881bfd021578ccf20c19e161332fa37a68
Niels van Dijk <niels@differentstep.com>
2018-09-10 14:13:14 +0200

    SIG-594 Added Priority model with data/schema migration.

--- api/app/signals/apps/signals/models.py
+++ api/app/signals/apps/signals/models.py
@@ -413,3 +413,26 @@ class Status(CreatedUpdatedModel):
 
     def __str__(self):
         return str(self.text)
+
+
+class Priority(CreatedUpdatedModel):
+    PRIORITY_NORMAL = 'normal'
+    PRIORITY_HIGH = 'high'
+    PRIORITY_CHOICES = (
+        (PRIORITY_NORMAL, 'Normal'),
+        (PRIORITY_HIGH, 'High'),
+    )
+
+    _signal = models.ForeignKey('signals.Signal',
+                                related_name='priorities',
+                                null=False,
+                                on_delete=models.CASCADE)
+    priority = models.CharField(max_length=10, choices=PRIORITY_CHOICES, default=PRIORITY_NORMAL)
+    created_by = models.EmailField(null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = 'Priorities'
+
+    def __str__(self):
+        """String representation."""
+        return self.get_priority_display()
