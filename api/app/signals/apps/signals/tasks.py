2375ec7de077176b294e2425970ba53df0ae32c2
Thijs Coenen <t.coenen@amsterdam.nl>
2018-09-12 10:22:10 +0200

    SIG-590 small clean-up

--- api/app/signals/apps/signals/tasks.py
+++ api/app/signals/apps/signals/tasks.py
@@ -8,15 +8,6 @@ from signals.utils.datawarehouse import save_csv_files_datawarehouse
 logger = logging.getLogger(__name__)
 
 
-def retrieve_signal(pk: int) -> Optional[Signal]:
-    try:
-        signal = Signal.objects.get(id=pk)
-    except Signal.DoesNotExist as e:
-        logger.exception(str(e))
-        return None
-    return signal
-
-
 @app.task
 def task_save_csv_files_datawarehouse():
     """Celery task to save CSV files for Datawarehouse.
