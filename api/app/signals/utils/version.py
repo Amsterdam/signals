de43502c71356c3b61f9cc93d6ca1b8b2965e478
Niels van Dijk <niels@differentstep.com>
2018-09-19 15:10:48 +0200

    Fixed get_version util function to work without given version argument as well.

--- api/app/signals/utils/version.py
+++ api/app/signals/utils/version.py
@@ -1,8 +1,11 @@
-def get_version(version: tuple) -> str:
+def get_version(version: tuple = None) -> str:
     """Return version string (x.y[.z]) from given version tuple.
 
     :param version: version tuple
     :returns: version string
     """
+    if version is None:
+        from signals import VERSION as version
+
     parts = 2 if version[2] == 0 else 3
     return '.'.join(str(v) for v in version[:parts])
