f390931c3bad6bd277704b3ce158c37357cc324f
Niels van Dijk <niels@differentstep.com>
2018-09-20 10:31:41 +0200

    Bumped version to 0.1.2.

--- api/app/signals/__init__.py
+++ api/app/signals/__init__.py
@@ -5,6 +5,6 @@ from signals.utils.version import get_version
 
 __all__ = ['celery_app', ]
 
-VERSION = (0, 1, 1)  # Major, minor, patch
+VERSION = (0, 1, 2)  # Major, minor, patch
 
 __version__ = get_version(VERSION)
