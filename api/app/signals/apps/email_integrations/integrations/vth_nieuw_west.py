b0765297a56c542efacc4ff1dd630c7a26615f93
Niels van Dijk <niels@differentstep.com>
2018-09-20 11:04:59 +0200

    SIG-619 Fixed email integration vth nieuw west with tests.

--- api/app/signals/apps/email_integrations/integrations/vth_nieuw_west.py
+++ api/app/signals/apps/email_integrations/integrations/vth_nieuw_west.py
@@ -3,13 +3,14 @@ E-mail integration for VTH Nieuw West.
 """
 from django.conf import settings
 from django.core.mail import send_mail as django_send_mail
+from django.db.models import Q
 from django.utils import timezone
 
 from signals.apps.email_integrations.utils import (
     create_default_notification_message,
     is_business_hour
 )
-from signals.apps.signals.models import STADSDEEL_NIEUWWEST, Signal
+from signals.apps.signals.models import STADSDEEL_NIEUWWEST, Signal, SubCategory
 
 
 def send_mail(signal: Signal) -> int:
@@ -47,16 +48,13 @@ def is_signal_applicable(signal: Signal) -> bool:
     if signal.location.stadsdeel != STADSDEEL_NIEUWWEST:
         return False
 
-    eligible_main_category = 'Overlast Bedrijven en Horeca'
-    eligible_sub_categories = (
-        'Geluidsoverlast muziek',
-        'Geluidsoverlast installaties',
-        'Overlast terrassen',
-        'Stankoverlast',
-        'Overlast door bezoekers (niet op terras)',
-    )
-    is_applicable = (
-        signal.category.main == eligible_main_category and
-        signal.category.sub in eligible_sub_categories)
+    # TODO: move this query to object manager.
+    eligible_sub_categories = SubCategory.objects.filter(
+        Q(main_category__name='Overlast Bedrijven en Horeca') & (
+            Q(name='Geluidsoverlast muziek') |
+            Q(name='Geluidsoverlast installaties') |
+            Q(name='Overlast terrassen') |
+            Q(name='Stankoverlast') |
+            Q(name='Overlast door bezoekers (niet op terras)')))
 
-    return is_applicable
+    return signal.category_assignment.sub_category in eligible_sub_categories
