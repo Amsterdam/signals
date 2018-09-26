d830a68f78aa0803575e688148fa676003ff1986
Niels van Dijk <niels@differentstep.com>
2018-09-26 15:22:17 +0200

    SIG-612 Added test for sub category detail endpoint.

--- api/app/tests/apps/signals/test_api_endpoints.py
+++ api/app/tests/apps/signals/test_api_endpoints.py
@@ -16,7 +16,7 @@ from signals.apps.signals.models import (
     Status,
     MainCategory)
 from tests.apps.signals import factories
-from tests.apps.signals.factories import MainCategoryFactory
+from tests.apps.signals.factories import MainCategoryFactory, SubCategoryFactory
 from tests.apps.users.factories import SuperUserFactory, UserFactory
 
 
@@ -437,3 +437,15 @@ class TestCategoryTermsEndpoints(APITestCase):
         data = response.json()
         self.assertEqual(data['name'], 'Afval')
         self.assertEqual(len(data['sub_categories']), 13)
+
+    def test_sub_category_detail(self):
+        sub_category = SubCategoryFactory.create(name='Grofvuil', main_category__name='Afval')
+
+        url = '/signals/v1/public/terms/categories/{slug}/{sub_slug}'.format(
+            slug=sub_category.main_category.slug,
+            sub_slug=sub_category.slug)
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+        data = response.json()
+        self.assertEqual(data['name'], 'Grofvuil')
