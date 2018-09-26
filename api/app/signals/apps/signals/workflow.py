9bcf613a3ddeeee30782a9d269d4cefe054dbf8e
Thijs Coenen <t.coenen@amsterdam.nl>
2018-09-26 16:00:36 +0200

    SIG-661 update state machine with new state transitions

--- api/app/signals/apps/signals/workflow.py
+++ api/app/signals/apps/signals/workflow.py
@@ -27,10 +27,14 @@ AFGEHANDELD_EXTERN = 'A'
 
 STATUS_OPTIONS_EXTERNAL = (
     (TE_VERZENDEN, 'Te verzenden naar extern systeem.'),
-    (VERZONDEN, 'Verzonden naar extern systeem.')
+    (VERZONDEN, 'Verzonden naar extern systeem.'),
     (VERZENDEN_MISLUKT, 'Verzending naar extern systeem mislukt'),
     (AFGEHANDELD_EXTERN, 'Melding is afgehandeld in extern systeem.'),
 )
+
+# The state machine is used to check POSTs in the status serializer, hence below we
+# leave out certain transitions that are possible --- but, not through the API.
+# E.g. TE_VERZENDEN -> VERZENDEN_MISLUKT is only possible at the system level.
 
 STATUS_OVERGANGEN = {
     LEEG: [GEMELD],  # Een nieuw melding mag alleen aangemaakt worden met gemeld
@@ -41,6 +45,7 @@ STATUS_OVERGANGEN = {
         GEANNULEERD,
         GEMELD,
         ON_HOLD,
+        TE_VERZENDEN,
     ],
     AFWACHTING: [
         AFGEHANDELD,
@@ -48,12 +53,14 @@ STATUS_OVERGANGEN = {
         BEHANDELING,
         GEANNULEERD,
         ON_HOLD,
+        TE_VERZENDEN,
     ],
     BEHANDELING: [
         AFGEHANDELD,
         BEHANDELING,
         GEANNULEERD,
         ON_HOLD,
+        TE_VERZENDEN,
     ],
     ON_HOLD: [
         AFGEHANDELD,
@@ -61,7 +68,18 @@ STATUS_OVERGANGEN = {
         BEHANDELING,
         GEANNULEERD,
         GEMELD,
+        TE_VERZENDEN,
     ],
     AFGEHANDELD: [],
     GEANNULEERD: [],
+    TE_VERZENDEN: [
+        VERZONDEN,  # Do not allow transition to VERZENDEN_MISLUKT (see comment above)
+    ],
+    VERZONDEN: [
+        AFGEHANDELD_EXTERN,
+    ],
+    AFGEHANDELD_EXTERN: [
+        AFGEHANDELD,
+        GEANNULEERD,
+    ],
 }
