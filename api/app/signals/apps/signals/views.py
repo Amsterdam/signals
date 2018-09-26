1ecea3b431c1e85627d8067ab5fd87e491faf2db
Niels van Dijk <niels@differentstep.com>
2018-09-26 09:32:28 +0200

    SIG-612 Implemented get_object for sub category viewset to work with double slug kwargs.

--- api/app/signals/apps/signals/views.py
+++ api/app/signals/apps/signals/views.py
@@ -1,10 +1,12 @@
 import logging
 import re
 
+from datapunt_api.pagination import HALPagination
 from datapunt_api.rest import DatapuntViewSet
 from django.conf import settings
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import mixins, viewsets
+from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.settings import api_settings
 from rest_framework.status import HTTP_202_ACCEPTED
@@ -165,6 +167,12 @@ class MainCategoryViewSet(DatapuntViewSet):
 class SubCategoryViewSet(mixins.RetrieveModelMixin, viewsets.GenericViewSet):
     queryset = SubCategory.objects.all()
     serializer_class = SubCategoryHALSerializer
+    pagination_class = HALPagination
 
-    def retrieve(self, request, *args, **kwargs):
-        pass
+    def get_object(self):
+        queryset = self.filter_queryset(self.get_queryset())
+        obj = get_object_or_404(queryset,
+                                main_category__slug=self.kwargs['slug'],
+                                slug=self.kwargs['sub_slug'])
+        self.check_object_permissions(self.request, obj)
+        return obj
