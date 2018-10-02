c5d42e23f87727f5bf02b857f17f72f64ce43b20
Thijs Coenen <t.coenen@amsterdam.nl>
2018-10-02 14:40:18 +0200

    SIG-673 fix test

--- api/app/tests/apps/sigmax/test_signal_receivers.py
+++ api/app/tests/apps/sigmax/test_signal_receivers.py
@@ -2,9 +2,16 @@ from unittest import mock
 
 from django.test import TestCase
 
+from signals.apps.signals.models import update_status
+from tests.apps.signals.factories import SignalFactory
+
 
 class TestSignalReceivers(TestCase):
 
     @mock.patch('signals.apps.sigmax.signal_receivers.tasks', autospec=True)
     def test_create_initial_handler(self, mocked_tasks):
-        pass
+        signal = SignalFactory.create()
+        update_status.send(sender=self.__class, signal_obj=signal)
+
+        mocked_tasks.push_to_sigmax.delay.assert_called_once_with(pk=signal.id)
+
