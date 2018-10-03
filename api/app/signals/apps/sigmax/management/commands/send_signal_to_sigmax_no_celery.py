8d67c62b3bba203e36f24cf3a9fd17c665c46b17
Thijs Coenen <t.coenen@amsterdam.nl>
2018-10-03 15:44:00 +0200

    SIG-696 port send_signal_to_sigmax to django-signals

---
+++ api/app/signals/apps/sigmax/management/commands/send_signal_to_sigmax_no_celery.py
@@ -0,0 +1,43 @@
+from django.core.management.base import BaseCommand
+
+from signals.apps.sigmax.outgoing import (
+    CREEER_ZAAK_SOAPACTION,
+    VOEG_ZAAKDOCUMENT_TOE_SOAPACTION,
+    _generate_creeerZaak_Lk01,
+    _generate_voegZaakdocumentToe_Lk01,
+    _send_stuf_message
+)
+from signals.apps.signals.models import Signal
+from tests.apps.signals.factories import SignalFactoryValidLocation
+
+
+# Known to still be problematic, work in progress
+class Command(BaseCommand):
+    help = 'Send a message to "Sigmax" as a manual test.'
+
+    def handle(self, *args, **options):
+        # first step; generate a "Zaak" in Sigmax's system
+        self.stdout.write('Send a message to Sigmax.')
+        test_signal: Signal = SignalFactoryValidLocation.create(
+            text='Dit is een test bericht van Datapunt Amsterdam aan Sigmax City Control',
+        )
+
+        msg = _generate_creeerZaak_Lk01(test_signal)
+        self.stdout.write('Hier het bericht:')
+        self.stdout.write(msg)
+        self.stdout.write('Einde bericht')
+
+        self.stdout.write('Sending a message to Sigmax.')
+        r = _send_stuf_message(msg, CREEER_ZAAK_SOAPACTION)
+        self.stdout.write('response status code: {}'.format(r.status_code))
+        self.stdout.write('Logging response.text :')
+        self.stdout.write(r.text)
+
+        # second step; generate PDF and send it to SIGMAX
+        msg_2 = _generate_voegZaakdocumentToe_Lk01(test_signal)
+
+        self.stdout.write('Sending a PDF to Sigmax.')
+        r = _send_stuf_message(msg_2, VOEG_ZAAKDOCUMENT_TOE_SOAPACTION)
+        self.stdout.write('response status code: {}'.format(r.status_code))
+        self.stdout.write('Logging response.text :')
+        self.stdout.write(r.text)
