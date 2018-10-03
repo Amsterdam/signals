84deece78ebd5c67b25f0c2c3584c849acf8e198
Niels van Dijk <niels@differentstep.com>
2018-10-03 16:42:57 +0200

    SIG-688 Updated docstring.

--- api/app/signals/apps/sigmax/outgoing.py
+++ api/app/signals/apps/sigmax/outgoing.py
@@ -36,12 +36,11 @@ class SigmaxException(Exception):
 # TODO SIG-593: implement data mapping if and when it is defined
 
 def _generate_creeerZaak_Lk01(signal):
+    """Generate XML for Sigmax creeerZaak_Lk01
+
+    SIGMAX will be set up to receive Signals (meldingen) that have no address but do have
+    coordinates (middle of park, somewhere on a body of water, etc.).
     """
-    Generate XML for Sigmax creeerZaak_Lk01
-    """
-    # SIGMAX will be set up to receive Signals (meldingen) that have no
-    # address but do have coordinates (middle of park, somewhere on a
-    # body of water, etc.) Here we set the address if we have it.
     num_days_priority_mapping = {
         Priority.PRIORITY_HIGH: 1,
         Priority.PRIORITY_NORMAL: 3,
