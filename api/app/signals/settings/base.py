74fb8e9a69bc2c32784cabeea7a284b47a3753c9
Niels van Dijk <niels@differentstep.com>
2018-10-09 17:04:53 +0200

    SIG-714 Changed media and static dir locations for development.

--- api/app/signals/settings/base.py
+++ api/app/signals/settings/base.py
@@ -144,9 +144,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images) and media files
 STATIC_URL = '/signals/static/'
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
+STATIC_ROOT = os.path.join(os.path.dirname(os.path.dirname(BASE_DIR)), 'static')
 MEDIA_URL = '/signals/media/'
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
+MEDIA_ROOT = os.path.join(os.path.dirname(os.path.dirname(BASE_DIR)), 'media')
 
 # Object store / Swift
 if os.getenv('SWIFT_ENABLED', 'false') == 'true':
