9813726688b11a5c2b0a05d304ab5357a00c65bd
Niels van Dijk <niels@differentstep.com>
2018-10-18 11:27:47 +0200

    SIG-570 Changed sending notification email initial create implementation and added tests.

--- api/app/signals/apps/email_integrations/integrations/core.py
+++ api/app/signals/apps/email_integrations/integrations/core.py
@@ -1,7 +1,6 @@
 """
 E-mail integration for 'core' Signal behaviour.
 """
-import logging
 import re
 
 from django.conf import settings
@@ -9,9 +8,7 @@ from django.core.mail import send_mail
 from django.template import loader
 
 from signals.apps.email_integrations.messages import ALL_AFHANDELING_TEXT
-from signals.apps.signals.workflow import AFGEHANDELD
-
-LOG = logging.getLogger()
+from signals.apps.signals import workflow
 
 
 def get_valid_email(signal):
@@ -27,7 +24,47 @@ def get_valid_email(signal):
 
 
 def send_mail_reporter_created(signal):
-    LOG.info('Handling create signal')
+    """Send a notification e-mail to the reporter about initial create of the given `Signal` object.
+
+    :param signal: Signal object
+    :returns: number of successfully send messages or None
+    """
+    email = get_valid_email(signal)
+    if not email:
+        return None
+
+    subject = f'Bedankt voor uw melding ({signal.id})'
+    message = create_initial_create_notification_message(signal)
+    to = signal.reporter.email
+
+    return send_mail(subject, message, settings.NOREPLY, (to, ))
+
+
+def create_initial_create_notification_message(signal):
+    """Create e-mail body message about initial create of the given `Signal` object.
+
+    :param signal: Signal object
+    :returns: message (str)
+    """
+    context = {
+        'signal': signal,
+        'afhandelings_text': (
+            ALL_AFHANDELING_TEXT[signal.category_assignment.sub_category.handling]
+        ),
+    }
+    template = loader.get_template('email/signal_created.txt')
+    message = template.render(context)
+    return message
+
+
+def send_mail_reporter_status_changed(signal, status):
+    """Send a notification e-mail to the reporter about status change of the given `Signal` object.
+
+    :param signal: Signal object
+    :param status: Status object
+    :returns: number of successfully send messages
+    """
+    signal_is_afgehandeld = status.state == workflow.AFGEHANDELD
     email = get_valid_email(signal)
     LOG.debug('Valid email: ' + str(email))
     if email:
--- api/app/tests/apps/email_integrations/integrations/test_core.py
+++ api/app/tests/apps/email_integrations/integrations/test_core.py
@@ -1,3 +1,4 @@
+from django.core import mail
 from django.test import TestCase
 
 from signals.apps.email_integrations.integrations import core
@@ -8,18 +9,35 @@ class TestCore(TestCase):
 
     def setUp(self):
         self.signal = SignalFactory.create(reporter__email='foo@bar.com')
+        self.signal_invalid = SignalFactory.create(reporter__email='no-valid-email-address')
 
     def test_get_valid_email(self):
         email = core.get_valid_email(self.signal)
         self.assertEqual(email, 'foo@bar.com')
 
     def test_get_valid_email_invalid(self):
-        signal = SignalFactory.create(reporter__email='no-valid-email-address')
-        email = core.get_valid_email(signal)
+        email = core.get_valid_email(self.signal_invalid)
         self.assertEqual(email, None)
 
     def test_send_mail_reporter_created(self):
-        pass
+        num_of_messages = core.send_mail_reporter_created(self.signal)
+
+        self.assertEqual(num_of_messages, 1)
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertEqual(mail.outbox[0].subject, f'Bedankt voor uw melding ({self.signal.id})')
+        self.assertEqual(mail.outbox[0].to, ['foo@bar.com', ])
+
+    def test_send_mail_reporter_created_invalid_email(self):
+        num_of_messages = core.send_mail_reporter_created(self.signal_invalid)
+
+        self.assertEqual(num_of_messages, None)
+
+    def test_create_initial_create_notification_message(self):
+        message = core.create_initial_create_notification_message(self.signal)
+
+        self.assertIn(str(self.signal.id), message)
+        self.assertIn(self.signal.text, message)
+        self.assertIn(self.signal.reporter.email, message)
 
     def test_send_mail_reporter_status_changed(self):
         pass
