45dda3a84c223f1e288ee23a56b84d93dd9dfa37
Niels van Dijk <niels@differentstep.com>
2018-10-18 16:09:26 +0200

    SIG-620 Changed root view name for api version 1.

--- api/app/signals/apps/signals/routers.py
+++ api/app/signals/apps/signals/routers.py
@@ -29,7 +29,7 @@ class SignalsAPIRootViewVersion0(routers.APIRootView):
         response.data['v1'] = {
             '_links': {
                 'self': {
-                    'href': reverse('api-v1:api-root'),
+                    'href': reverse('api-root-v1'),
                 }
             },
             'status': 'in development',
@@ -46,6 +46,7 @@ class SignalsAPIRootViewVersion1(routers.APIRootView):
 
 
 class SignalsRouterVersion1(routers.DefaultRouter):
+    root_view_name = 'api-root-v1'
     APIRootView = SignalsAPIRootViewVersion1
     routes = [
         # List route.
