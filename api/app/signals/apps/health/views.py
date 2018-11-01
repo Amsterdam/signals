de24bf4e241788340063384f16f4cc59746cf927
David van Buiten <david.vanbuiten@elements.nl>
2018-11-01 10:43:18 +0100

    WIP Health check categories

--- api/app/signals/apps/health/views.py
+++ api/app/signals/apps/health/views.py
@@ -40,3 +40,37 @@ def check_data(request):
         'Data OK {count} {model}'.format(count=count, model=health_check_model.__name__),
         content_type='text/plain',
         status=200)
+
+
+def _check_main_categories(request):
+    health_model = 'signals.MainCategory'
+    at_least_in_database = 1
+
+    try:
+        health_check_model = apps.get_model(health_model)
+    except LookupError:
+        raise ImproperlyConfigured(
+            "{} doesn't resolve to a useable model".format(health_model))
+
+    count = health_check_model.objects.count()
+    if count < at_least_in_database:
+        error_msg = 'Too few \'{}\' items in the database'.format(health_model)
+        logger.error(error_msg)
+        return HttpResponse(error_msg, content_type='text/plain', status=500)
+
+
+def _check_sub_categories(request):
+    health_model = 'signals.SubCategory'
+    at_least_in_database = 1
+
+    try:
+        health_check_model = apps.get_model(health_model)
+    except LookupError:
+        raise ImproperlyConfigured(
+            "{} doesn't resolve to a useable model".format(health_model))
+
+    count = health_check_model.objects.count()
+    if count < at_least_in_database:
+        error_msg = 'Too few \'{}\' items in the database'.format(health_model)
+        logger.error(error_msg)
+        return HttpResponse(error_msg, content_type='text/plain', status=500)
