dde067e9899029eed6e38ebb574dfdc5d2dcc356
Thijs Coenen <t.coenen@amsterdam.nl>
2018-11-09 14:59:01 +0100

    SIG-437 flake8

--- api/app/signals/apps/signals/migrations/0025_create_history_view_in_db.py
+++ api/app/signals/apps/signals/migrations/0025_create_history_view_in_db.py
@@ -17,10 +17,10 @@ CREATE VIEW "signals_history_view" AS
         CAST("what" AS varchar(255)),
         CAST("who" AS varchar(255)),
         CAST("extra" AS varchar(255)),
-        CAST("description" AS varchar(3000))  /* Most extreme case is the Note model's text field. */
+        CAST("description" AS varchar(3000))  /* Most extreme case; the Note model's text field. */
     FROM (
         SELECT
-			CONCAT('UPDATE_STATUS_', CAST("s"."id" as VARCHAR(255))) AS "identifier",
+            CONCAT('UPDATE_STATUS_', CAST("s"."id" as VARCHAR(255))) AS "identifier",
             "s"."_signal_id" AS "_signal_id",
             "s"."created_at" AS "when",
             'UPDATE_STATUS' As "what",
@@ -30,7 +30,7 @@ CREATE VIEW "signals_history_view" AS
         FROM
             "public"."signals_status" AS "s"
         UNION SELECT
-			CONCAT('UPDATE_PRIORITY_', CAST("p"."id" as VARCHAR(255))) AS "identifier",
+            CONCAT('UPDATE_PRIORITY_', CAST("p"."id" as VARCHAR(255))) AS "identifier",
             "p"."_signal_id" AS "_signal_id",
             "p"."created_at" AS "when",
             'UPDATE_PRIORITY' AS "what",
@@ -40,7 +40,7 @@ CREATE VIEW "signals_history_view" AS
         FROM
             "public"."signals_priority" AS "p"
         UNION SELECT
-			CONCAT('UPDATE_CATEGORY_ASSIGNMENT_', CAST("ca"."id" as VARCHAR(255))) AS "identifier",
+            CONCAT('UPDATE_CATEGORY_ASSIGNMENT_', CAST("ca"."id" as VARCHAR(255))) AS "identifier",
             "ca"."_signal_id" AS "_signal_id",
             "ca"."created_at" AS "when",
             'UPDATE_CATEGORY_ASSIGNMENT' AS "what",
@@ -53,7 +53,7 @@ CREATE VIEW "signals_history_view" AS
         WHERE
             "ca"."sub_category_id" = "sc"."id"
         UNION SELECT
-			CONCAT('CREATE_NOTE_', CAST("n"."id" as VARCHAR(255))) AS "identifier",
+            CONCAT('CREATE_NOTE_', CAST("n"."id" as VARCHAR(255))) AS "identifier",
             "n"."_signal_id" as "_signal_id",
             "n"."created_at" as "when",
             'CREATE_NOTE' as "what",
@@ -63,7 +63,7 @@ CREATE VIEW "signals_history_view" AS
         FROM
             "public"."signals_note" AS "n"
         UNION SELECT
-			CONCAT('UPDATE_LOCATION_', CAST("l"."id" as VARCHAR(255))) AS "identifier",
+            CONCAT('UPDATE_LOCATION_', CAST("l"."id" as VARCHAR(255))) AS "identifier",
             "l"."_signal_id" AS "_signal_id",
             "l"."created_at" AS "when",
             'UPDATE_LOCATION' AS "what",
