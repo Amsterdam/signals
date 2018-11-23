a673540b870a9291f05e2cbd330ccf5563c39ece
David van Buiten <david.vanbuiten@elements.nl>
2018-11-23 11:10:52 +0100

    Created a util function to get the main and sub category from a path

---
+++ api/app/signals/apps/signals/utils.py
@@ -0,0 +1,13 @@
+from django.urls import resolve
+
+from signals.apps.signals.views import MainCategoryViewSet, SubCategoryViewSet
+
+
+def resolve_categories(path):
+    view, args, kwargs = resolve(path)
+    if id(view.cls) == id(SubCategoryViewSet):
+        sub_category = view.cls.queryset.get(slug=kwargs['sub_slug'],
+                                             main_category__slug=kwargs['slug'])
+        return sub_category.main_category, sub_category
+    elif id(view.cls) == id(MainCategoryViewSet):
+        return view.cls.queryset.get(slug=kwargs['slug']), None
---
+++ api/app/tests/apps/signals/test_categories.py
@@ -0,0 +1,37 @@
+from rest_framework.reverse import reverse
+from rest_framework.test import APITestCase
+
+from signals.apps.signals.models import SubCategory
+from signals.apps.signals.utils import resolve_categories
+from tests.apps.signals.factories import MainCategoryFactory, SubCategoryFactory
+
+
+class TestResolveCategories(APITestCase):
+    def setUp(self):
+        self.main_category = MainCategoryFactory.create()
+        self.sub_category = SubCategoryFactory.create()
+
+    def test_main_category(self):
+        path = reverse('v1:category-detail', kwargs={'slug': self.main_category.slug})
+        main_category, sub_category = resolve_categories(path=path)
+
+        self.assertEqual(self.main_category.pk, main_category.pk)
+
+    def test_sub_category(self):
+        path = reverse('v1:sub-category-detail', kwargs={
+            'slug': self.sub_category.main_category.slug,
+            'sub_slug': self.sub_category.slug
+        })
+        main_category, sub_category = resolve_categories(path=path)
+
+        self.assertEqual(self.sub_category.main_category.pk, main_category.pk)
+        self.assertEqual(self.sub_category.pk, sub_category.pk)
+
+    def test_does_not_exists_sub_category(self):
+        path = reverse('v1:sub-category-detail', kwargs={
+            'slug': 'does_not',
+            'sub_slug': 'exists'
+        })
+
+        with self.assertRaises(SubCategory.DoesNotExist):
+            resolve_categories(path=path)
