b8327e303f5a46d2f7d3158c25bfeec1ee4afcac
Thijs Coenen <t.coenen@amsterdam.nl>
2018-11-26 09:40:22 +0100

    Compare identity with the `is` operator not `==`

--- api/app/signals/apps/signals/utils.py
+++ api/app/signals/apps/signals/utils.py
@@ -5,9 +5,9 @@ from signals.apps.signals.views import MainCategoryViewSet, SubCategoryViewSet
 
 def resolve_categories(path):
     view, args, kwargs = resolve(path)
-    if id(view.cls) == id(SubCategoryViewSet):
+    if view.cls is SubCategoryViewSet:
         sub_category = view.cls.queryset.get(slug=kwargs['sub_slug'],
                                              main_category__slug=kwargs['slug'])
         return sub_category.main_category, sub_category
-    elif id(view.cls) == id(MainCategoryViewSet):
+    elif view.cls is MainCategoryViewSet:
         return view.cls.queryset.get(slug=kwargs['slug']), None
