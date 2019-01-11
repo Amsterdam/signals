cb5af494de0f056744c2974fdcbcb585b989e9ab
Jorik Kraaikamp <jorikkraaikamp@gmail.com>
2019-01-11 10:30:09 +0100

    Fixed the last of the tests.

--- api/app/tests/apps/zds/mixins.py
+++ api/app/tests/apps/zds/mixins.py
@@ -65,11 +65,12 @@ class ZDSMockMixin(object):
             self.urls[name], text=getattr(self, 'error_{}'.format(status)), status_code=status
         )
 
-    def get_exception_mock(self, mock, name, exception_class):
+    def get_exception_mock(self, mock, name, exception_class, url=None):
         """This is a mock for an exception. This will throw the exception_class error"""
-        return mock.get(
-            self.urls[name], exc=exception_class
-        )
+        if url is None:
+            url = self.urls[name]
+
+        return mock.get(url, exc=exception_class)
 
     # ZRC ##########################################################################################
     @property
