54eead6b4a1f73481b3eba6a9c0d875c8cb0ee82
Jaap Koetsier <jaap@chariot-it.nl>
2019-01-30 13:58:58 +0100

    SIG-930 PR comments, refactor test class

--- api/app/tests/apps/signals/test_filters_v1.py
+++ api/app/tests/apps/signals/test_filters_v1.py
@@ -1,14 +1,12 @@
 from datetime import datetime, timedelta
+from random import shuffle
 
-from django.db import connection
+from freezegun import freeze_time
 from rest_framework.test import APITestCase
 
-from signals.apps.signals.models import CategoryAssignment, Signal, Status
-from signals.apps.signals.workflow import BEHANDELING, ON_HOLD
+from signals.apps.signals.workflow import BEHANDELING, GEMELD, ON_HOLD
 from tests.apps.signals.factories import (
-    LocationFactory,
-    PriorityFactory,
-    ReporterFactory,
+    CategoryAssignmentFactory,
     SignalFactory,
     StatusFactory,
     SubCategoryFactory
@@ -25,111 +23,55 @@ class TestFilters(APITestCase):
 
     signals = []
     sub_categories = []
+    states = []
 
     @classmethod
     def _create_signals(cls):
-        cls.signals = [SignalFactory.create() for _ in
-                       range(cls.SIGNALS_DISTRIBUTE_DAYS_CNT + cls.SIGNALS_DISTRIBUTE_HOURS_CNT)]
+        """
+        Creates SIGNALS_DISTRIBUTE_DAYS_CNT signals. One per day backwards starting from yesterday
+        current time.
+        Creates SIGNALS_DISTRIBUTE_HOURS_CNT signals every hour backwards from now
 
-    @classmethod
-    def _set_signal_created_updated_at(cls, signal_id: int, dt: datetime):
-        """ Sets created_at and updated_at to dt for signal with signal_id """
-        signals_tablename = Signal._meta.db_table
-
-        query = "UPDATE {} SET created_at = %s, updated_at = %s WHERE signal_id=%s".format(
-            signals_tablename)
-
-        with connection.cursor() as cursor:
-            cursor.execute(query, [dt, dt, signal_id])
-
-    @classmethod
-    def _distribute_signals_days(cls):
-        """ Distributes SIGNALS_DISTRIBUTE_DAYS_CNT signals, one per day, from yesterday
-        backwards """
-
+        If today is Friday 16:00, and SIGNALS_DISTRIBUTE_DAYS_CNT and SIGNALS_DISTRIBUTE_HOURS_CNT
+        are both set to 3, we will generate the following signals:
+        - Per day: Thursday (yesterday) 16:00, Wednesday (day before yesterday) 16:00, Tuesday 16:00
+        - Per hour: Friday 16:00 (now), Friday 15:00 (now minus 1 hour), Friday 14:00 (now minus 2h)
+        """
         now = datetime.utcnow()
 
-        for idx, signal in enumerate(cls.signals[:cls.SIGNALS_DISTRIBUTE_DAYS_CNT]):
-            cls._set_signal_created_updated_at(signal.signal_id, now - timedelta(days=idx + 1))
+        # Generate created/updated times for new signals.
+        times = [now - timedelta(days=idx + 1)
+                 for idx in range(cls.SIGNALS_DISTRIBUTE_DAYS_CNT)] + \
+                [now - timedelta(hours=idx)
+                 for idx in range(cls.SIGNALS_DISTRIBUTE_HOURS_CNT)]
 
-    @classmethod
-    def _distribute_signals_hours(cls):
-        """ Distributes SIGNALS_DISTRIBUTE_HOURS_CNT signals, one per hour, starting at time now()
-        going backwards """
+        cls.states = 3 * [BEHANDELING] + 2 * [ON_HOLD] + (len(times) - 3 - 2) * [GEMELD]
+        shuffle(cls.states)
 
-        now = datetime.utcnow()
-
-        frm = cls.SIGNALS_DISTRIBUTE_DAYS_CNT
-        to = cls.SIGNALS_DISTRIBUTE_DAYS_CNT + cls.SIGNALS_DISTRIBUTE_HOURS_CNT
-        for idx, signal in enumerate(cls.signals[frm:to]):
-            cls._set_signal_created_updated_at(signal.signal_id, now - timedelta(hours=idx))
-
-    @classmethod
-    def _distribute_signals_categories(cls):
-        """ Assigns created signals to new subcategories, round-robin """
         cls.sub_categories = [SubCategoryFactory.create() for _ in range(cls.SUBCATEGORY_CNT)]
 
-        for idx, signal in enumerate(cls.signals):
-            assignment = CategoryAssignment()
-            assignment._signal = signal
-            assignment.sub_category = cls.sub_categories[idx % len(cls.sub_categories)]
-            assignment.save()
-            signal.category_assignment = assignment
-            signal.save()
+        for idx, time in enumerate(times):
+            with freeze_time(time):
+                signal = SignalFactory.create()
+                StatusFactory(_signal=signal, state=cls.states[idx])
+                category_assignment = CategoryAssignmentFactory(_signal=signal,
+                                                                sub_category=cls.sub_categories[
+                                                                    idx % len(cls.sub_categories)])
+                signal.category_assignment = category_assignment
+                signal.save()
 
-    @classmethod
-    def _assign_default_relations(cls):
-        for signal in cls.signals:
-            location = LocationFactory(_signal=signal)
-            reporter = ReporterFactory(_signal=signal)
-            priority = PriorityFactory(_signal=signal)
-            status = StatusFactory(_signal=signal)
-            signal.location = location
-            signal.reporter = reporter
-            signal.status = status
-            signal.priority = priority
-            signal.save()
-
-    @classmethod
-    def _change_statuses(cls):
-        """ Changes status of 3 signals to BEHANDELING and 5 to ON_HOLD """
-
-        # Create 3 BEHANDELING
-        for signal in cls.signals[:3]:
-            status = Status()
-            status._signal = signal
-            status.state = BEHANDELING
-            status.save()
-
-            signal.status = status
-            signal.save()
-
-        # Create 2 ON_HOLD
-        for signal in cls.signals[3:5]:
-            status = Status()
-            status._signal = signal
-            status.state = ON_HOLD
-            status.save()
-
-            signal.status = status
-            signal.save()
+                cls.signals.append(signal)
 
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
         cls._create_signals()
-        cls._distribute_signals_categories()
-        cls._assign_default_relations()
-        cls._change_statuses()
-
-        cls._distribute_signals_days()
-        cls._distribute_signals_hours()
-
         cls.superuser = SuperUserFactory(username='superuser@example.com')
 
     def setUp(self):
         self.signals = TestFilters.signals
         self.sub_categories = TestFilters.sub_categories
+        self.states = TestFilters.states
 
     def _request_filter_signals(self, filter_params: dict):
         """ Does a filter request and returns the signal ID's present in the request """
@@ -276,22 +218,23 @@ class TestFilters(APITestCase):
         params = {"status": statuses}
 
         result_ids = self._request_filter_signals(params)
-        self.assertEquals(20, len(result_ids))
+        self.assertEquals(len(self.signals), len(result_ids))
 
     def test_filter_statuses_separate(self):
         """ Test result sets of statuses separately, and combined. """
         params = {"status": ['m']}
         result_ids = self._request_filter_signals(params)
-        self.assertEquals(15, len(result_ids))
+        self.assertEquals(self.states.count(GEMELD), len(result_ids))
 
         params = {"status": ['b']}
         result_ids = self._request_filter_signals(params)
-        self.assertEquals(3, len(result_ids))
+        self.assertEquals(self.states.count(BEHANDELING), len(result_ids))
 
         params = {"status": ['h']}
         result_ids = self._request_filter_signals(params)
-        self.assertEquals(2, len(result_ids))
+        self.assertEquals(self.states.count(ON_HOLD), len(result_ids))
 
         params = {"status": ['h', 'b']}
         result_ids = self._request_filter_signals(params)
-        self.assertEquals(5, len(result_ids))
+        self.assertEquals(self.states.count(ON_HOLD) + self.states.count(BEHANDELING),
+                          len(result_ids))
