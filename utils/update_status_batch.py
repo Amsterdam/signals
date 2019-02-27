09aace8add7ac736c60c7c0f06f3b9fe640ca455
Thijs Coenen <t.coenen@amsterdam.nl>
2019-02-27 17:20:30 +0100

    SIG-834 update script rewrite

--- utils/update_status_batch.py
+++ utils/update_status_batch.py
@@ -1,108 +1,96 @@
 """
-update_status_batch.py - Batch update signal status script
+Batch updater, reads CSV, uses SIA V1 API to update Signals in SIA.
 
-Expects a CSV file with status updates as input, with three values per row; the signal id, the new
-status (m, o, e, i, or ...) and the status change text.
+Rationale for using the API:
+The SIA API will log all actions and attribute them to a user. By using the API
+for bulk updates everything will be logged correctly. Furthermore, there should
+be one well maintained and complete interface to the data: the API.
 """
-import argparse
 import csv
-import os
-import sys
 
 import requests
-from get_signals import GetAccessToken
 
-EMAIL = ''
-PASSWORD = ''
-ACCEPTANCE = True
-API_URL = 'http://localhost:8000/signals'
+# Modes:
+# - local development: no log-in
+# - acceptation: logged-in
+# - production: logged-in
+
+# - should be resuamable without re-doing changes
+
+class CSVValidationError(Exception):
+    pass
 
 
-class BatchUpdate:
-    CSV_DELIMITER = ';'
+class AlreadyUpdated(Exception):
+    pass
 
-    rows = []
-    auth_header = {}
 
-    success = []
-    error = []
+class StatusUpdater:
+    DETAIL_PATH = '/v1/private/signals/{signal_id}'
+    DELIMITER = ';'
 
-    def __init__(self, filename: str):
-        with open(os.path.join(os.path.dirname(__file__), filename)) as f:
-            self.rows = list(csv.reader(f, delimiter=self.CSV_DELIMITER))
+    def __init__(self, csv_file, base_url, email, password):
+        """Load CSV file with requested updates plus some sanity checks."""
+        self._rows = self._load_csv(csv_file)
+        self._authorization_header = self._get_authorization_header()
 
-        self.auth_header = self._get_access_token()
+    def _load_csv(self, csv_file):
+        """Load CSV file and perform some sanity checks."""
+        rows = []
+        with open(csv_file, 'r') as f:
+            reader = csv.reader(f, delimiter=self.DELIMITER)
+
+            for i, row in enumerate(reader(), start=1):
+                if len(row) != 3:
+                    raise CSVValidationError(
+                        'Error in line {}. It should contain 3 fields. '
+                        'Current line contains: {}'.format(i , row)
+                    )
+                try:
+                    int(row[0])
+                except ValueError:
+                    raise CSVValidationError(
+                        'Error in CSV file line {}. '
+                        'First column must contain numerical signal id. '
+                        'This field currently contains {}'.format(i, row[0])
+                    )
+                # TODO: once eligable statusses are exposed in API, check that
+                # requested status change is possible.
+                rows.append(row)
+
+        return rows
+
+    def _get_authorization_header(self):
+        pass
+
+    def _get_signal(self, signal_id):
+        pass
+
+    def _patch_signal(self, signal_id, status, message):
+        pass
+
+    def _process_row(self, row):
+        """Update a single Signal."""
+        signal_id, target_status, target_msg = row
+
+        signal_data = self._get_signal(signal_id)   ## Move to requests, see etags
+        if signal_data['status']['state'] == target_status and \
+                signal_data['status']['text'] == target_msg:
+            error_msg = 'Signal {} was already updated.'.format(signal_id)
+            raise AlreadyUpdated(error_msg)
+
+        self._patch_signal(signal_id, target_status, target_msg)
+
+    def _process_rows(self):
+        """Update all Signals as requested in CSV file."""
+
+        for i, row in enumerate(self._rows, start=1):
+            try:
+                self._process_row(row)
+            except AlreadyUpdated:
+                print('Already processed line {}, skipping'.format(i))
+            # TODO: HTTP STATUS CODE ERRORS THAT BUBBLE UP FROM self._patch_signal
 
     def run(self):
-        for _id, status, text in self.rows:
-            self._update_status(_id, status, text)
+        self._process_rows()
 
-    def _get_access_token(self):
-        gat = GetAccessToken()
-        return gat.getAccessToken(EMAIL, PASSWORD, ACCEPTANCE)
-
-    def _update_status(self, signal_id, signal_status, update_text):
-        url = API_URL + '/v1/private/signals/{}'.format(signal_id)
-
-        resp = requests.get(url, headers=self.auth_header)
-
-        if resp.status_code != 200:
-            print("Signal {: >8}: Problem retrieving (HTTP response code {})".format(signal_id,
-                                                                                     resp.status_code))
-            return
-
-        if resp.json()["status"]["state"] == signal_status:
-            print("Signal {: >8}: Signal already has status '{}'".format(signal_id, signal_status))
-            return
-
-        data = {
-            "status": {
-                "state": signal_status,
-                "text": update_text
-            }
-        }
-
-        resp = requests.patch(url, json=data, headers=self.auth_header)
-
-        if resp.status_code == 200:
-            print("Signal {: >8}: Updated to status '{}' with text '{}'".format(
-                signal_id,
-                signal_status,
-                update_text,
-            ))
-
-            return
-
-        print("Signal {: >8}: ERROR".format(signal_id))
-
-
-def handle_commandline():
-    parser = argparse.ArgumentParser(description='Perform bulk status update.')
-    parser.add_argument(
-        'csv_file', metavar='CSV_FILE', type=str,
-        help='CSV file with <signal id>;<status string>;<Status update message>',
-    )
-    parser.add_argument(
-        '--url', metavar='API_URL', type=str, required=True,
-        help='Signals API Base url e.g. http://localhost:8000/signals'
-    )
-    parser.add_argument(
-        '--user', metavar='USER', type=str, required=True,
-        help='SIA user on whose behalf these changes will be performed'
-    )
-    parser.add_argument(
-        '--password', metavar='PW', type=str, required=True,
-        help='Password to authenticate the SIA user with.'
-    )
-    parser.add_argument(
-        '--production', type=bool, action='store_true',
-        help='Perform bulk update on the PRODUCTION system!'
-    )
-
-    return parser.parse_args()
-
-if __name__ == "__main__":
-    args = handle_commandline()
-
-    batch_update = BatchUpdate(args.csv_file)
-    # batch_update.run()
