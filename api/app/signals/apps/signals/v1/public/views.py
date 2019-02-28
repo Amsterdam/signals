62500b16fdc46f34f349e6fea0f5ec6d41248486
David van Buiten <david.vanbuiten+github@gmail.com>
2019-02-28 10:51:32 +0100

    SIG-990 Added parent and children to the _links, also added curies and added namespacing

--- api/app/signals/apps/signals/v1/public/views.py
+++ api/app/signals/apps/signals/v1/public/views.py
@@ -2,6 +2,8 @@ from datapunt_api.pagination import HALPagination
 from datapunt_api.rest import DatapuntViewSet
 from rest_framework.generics import get_object_or_404
 from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.viewsets import GenericViewSet
 from rest_framework_extensions.mixins import DetailSerializerMixin
 
@@ -54,3 +56,14 @@ class SubCategoryViewSet(RetrieveModelMixin, GenericViewSet):
                                 slug=self.kwargs['sub_slug'])
         self.check_object_permissions(self.request, obj)
         return obj
+
+
+class NamespaceView(APIView):
+    """
+    Used for the curies namespace, at this moment it is just a dummy landing page so that we have
+    a valid URI that resolves
+
+    TODO: Implement HAL standard for curies in the future
+    """
+    def get(self, request):
+        return Response()
