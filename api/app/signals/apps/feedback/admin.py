74c508cdd2a0f257e20d8c9267f7c064fd1b8647
Thijs Coenen <t.coenen@amsterdam.nl>
2019-04-04 11:50:13 +0200

    SIG-1085 flake8

--- api/app/signals/apps/feedback/admin.py
+++ api/app/signals/apps/feedback/admin.py
@@ -3,7 +3,7 @@ Allow standard feedback answers to be managed through Django Admin.
 """
 from django.contrib import admin
 
-from signals.apps.feedback.models import Feedback, StandardAnswer
+from signals.apps.feedback.models import StandardAnswer
 
 
 class StandardAnswerAdmin(admin.ModelAdmin):
