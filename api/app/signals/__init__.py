fa1f0ea77715fb466c16a1c1636e291cc4934f63
Thijs Coenen <t.coenen@amsterdam.nl>
2019-04-05 14:32:49 +0200

    Version bump bugfix release 0.10.5

--- api/app/signals/__init__.py
+++ api/app/signals/__init__.py
@@ -16,11 +16,11 @@ __all__ = ['celery_app', 'VERSION', 'API_VERSIONS', ]
 # `/signals/v1/...` will always have major API version number `1`.
 
 # Application version (Major, minor, patch)
-VERSION = (0, 10, 4)
+VERSION = (0, 10, 5)
 
 # API versions (Major, minor, patch)
 API_VERSIONS = {
-    'v0': (0, 2, 1),
+    'v0': (0, 3, 0),
     'v1': (1, 6, 0),
 }
 
