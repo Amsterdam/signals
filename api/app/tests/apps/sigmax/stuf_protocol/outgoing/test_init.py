e6f092753d9ba2de10dae0c2b021f512065a8e77
David van Buiten <david.vanbuiten+github@gmail.com>
2019-07-29 11:43:54 +0200

    Added 2 missing unittest

--- api/app/tests/apps/sigmax/stuf_protocol/outgoing/test_init.py
+++ api/app/tests/apps/sigmax/stuf_protocol/outgoing/test_init.py
@@ -7,7 +7,11 @@ from unittest import mock
 from django.test import TestCase
 
 from signals.apps.sigmax.models import CityControlRoundtrip
-from signals.apps.sigmax.stuf_protocol.exceptions import SentTooManyTimesError
+from signals.apps.sigmax.stuf_protocol.exceptions import (
+    CreeerZaakLk01Error,
+    SentTooManyTimesError,
+    VoegZaakDocumentToeLk01Error
+)
 from signals.apps.sigmax.stuf_protocol.outgoing import handle
 from signals.apps.signals import workflow
 from signals.apps.signals.models import Status
@@ -53,3 +57,26 @@ class TestHandle(TestCase):
              f'Verzending van melding naar THOR is gelukt onder nummer {self.signal.sia_id}.01.',  # noqa
              last_status.text,
         )
+
+    @mock.patch('signals.apps.sigmax.stuf_protocol.outgoing.send_signal_and_pdf', autospec=True)
+    def test_CreeerZaakLk01Error(self, patched_send_signal_and_pdf):
+        patched_send_signal_and_pdf.side_effect = CreeerZaakLk01Error('this is a test')
+
+        with self.assertRaises(CreeerZaakLk01Error):
+            handle(self.signal)
+
+        self.signal.refresh_from_db()
+        self.assertEqual(self.signal.status.state, workflow.VERZENDEN_MISLUKT)
+        self.assertEqual(self.signal.status.text, 'Verzending van melding naar THOR is mislukt.')
+
+    @mock.patch('signals.apps.sigmax.stuf_protocol.outgoing.send_signal_and_pdf', autospec=True)
+    def test_VoegZaakDocumentToeLk01Error(self, patched_send_signal_and_pdf):
+        patched_send_signal_and_pdf.side_effect = VoegZaakDocumentToeLk01Error('this is a test')
+
+        handle(self.signal)
+
+        self.signal.refresh_from_db()
+
+        self.assertEqual(self.signal.status.state, workflow.VERZONDEN)
+        self.assertIn('Let op: waarschijnlijk is de PDF niet verzonden naar CityControl.',
+                      self.signal.status.text)
