afee7a8b9536107580de7dfb210ce4005dfe57fc
David van Buiten <david.vanbuiten+github@gmail.com>
2019-08-28 11:29:08 +0200

    Added commments to code

--- api/app/signals/apps/api/generics/relations.py
+++ api/app/signals/apps/api/generics/relations.py
@@ -13,6 +13,21 @@ class ParameterisedHyperlinkedRelatedField(relations.HyperlinkedRelatedField):
         return False
 
     def get_object(self, view_name, view_args, view_kwargs):
+        """
+        Return the object corresponding to a matched URL.
+
+        Takes the matched URL conf arguments, and should return an object instance, or raise an
+        `ObjectDoesNotExist` exception.
+
+        For example:
+            In SIA we use this to get the Category that has a URL configured like
+            'public/terms/categories/<str:slug>/sub_categories/<str:sub_slug>'
+
+            For this example the lookup_fields look like
+                (('parent.slug', 'slug'), ('slug', 'sub_slug'))
+            Which will result in a lookup in the database like
+                self.get_queryset().get(parent__slug='X', slug='Y')
+        """
         kwargs = {}
         for model_field, url_param in self.lookup_fields:
             model_field = model_field.replace('.', '__')
@@ -20,6 +35,20 @@ class ParameterisedHyperlinkedRelatedField(relations.HyperlinkedRelatedField):
         return self.get_queryset().get(**kwargs)
 
     def get_url(self, obj, view_name, request, format):
+        """
+        Given an object, return the URL that hyperlinks to the object.
+
+        The URL can be parametrised with multiple attributes from the given object (Or any related
+        object). In the lookup_fields it is possible to give the model_field in the same notation as
+        used by the ORM and the parameter in the URL. Like (('pk', 'pk'))
+
+        For example:
+            In SIA we use this to get the URL of a Category which is configured with an URL that
+            looks like 'public/terms/categories/<str:slug>/sub_categories/<str:sub_slug>'
+
+            For this example the lookup_fields will look like
+                (('parent.slug', 'slug'), ('slug', 'sub_slug'))
+        """
         kwargs = {}
         for model_field, url_param in self.lookup_fields:
             attr = obj
