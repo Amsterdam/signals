6a7ffd15fde182304ab9fc43aab5b59efb2c8dae
Thijs Coenen <t.coenen@amsterdam.nl>
2019-09-02 16:07:34 +0200

    SIG-1545 working version for demo

--- api/app/signals/apps/reporting/indicators/p_melding_tevreden.py
+++ api/app/signals/apps/reporting/indicators/p_melding_tevreden.py
@@ -4,25 +4,29 @@ from django.db import connection
 
 SQL = """
 select
-    cat.slug,
-    count(feedback.submitted_at),
-    round(
-        count(case when feedback.is_satisfied = true then true else null end) :: float 
-        / count(feedback.submitted_at) :: float
-        * 100
-    ) :: int as P_MELDING_TEVREDEN
-from
-    public.feedback_feedback as feedback
-    inner join public.signals_signal as sig
-        on feedback."_signal_id" = sig.id
-    inner join public.signals_categoryassignment as cas
-        on sig.category_assignment_id = cas.id
-    inner join public.signals_category as cat
-        on cat.id = cas.category_id
-where feedback.submitted_at >= %(begin)s :: timestamp
-    and feedback.submitted_at < %(end)s :: timestamp
-    and cat.parent_id is not null
-group by cat.id;
+    category_id,
+    parent_category_id,
+    case 
+        when sub.n_submitted = 0 then 0 
+        else round(100 * (sub.n_satisfied :: float) / sub.n_submitted) :: integer
+    end as P_MELDING_TEVREDEN
+from (
+    select
+        cat.id as category_id,
+        cat.parent_id as parent_category_id,
+        count(feedback.submitted_at) as n_submitted,
+        sum(case when feedback.is_satisfied then 1 else 0 end) as n_satisfied
+    from
+        public.feedback_feedback as feedback
+        inner join public.signals_signal as sig
+            on feedback."_signal_id" = sig.id
+        inner join public.signals_categoryassignment as cas
+            on sig.category_assignment_id = cas.id
+        inner join public.signals_category as cat
+            on cat.id = cas.category_id
+    where cat.parent_id is not null
+    group by cat.id
+) as sub
 """
 
 
