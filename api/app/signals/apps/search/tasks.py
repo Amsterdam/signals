2f9e2f73ee5dc4cdf67d6854d1a7361f6e6aaf9b
David van Buiten <david.vanbuiten+github@gmail.com>
2019-09-30 14:09:10 +0200

    WIP

--- api/app/signals/apps/search/tasks.py
+++ api/app/signals/apps/search/tasks.py
@@ -20,22 +20,5 @@ def save_to_elastic(signal_id):
 @app.task
 def rebuild_index():
     log.info('rebuild_index - start')
-
-    lock_file = Path('/tmp/rebuild_index.lock')
-    if lock_file.exists():
-        log.warning('rebuild_index - task is either still running or failed!')
-        return
-
-    log.info('rebuild_index - creating lock file')
-    lock_file.write_text(str(timezone.now()))
-
-    log.debug('rebuild_index - clear the index')
-    SignalDocument.clear_index()
-
-    log.debug('rebuild_index - rebuild the index')
     SignalDocument.index_documents()
-
-    log.info('rebuild_index - removing lock file')
-    lock_file.unlink()
-
     log.info('rebuild_index - done!')
