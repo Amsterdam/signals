733caa3a7d1905214bcbc28af88d0598f99ead2b
David van Buiten <david.vanbuiten+github@gmail.com>
2019-12-18 12:40:44 +0100

    Django security fix + disabled search for prod release

--- api/app/signals/settings/production.py
+++ api/app/signals/settings/production.py
@@ -7,5 +7,10 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
+# Full text search is not yet enabled on our production environment.
+# TODO: remove this override of the base settings when appropriate
+FEATURE_FLAGS['API_SEARCH_ENABLED'] = False  # noqa F405
+FEATURE_FLAGS['SEARCH_BUILD_INDEX'] = False  # noqa F405
+
 # Filter extra properties is not yet enabled for production
 FEATURE_FLAGS['API_FILTER_EXTRA_PROPERTIES'] = False  # noqa F405
