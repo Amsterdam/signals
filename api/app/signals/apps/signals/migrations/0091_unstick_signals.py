fdc1091a5d6af1e5da53390bb36af39e3b5dcb97
Thijs Coenen <t.coenen@amsterdam.nl>
2020-01-15 11:31:28 +0100

    SIG-2112 release a stuck signal + small doc update

---
+++ api/app/signals/apps/signals/migrations/0091_unstick_signals.py
@@ -0,0 +1,36 @@
+"""
+Fix SIG-2112
+"""
+from django.db import migrations
+
+PROBLEMATIC_SIA_IDS = [
+    365188
+]
+
+
+def _set_state_afgehandeld_extern(apps, schema_editor):
+    """
+    Move a Signal to state AFGEHANDELD_EXTERN regardless of workflow checks.
+    """
+    Signal = apps.get_model('signals', 'Signal')
+    Status = apps.get_model('signals', 'Status')
+
+    for signal in Signal.objects.filter(id__in=PROBLEMATIC_SIA_IDS):
+        new_status = Status(
+            _signal=signal,
+            state=AFGEHANDELD_EXTERN,
+            text='Vastgelopen melding vrijgegeven zonder tussenkomst CityControl.'
+        )
+        new_status.save()  # no full_clean, bypass workflow checks
+        signal.status = new_status
+        signal.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('signals', '0090_SIG-2008'),
+    ]
+
+    operations = [
+    ]
