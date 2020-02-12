6f4f9a3eb7c088660ed80ccd5fadf91b34641a58
David van Buiten <david.vanbuiten+github@gmail.com>
2020-02-12 16:12:28 +0100

    SIG-1708 Part 1 (Part 2 is now SIG-2242)

--- api/app/tests/apps/api/v1/test_private_category_endpoint.py
+++ api/app/tests/apps/api/v1/test_private_category_endpoint.py
@@ -65,8 +65,8 @@ class TestPrivateCategoryEndpoint(SIAReadWriteUserMixin, SignalsBaseApiTestCase)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
         data = response.json()
-        self.assertEqual(data['count'], 142)
-        self.assertEqual(len(data['results']), 142)
+        self.assertEqual(data['count'], 147)
+        self.assertEqual(len(data['results']), 147)
 
     def test_get_parent_category(self):
         self.client.force_authenticate(user=self.sia_read_write_user)
