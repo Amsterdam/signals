2ff78b5ef55b2e4a61af73007a0688def484308c
David van Buiten <david.vanbuiten+github@gmail.com>
2020-02-18 09:11:00 +0100

    SIG-1976 refactor of anonymizing reporter data

--- api/app/signals/apps/signals/models/reporter.py
+++ api/app/signals/apps/signals/models/reporter.py
@@ -4,29 +4,59 @@ from signals.apps.signals.models.mixins import CreatedUpdatedModel
 
 
 class Reporter(CreatedUpdatedModel):
-    """Privacy sensitive information on reporter."""
+    """
+    Privacy sensitive information on reporter.
 
-    ANONYMOUS_EMAIL = 'anonymous@example.com'
-    ANONYMOUS_PHONE = '+31000000000'
-
+    This information will be anonymized after X time
+    """
     _signal = models.ForeignKey(
-        "signals.Signal", related_name="reporters",
+        'signals.Signal', related_name='reporters',
         null=False, on_delete=models.CASCADE
     )
 
-    email = models.EmailField(blank=True)
-    phone = models.CharField(max_length=17, blank=True)
+    email = models.EmailField(blank=True, null=True)
+    phone = models.CharField(max_length=17, blank=True, null=True)
 
-    is_anonymized = models.BooleanField(default=False)
+    email_anonymized = models.BooleanField(default=False)
+    phone_anonymized = models.BooleanField(default=False)
 
-    def anonymize(self):
-        if self.email and self.email != self.ANONYMOUS_EMAIL:
-            self.email = self.ANONYMOUS_EMAIL
+    @property
+    def is_anonymized(self):
+        return self.email_anonymized or self.phone_anonymized
 
-        if self.phone and self.phone != self.ANONYMOUS_PHONE:
-            self.phone = self.ANONYMOUS_PHONE
+    def _anonymize_email(self):
+        if self.email:
+            if not self.email_anonymized:
+                self.email_anonymized = True
+            self.email = None
 
-        if not self.is_anonymized:
-            self.is_anonymized = True
+    def _anonymize_phone(self):
+        if self.phone:
+            if not self.phone_anonymized:
+                self.phone_anonymized = True
+            self.phone = None
 
-        self.save()
+    def anonymize(self, always_call_save=False):
+        call_save = False
+        if not self.email_anonymized and self.email:
+            self.email_anonymized = True
+            call_save = True
+
+        if not self.phone_anonymized and self.phone:
+            self.phone_anonymized = True
+            call_save = True
+
+        if call_save or always_call_save:
+            self.save()
+
+    def save(self, *args, **kwargs):
+        """
+        Make sure that the email and phone are set to none while saving the Reporter
+        """
+        if self.email_anonymized:
+            self.email = None
+
+        if self.phone_anonymized:
+            self.phone = None
+
+        super(Reporter, self).save(*args, **kwargs)
