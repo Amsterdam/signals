0dfc61a5c7db3177e85a99055a4a8ebfe277de00
David van Buiten <david.vanbuiten+github@gmail.com>
2020-04-02 11:14:33 +0200

    Workarround for bug SIG-2432, do not set the request on a thread when going to the django Admin

--- api/app/change_log/middleware.py
+++ api/app/change_log/middleware.py
@@ -6,6 +6,8 @@ class ChangeLoggerMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        ChangeLogger.thread.request = request
+        if not request.path.startswith('/signals/admin'):
+            # Only add the request on the thread for non admin calls
+            ChangeLogger.thread.request = request
         response = self.get_response(request)
         return response
