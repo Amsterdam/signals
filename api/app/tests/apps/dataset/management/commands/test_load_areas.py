376cb5305546244b8d6dce16badc5f7fc32ee7af
Thijs Coenen <t.coenen@amsterdam.nl>
2020-05-20 13:49:29 +0200

    SIG-2483 test load_areas command

---
+++ api/app/tests/apps/dataset/management/commands/test_load_areas.py
@@ -0,0 +1,32 @@
+from io import StringIO
+from unittest.mock import patch
+
+from django.core.management import call_command
+from django.test import TestCase
+
+from signals.apps.dataset.base import AreaLoader
+
+
+class FakeAreaLoader(AreaLoader):
+    PROVIDES = ['fake']
+
+    def __init__(self, type_string):
+        pass
+
+    def load(self):
+        pass
+
+
+class TestLoadAreas(TestCase):
+    @patch(
+        'signals.apps.dataset.management.commands.load_areas.inspect.getmembers',
+        return_value=[
+            (FakeAreaLoader.__name__, FakeAreaLoader),
+        ]
+    )
+    def test_load_areas(self, patched_getmembers):
+        buffer = StringIO()
+        call_command('load_areas', 'fake', stdout=buffer)
+
+        output = buffer.getvalue()
+        self.assertIn('Loading fake areas from gebieden API...', output)
