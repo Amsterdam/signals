d6b9494a7ec2b5111d0c9869158c704d945bf7bc
CBuiVNG <info@iloki.nl>
2020-07-09 18:27:04 +0200

    fix type lookup

--- api/app/signals/apps/dsl/evaluators/InEvaluator.py
+++ api/app/signals/apps/dsl/evaluators/InEvaluator.py
@@ -7,21 +7,31 @@ class InEvaluator(Evaluator):
     _TIME_FORMAT = '%H:%M:%S'
     def __init__(self, **kwargs):
         self._CMD_MAP = {
-            type(str) : self._list_handler,
-            type(datetime.datetime) : self._time_handler,
-            type(geos.Point) : self._geo_handler,
+            'str' : self._list_handler,
+            'datetime' : self._time_handler,
+            'point' : self._geo_handler,
         }
 
         self.lhs = kwargs.pop('lhs')
         self.rhs = kwargs.pop('rhs')
         self.rhs_prop = kwargs.get('rhs_prop', None)
 
+    def _get_type(self, obj):
+        if isinstance(obj, str):
+            return 'str'
+        elif isinstance(obj, datetime.datetime):
+            return 'datetime'
+        elif isinstance(obj, geos.Point):
+            return 'point'
+        else:
+            return 'unknown'
+    
     def _rais_type_error(self, exp, act):
         raise Exception("Error: expected: '{exp}', actual: '{act}'".format(exp=exp, act=act))
 
     def evaluate(self, ctx):
-        lhs_val = self.lhs.evaluate(ctx)
-        lhs_type = type(lhs_val)
+        lhs_val = self.resolve(ctx, self.lhs)
+        lhs_type = self._get_type(lhs_val)
         if lhs_type in self._CMD_MAP:
             return self._CMD_MAP[lhs_type](ctx, lhs_val)
         else:
@@ -29,13 +39,13 @@ class InEvaluator(Evaluator):
 
     
     def _list_handler(self, ctx, lhs_val):
-        rhs_val = self.rhs.evaluate(ctx)
+        rhs_val = self.resolve(ctx, self.rhs)
         if type(rhs_val) is not set:
             self._rais_type_error(exp=type(set), act=type(rhs_val))
         return lhs_val in rhs_val
 
     def _geo_handler(self, ctx, lhs_val):
-        rhs_val = self.rhs.evaluate(ctx)
+        rhs_val = self.resolve(ctx, self.rhs)
         if type(rhs_val) is not geos.MultiPolygon:
             self._rais_type_error(exp=type(geos.MultiPolygon), act=type(rhs_val))
         return rhs_val.within(lhs_val)
