4a5cb1e4832db75e1fe8a6275047ebf0d8e35a5a
CBuiVNG <info@iloki.nl>
2020-07-13 20:51:51 +0200

    simplified int and float

--- api/app/signals/apps/dsl/evaluators/TerminalEvaludator.py
+++ api/app/signals/apps/dsl/evaluators/TerminalEvaludator.py
@@ -4,15 +4,13 @@ from .Evaluator import Evaluator
 class TerminalEvaluator(Evaluator):
     id_val = None
     str_val = None
-    int_val = None
-    float_val = None
+    numeric_val = None
     time_val = None
 
     def __init__(self, **kwargs):
         self.id_val = kwargs.get('id_val', None)
         self.str_val = kwargs.get('str_val', None)
-        self.int_val = kwargs.get('int_val', None)
-        self.float_val = kwargs.get('float_val', None)
+        self.numeric_val = kwargs.get('numeric_val', None)
         self.time_val = kwargs.get('time_val', None)
 
     def _convert(self, s, flist=['%H:%M:%S', '%H:%M']):
@@ -29,9 +27,7 @@ class TerminalEvaluator(Evaluator):
             return self.str_val
         if self.time_val:
             return self._convert(self.time_val)
-        if self.int_val:
-            return self.int_val
-        if self.float_val:
-            return self.float_val
+        if self.numeric_val != None:
+            return self.numeric_val
 
         raise Exception("No value for term evaluator")
