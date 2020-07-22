8c9110f791fe462c95719f15f0a8900c49083335
David van Buiten <david.vanbuiten+github@gmail.com>
2020-07-22 11:33:48 +0200

    Added management command for signamx fail stucked signals. Removed celery-beat container from docker-compose.yml. Removed beat schedule from base settings file. Plus some small fixes.

---
+++ api/app/signals/apps/sigmax/management/commands/fail_stuck_sending_signals.py
@@ -0,0 +1,20 @@
+from timeit import default_timer as timer
+
+from django.core.management import BaseCommand
+
+from signals.apps.sigmax.tasks import fail_stuck_sending_signals
+from signals.apps.signals.workflow import TE_VERZENDEN, VERZENDEN_MISLUKT
+from signals.settings import SIGMAX_SEND_FAIL_TIMEOUT_MINUTES
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        start = timer()
+
+        self.stdout.write(f'Add status "{VERZENDEN_MISLUKT}" to Signals that are stuck in "{TE_VERZENDEN}" '
+                          f'for at least {SIGMAX_SEND_FAIL_TIMEOUT_MINUTES} minutes')
+        fail_stuck_sending_signals()
+
+        stop = timer()
+        self.stdout.write(f'Time: {stop - start:.2f} second(s)')
+        self.stdout.write('Done!')
