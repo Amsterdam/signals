5915a156d14282a3022d85a9f65ef1786ad94f61
Thijs Coenen <t.coenen@amsterdam.nl>
2020-08-10 22:35:40 +0200

    SIG-2730 add logo to PDF, marker position needs to be fixed still

--- api/app/signals/apps/api/v1/views/pdf.py
+++ api/app/signals/apps/api/v1/views/pdf.py
@@ -1,3 +1,8 @@
+import base64
+import os
+
+from django.conf import settings
+from django.contrib.staticfiles import finders
 from django.utils import timezone
 from django.views.generic.detail import SingleObjectMixin
 
@@ -5,6 +10,32 @@ from signals.apps.api.generics.permissions import SIAPermissions
 from signals.apps.api.pdf.views import PDFTemplateView  # TODO: move these
 from signals.apps.signals.models import Signal
 from signals.auth.backend import JWTAuthBackend
+
+
+def _get_data_uri(static_file):
+    formats = {
+        'svg': 'data:image/svg+xml;base64,',
+        'jpg': 'data:image/jpeg;base64',
+        'png': 'data:image/png;base64,',
+    }
+
+    result = finders.find(static_file)
+    if result:
+        filename = os.path.split(result)[1]
+        ext = filename.split('.')[-1]
+
+        try:
+            start = formats[ext]
+        except KeyError:
+            return ''  # We want no HTTP 500, just a missing image
+
+        with open(result, 'rb') as f:
+            encoded = base64.b64encode(f.read()).decode('utf-8')
+        data_uri = start + encoded
+
+        return data_uri
+    else:
+        return ''  # missing static file, results in missing image
 
 
 class GeneratePdfView(SingleObjectMixin, PDFTemplateView):
@@ -26,8 +57,12 @@ class GeneratePdfView(SingleObjectMixin, PDFTemplateView):
             rd_coordinates.x + 340.00,
             rd_coordinates.y + 125.00,
         )
+
+        logo_src = _get_data_uri(settings.API_PDF_LOGO_STATIC_FILE)
+
         return super(GeneratePdfView, self).get_context_data(
             bbox=bbox,
             images=self.object.attachments.filter(is_image=True),
             user=self.request.user,
+            logo_src=logo_src,
         )
