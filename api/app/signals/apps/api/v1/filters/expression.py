a14e62682a416df4f6271da0390a074a5217c168
CBuiVNG <info@iloki.nl>
2020-09-15 12:59:53 +0200

    fixed filters, fix migration issues

---
+++ api/app/signals/apps/api/v1/filters/expression.py
@@ -0,0 +1,13 @@
+from django_filters.rest_framework import FilterSet, filters
+
+from signals.apps.api.v1.filters.utils import expression_choices, expression_type_choices
+
+
+class ExpressionFilterSet(FilterSet):
+    """
+    FilterSet used in the V1 API of expressions, options to filter on:
+    - expression name
+    - expression type
+    """
+    name = filters.ChoiceFilter(choices=expression_choices)
+    type_name = filters.MultipleChoiceFilter(field_name='_type__name', choices=expression_type_choices)
