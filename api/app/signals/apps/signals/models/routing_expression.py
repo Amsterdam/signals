98851e59df81d4047252da1e8c14e28d61c75d5d
CBuiVNG <info@iloki.nl>
2020-09-18 12:17:22 +0200

    add expression relation to department assignment

--- api/app/signals/apps/signals/models/routing_expression.py
+++ api/app/signals/apps/signals/models/routing_expression.py
@@ -3,10 +3,31 @@ from django.contrib.gis.db import models
 from signals.apps.signals.models import Department, Expression
 
 
+class RoutingExpressionManager(models.Manager):
+    def update_routing(self, instance, data):
+        department = data['_department']['id']
+        order = data.get('order', None)
+        if not hasattr(instance, 'routing_department'):
+            instance.routing_department = RoutingExpression.objects.create(
+                _department=department,
+                _expression=instance,
+                order=order
+            )
+        else:
+            instance.routing_department._department = department
+            instance.routing_department.order = order
+            instance.routing_department.save()
+
+
 class RoutingExpression(models.Model):
-    _expression = models.ForeignKey(Expression, on_delete=models.CASCADE)
+    # we only allow one department routing per expression
+    _expression = models.OneToOneField(
+        Expression,
+        on_delete=models.CASCADE,
+        unique=True,
+        related_name='routing_department'
+    )
     _department = models.ForeignKey(Department, on_delete=models.CASCADE)
     order = models.PositiveIntegerField(default=0, db_index=True)
 
-    class Meta:
-        unique_together = ('_expression', '_department')
+    objects = RoutingExpressionManager()
