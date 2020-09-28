69d8d023ec4f1e4e527cd6af650c87dad11304dc
David van Buiten <david.vanbuiten+github@gmail.com>
2020-09-28 11:49:09 +0200

    Removed _default value

--- api/app/signals/apps/reporting/utils.py
+++ api/app/signals/apps/reporting/utils.py
@@ -6,15 +6,12 @@ from django.core.exceptions import ImproperlyConfigured
 from django.core.files.storage import FileSystemStorage
 from swift.storage import SwiftStorage
 
-_default = os.getenv('SWIFT_DEFAULT_USING', 'default')
 
-
-def _get_storage_backend(using: str = _default) -> Union[FileSystemStorage, SwiftStorage]:
+def _get_storage_backend(using: str) -> Union[FileSystemStorage, SwiftStorage]:
     """
     Returns a SwiftStorage class OR a FileSystemStorage
 
-    When not providing a "using" value the "default" settings will be used, this means that the normal SWIFT_* settings
-    must be set properly. Otherwise the code will look for the settings in the SWIFT dict where "using" must be a
+    When providing a "using" value the code will look for the settings in the SWIFT dict where "using" must be a
     existing key containing a dict of settings.
 
     To disable Swift storage the SWIFT_ENABLED environment variable can be set to anything but
@@ -29,5 +26,4 @@ def _get_storage_backend(using: str = _default) -> Union[FileSystemStorage, Swif
     if not hasattr(settings, 'SWIFT'):
         raise ImproperlyConfigured('SWIFT settings must be set!')
 
-    swift_settings = settings.SWIFT.get(using, {}) if using != _default else {}
-    return SwiftStorage(**swift_settings)
+    return SwiftStorage(**settings.SWIFT.get(using, {}))
