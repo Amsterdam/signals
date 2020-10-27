172cf2ed03367365827857cb79369a512e6316ae
David van Buiten <david.vanbuiten+github@gmail.com>
2020-10-27 09:33:30 +0100

    Small fix after rebase

--- api/app/tests/apps/api/v1/test_mixins.py
+++ api/app/tests/apps/api/v1/test_mixins.py
@@ -2,20 +2,20 @@ from django.contrib.gis.geos import Point
 from django.test import TestCase
 from rest_framework import serializers
 
-from signals.apps.api.generics.mixins import NearAmsterdamValidatorMixin
+from signals.apps.api.generics.mixins import WithinBoundingBoxValidatorMixin
 
-IN_AMSTERDAM = (4.898466, 52.361585)
-OUTSIDE_AMSTERDAM = tuple(reversed(IN_AMSTERDAM))
+IN_THE_NETHERLANDS = (4.898466, 52.361585)
+OUTSIDE_THE_NETHERLANDS = tuple(reversed(IN_THE_NETHERLANDS))
 
 
-class TestNearAmsterdamValidatorMixin(TestCase):
+class TestWithinBoundingBoxValidatorMixin(TestCase):
     def test_validate_geometrie(self):
         # note this test bypasses the API
-        correct = Point(IN_AMSTERDAM)
-        wrong = Point(OUTSIDE_AMSTERDAM)
+        correct = Point(IN_THE_NETHERLANDS)
+        wrong = Point(OUTSIDE_THE_NETHERLANDS)
 
         # check that valid data is returned, and wrong data rejected
-        v = NearAmsterdamValidatorMixin()
+        v = WithinBoundingBoxValidatorMixin()
         self.assertEqual(correct, v.validate_geometrie(correct))
 
         with self.assertRaises(serializers.ValidationError):
