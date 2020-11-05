7d8b4b0a1b31bc206a93ff9eb2c37f8b03eaf23f
David van Buiten <david.vanbuiten+github@gmail.com>
2020-11-05 14:31:26 +0100

    Added management commands to add dummy data + small changes to factories + unit tests

---
+++ api/app/signals/apps/signals/management/commands/dummy_departments.py
@@ -0,0 +1,23 @@
+from django.core.management import BaseCommand
+
+from signals.apps.signals.factories import DepartmentFactory
+
+
+class Command(BaseCommand):
+    default_to_create = 1
+    min_to_create = 1
+    max_to_create = 10
+
+    def add_arguments(self, parser):
+        parser.add_argument('--to-create', type=int, help='Total random Departments to create '
+                                                          f'(max {self.max_to_create}). '
+                                                          f'Default {self.default_to_create}.')
+
+    def handle(self, *args, **options):
+        to_create = int(options['to_create'] or self.default_to_create)
+        if not 0 < to_create <= 10:
+            self.stderr.write(f'The to create option must be an integer from {self.min_to_create} to '
+                              f'{self.max_to_create}, {to_create} given')
+        else:
+            DepartmentFactory.create_batch(to_create)
+            self.stdout.write(f'Created {to_create} random Department(s)')
