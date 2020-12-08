efe4f7c6fa9404ab77c8c9d0ef41f0c95ff299d7
CBuiVNG <info@iloki.nl>
2020-12-09 00:24:20 +0100

    implement open() and close() to init session.

--- api/app/signals/apps/email_integrations/custom_backends/rest_email_endpoint.py
+++ api/app/signals/apps/email_integrations/custom_backends/rest_email_endpoint.py
@@ -10,13 +10,17 @@ class RestEmailBackend(BaseEmailBackend):
     def __init__(self, fail_silently=False, **kwargs):
         super(RestEmailBackend, self).__init__(fail_silently)
         self.init_kwargs = kwargs
+        self.session = None
+        self.cert = None
+        if hasattr(settings, 'EMAIL_REST_ENDPOINT_CLIENT_CERT') and hasattr(settings, 'EMAIL_REST_ENDPOINT_CLIENT_KEY'): # noqa
+            self.cert = (settings.EMAIL_REST_ENDPOINT_CLIENT_CERT, settings.EMAIL_REST_ENDPOINT_CLIENT_KEY)
 
-    def _send_email_rest_api(self, session, message):
+    def _send_email_rest_api(self, message_attributes):
         try:
-            response = session.post(
+            response = self.session.post(
                 url=settings.EMAIL_REST_ENDPOINT,
                 headers={'Content-type': 'application/json', 'Accept': 'text/plain'},
-                data=json.dumps(message),
+                data=json.dumps(message_attributes),
                 timeout=settings.EMAIL_REST_ENDPOINT_TIMEOUT,
             )
             response.raise_for_status()
@@ -27,15 +31,19 @@ class RestEmailBackend(BaseEmailBackend):
                 return False
         return True
 
+    def open(self):
+        self.session = Session()
+        self.session.cert = self.cert
+
+    def close(self):
+        self.session.close()
+        self.session = None
+
     def send_messages(self, email_messages):
         messages_sent = 0
-        s = Session()
-        # init session, connection using client and server side certificate is expensive
-        if hasattr(settings, 'EMAIL_REST_ENDPOINT_CLIENT_CERT') and hasattr(settings, 'EMAIL_REST_ENDPOINT_CLIENT_KEY'): # noqa
-            s.cert = (settings.EMAIL_REST_ENDPOINT_CLIENT_CERT, settings.EMAIL_REST_ENDPOINT_CLIENT_KEY)
 
         for chunk in chunked(email_messages, settings.CELERY_EMAIL_CHUNK_SIZE):
-            chunk_messages = [email_to_dict(msg) for msg in chunk]
-            if self._send_email_rest_api(s, chunk_messages):
+            message_attributes = [email_to_dict(msg) for msg in chunk]
+            if self._send_email_rest_api(message_attributes):
                 messages_sent += 1
         return messages_sent
