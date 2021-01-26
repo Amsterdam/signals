6249032600ba040b136cccf543fceaaa3a6ab7ed
Thijs Coenen <458483+tcoenen@users.noreply.github.com>
2021-01-26 15:20:09 +0100

    SIG-3495 Demonstrate problem in PDF permissions.

--- api/app/tests/apps/api/v1/test_pdf.py
+++ api/app/tests/apps/api/v1/test_pdf.py
@@ -1,4 +1,6 @@
-from signals.apps.signals.factories import SignalFactory
+from django.contrib.auth.models import Permission
+
+from signals.apps.signals.factories import CategoryFactory, DepartmentFactory, SignalFactory
 from tests.test import SIAReadWriteUserMixin, SignalsBaseApiTestCase
 
 
@@ -7,6 +9,7 @@ class TestPDFView(SIAReadWriteUserMixin, SignalsBaseApiTestCase):
         self.signal = SignalFactory.create()
 
     def test_get_pdf(self):
+        self.sia_read_write_user.user_permissions.add(Permission.objects.get(codename='sia_can_view_all_categories'))
         self.client.force_authenticate(user=self.sia_read_write_user)
         url = '/signals/v1/private/signals/{}/pdf'.format(self.signal.pk)
         response = self.client.get(path=url)
@@ -31,3 +34,34 @@ class TestPDFView(SIAReadWriteUserMixin, SignalsBaseApiTestCase):
         response = self.client.get(path=url)
 
         self.assertEqual(response.status_code, 401)
+
+
+class TestPDFPermissions(SIAReadWriteUserMixin, SignalsBaseApiTestCase):
+    # Accessing PDFs must follow the same access rules as the signals.
+    # Specifically: rules around categories and departments must be followed.
+    detail_endpoint = '/signals/v1/private/signals/{}'
+    pdf_endpoint = '/signals/v1/private/signals/{}/pdf'
+
+    def setUp(self):
+        self.department = DepartmentFactory.create()
+        self.category = CategoryFactory.create(departments=[self.department])
+        self.signal = SignalFactory.create(category_assignment__category=self.category)
+
+    def test_assumptions(self):
+        self.client.force_authenticate(user=self.sia_read_write_user)
+        url = self.detail_endpoint.format(self.signal.pk)
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 403)
+
+    def test_cannot_access_without_proper_department(self):
+        self.client.force_authenticate(user=self.sia_read_write_user)
+        url = self.pdf_endpoint.format(self.signal.pk)
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 403)
+
+    def test_can_access_with_proper_department(self):
+        self.client.force_authenticate(user=self.sia_read_write_user)
+        self.sia_read_write_user.profile.departments.add(self.department)
+        url = self.pdf_endpoint.format(self.signal.pk)
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
