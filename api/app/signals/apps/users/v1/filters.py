2801a0dc2efc28f780c391fdc124b9ff5ea7756f
Bart Jeukendrup <bart@jeukendrup.nl>
2021-02-09 16:08:07 +0100

    Add ability to filter user list on department code

--- api/app/signals/apps/users/v1/filters.py
+++ api/app/signals/apps/users/v1/filters.py
@@ -3,10 +3,15 @@ from django.db.models.functions import Lower
 from django_filters.rest_framework import FilterSet, filters
 
 from signals.apps.api.generics.filters import OrderingExtraKwargsFilter
+from signals.apps.signals.models import Department
 
 
 def _get_group_queryset():
     return Group.objects.all()
+
+
+def _get_department_queryset():
+    return Department.objects.all()
 
 
 class UserFilterSet(FilterSet):
@@ -17,6 +22,10 @@ class UserFilterSet(FilterSet):
     role = filters.ModelMultipleChoiceFilter(queryset=_get_group_queryset(),
                                              to_field_name='name',
                                              field_name='groups__name')
+
+    profile_department_code = filters.ModelMultipleChoiceFilter(queryset=_get_department_queryset(),
+                                                                to_field_name='code',
+                                                                field_name='profile__departments__code')
 
     order = OrderingExtraKwargsFilter(
         fields=(
