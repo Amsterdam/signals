1c10a34fe82d98cbc53204323f097649ead401ef
Thijs Coenen <458483+tcoenen@users.noreply.github.com>
2021-02-17 23:56:33 +0100

    SIG-3509 Add deadline calculation plus tests

---
+++ api/app/signals/apps/services/domain/deadlines.py
@@ -0,0 +1,45 @@
+"""
+Calculate deadline for solving complaints.
+
+- supports calculations in working days or just normal calendar days
+- supports scaling a deadline with an arbitrary factor (used to find complaints
+  that overrun their deadline by a factor of 3)
+- workdays are defined as Monday through Friday, ignoring festivities
+"""
+from datetime import datetime, time, timedelta
+
+
+class DeadlineCalculationService:
+    @staticmethod
+    def get_start(created_at):
+        # When promise to complainant is given in working days and the complaint
+        # received during the weekend, work will only start on Monday. The
+        # deadline calculation needs to take this into account.
+        weekday = created_at.date().weekday()
+
+        if weekday > 4:
+            return datetime.combine(created_at.date(), time(0, 0, 0)) + timedelta(days=7-weekday)
+        return created_at
+
+    @staticmethod
+    def get_end(start, n_days, use_calendar_days, factor):
+        # When promise to complainant is given in working days we cannot just add
+        # the number of working days to the day the complaint was received. Non-
+        # working days need to be taken into account.
+        n_days = n_days * factor
+        n_weeks, days_remaining = divmod(n_days, 5)  # 5 days per workweek
+
+        deadline_weekday = start.date().weekday() + days_remaining
+        if deadline_weekday > 4:
+            # Deadline would fall in weekend and we have to shift by 2 extra days
+            return start + timedelta(days=(n_weeks * 7) + days_remaining + 2)
+        return start + timedelta(days=(n_weeks * 7) + days_remaining)
+
+    @staticmethod
+    def get_deadline(created_at, n_days, use_calendar_days, factor=1):
+        if use_calendar_days:
+            return created_at + timedelta(days=(n_days * factor))
+        start = DeadlineCalculationService.get_start(created_at)
+        deadline = DeadlineCalculationService.get_end(start, n_days, use_calendar_days, factor)
+
+        return deadline
---
+++ api/app/tests/apps/services/domain/test_deadlins.py
@@ -0,0 +1,80 @@
+from datetime import datetime
+
+from django.test import TestCase
+
+from signals.apps.services.domain.deadlines import DeadlineCalculationService
+
+FULL_TEST_CASES = [
+    # Columns below: created_at, n_days, use_calendar_days, factor, deadline
+    # One working day throughout week
+    (datetime(2021, 2, 15, 12, 0, 0), 1, False, 1, datetime(2021, 2, 16, 12, 0, 0)),  # Monday 12:00 -> Tuesday 12:00
+    (datetime(2021, 2, 16, 12, 0, 0), 1, False, 1, datetime(2021, 2, 17, 12, 0, 0)),  # Tuesday 12:00 -> Wednesday 12:00
+    (datetime(2021, 2, 19, 12, 0, 0), 1, False, 1, datetime(2021, 2, 22, 12, 0, 0)),  # Friday 12:00 -> Monday 12:00
+    (datetime(2021, 2, 20, 12, 0, 0), 1, False, 1, datetime(2021, 2, 23, 0, 0, 0)),  # Saturday 12:00 -> Tuesday 00:00
+    (datetime(2021, 2, 21, 12, 0, 0), 1, False, 1, datetime(2021, 2, 23, 0, 0, 0)),  # Sunday 12:00 -> Tuesday 00:00
+
+    # One working day throughout week, factor 3
+    (datetime(2021, 2, 15, 12, 0, 0), 1, False, 3, datetime(2021, 2, 18, 12, 0, 0)),  # Monday 12:00 -> Thursday 12:00
+    (datetime(2021, 2, 16, 12, 0, 0), 1, False, 3, datetime(2021, 2, 19, 12, 0, 0)),  # Tuesday 12:00 -> Friday 12:00
+    (datetime(2021, 2, 19, 12, 0, 0), 1, False, 3, datetime(2021, 2, 24, 12, 0, 0)),  # Friday 12:00 -> Wednesday 12:00
+    (datetime(2021, 2, 20, 12, 0, 0), 1, False, 3, datetime(2021, 2, 25, 0, 0, 0)),  # Saturday 12:00 -> Thursday 00:00
+    (datetime(2021, 2, 21, 12, 0, 0), 1, False, 3, datetime(2021, 2, 25, 0, 0, 0)),  # Sunday 12:00 -> Thursday 00:00
+
+    # One calendar day throughout the week
+    (datetime(2021, 2, 15, 12, 0, 0), 1, True, 1, datetime(2021, 2, 16, 12, 0, 0)),  # Monday 12:00 -> Tuesday 12:00
+    (datetime(2021, 2, 16, 12, 0, 0), 1, True, 1, datetime(2021, 2, 17, 12, 0, 0)),  # Tuesday 12:00 -> Wednesday 12:00
+    (datetime(2021, 2, 19, 12, 0, 0), 1, True, 1, datetime(2021, 2, 20, 12, 0, 0)),  # Friday 12:00 -> Monday 12:00
+    (datetime(2021, 2, 20, 12, 0, 0), 1, True, 1, datetime(2021, 2, 21, 12, 0, 0)),  # Saturday 12:00 -> Saturday 12:00
+    (datetime(2021, 2, 21, 12, 0, 0), 1, True, 1, datetime(2021, 2, 22, 12, 0, 0)),  # Sunday 12:00 -> Monday 12:00
+
+    # One calendar day throughout the week, factor 3
+    (datetime(2021, 2, 15, 12, 0, 0), 1, True, 3, datetime(2021, 2, 18, 12, 0, 0)),  # Monday 12:00 -> Thursday 12:00
+    (datetime(2021, 2, 16, 12, 0, 0), 1, True, 3, datetime(2021, 2, 19, 12, 0, 0)),  # Tuesday 12:00 -> Friday 12:00
+    (datetime(2021, 2, 19, 12, 0, 0), 1, True, 3, datetime(2021, 2, 22, 12, 0, 0)),  # Friday 12:00 -> Monday 12:00
+    (datetime(2021, 2, 20, 12, 0, 0), 1, True, 3, datetime(2021, 2, 23, 12, 0, 0)),  # Saturday 12:00 -> Tuesday 12:00
+    (datetime(2021, 2, 21, 12, 0, 0), 1, True, 3, datetime(2021, 2, 24, 12, 0, 0)),  # Sunday 12:00 -> Wednesday 12:00
+
+    # Two working days throughout week
+    (datetime(2021, 2, 15, 12, 0, 0), 2, False, 1, datetime(2021, 2, 17, 12, 0, 0)),  # Monday 12:00 -> Wednesday 12:00
+    (datetime(2021, 2, 18, 12, 0, 0), 2, False, 1, datetime(2021, 2, 22, 12, 0, 0)),  # Thursday 12:00 -> Monday 12:00
+    (datetime(2021, 2, 19, 12, 0, 0), 2, False, 1, datetime(2021, 2, 23, 12, 0, 0)),  # Friday 12:00 -> Tuesday 12:00
+    (datetime(2021, 2, 20, 12, 0, 0), 2, False, 1, datetime(2021, 2, 24, 0, 0, 0)),  # Saturday 12:00 -> Wednesday 00:00
+
+    # Three working days throughout week
+    (datetime(2021, 2, 15, 12, 0, 0), 3, False, 1, datetime(2021, 2, 18, 12, 0, 0)),  # Monday 12:00 -> Thursday 12:00
+    (datetime(2021, 2, 18, 12, 0, 0), 3, False, 1, datetime(2021, 2, 23, 12, 0, 0)),  # Thursday 12:00 -> Tuesday 12:00
+    (datetime(2021, 2, 19, 12, 0, 0), 3, False, 1, datetime(2021, 2, 24, 12, 0, 0)),  # Friday 12:00 -> Wednesday 12:00
+    (datetime(2021, 2, 20, 12, 0, 0), 3, False, 1, datetime(2021, 2, 25, 0, 0, 0)),  # Saturday 12:00 -> Thursday 00:00
+    (datetime(2021, 2, 21, 12, 0, 0), 3, False, 1, datetime(2021, 2, 25, 0, 0, 0)),  # Sunday 12:00 -> Thursday 00:00
+
+    # Ten working days
+    (datetime(2021, 2, 15, 12, 0, 0), 10, False, 1, datetime(2021, 3, 1, 12, 0, 0)),  # Monday 12:00 -> Monday 12:00
+    (datetime(2021, 2, 19, 12, 0, 0), 10, False, 1, datetime(2021, 3, 5, 12, 0, 0)),  # Friday 12:00 -> Friday 12:00
+
+    # Eleven working days
+    (datetime(2021, 2, 15, 12, 0, 0), 11, False, 1, datetime(2021, 3, 2, 12, 0, 0)),  # Monday 12:00 -> Tuesday 12:00
+    (datetime(2021, 2, 19, 12, 0, 0), 11, False, 1, datetime(2021, 3, 8, 12, 0, 0)),  # Friday 12:00 -> Monday 12:00
+]
+
+
+class TestDeadlineCalculationService(TestCase):
+    def test_get_start(self):
+        TEST_CASES = [
+            (datetime(2021, 2, 15, 12, 0, 0), datetime(2021, 2, 15, 12, 0, 0)),  # monday 12:00 -> tuesday 12:00
+            (datetime(2021, 2, 16, 12, 0, 0), datetime(2021, 2, 16, 12, 0, 0)),  # tuesday 12:00 -> tuesday 12:00
+            (datetime(2021, 2, 20, 12, 0, 0), datetime(2021, 2, 22, 0, 0, 0)),  # saturday 12:00 -> monday 00:00
+            (datetime(2021, 2, 21, 12, 0, 0), datetime(2021, 2, 22, 0, 0, 0)),  # sunday 12:00 -> monday 00:00
+        ]
+        for created_at, start in TEST_CASES:
+            calculated_start = DeadlineCalculationService.get_start(created_at)
+            self.assertEqual(calculated_start, start)
+
+    def test_get_end(self):
+        pass
+
+    def test_get_deadline(self):
+        # Go through testcases check that we get the correct answers.
+        for created_at, n_days, use_calendar_days, factor, deadline in FULL_TEST_CASES:
+            calculated_deadline = DeadlineCalculationService.get_deadline(
+                created_at, n_days, use_calendar_days, factor)
+            self.assertEqual(calculated_deadline, deadline)
