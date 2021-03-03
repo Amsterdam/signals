0111dfcfa55e36c882c20955a03d6ef9628d921a
Thijs Coenen <458483+tcoenen@users.noreply.github.com>
2021-03-03 14:26:53 +0100

    SIG-3439 fix conflicting migrations after rebase

--- api/app/signals/apps/signals/migrations/0135_json_fields.py
+++ api/app/signals/apps/signals/migrations/0135_json_fields.py
@@ -4,7 +4,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('signals', '0133_new_subcategories'),
+        ('signals', '0134_deadlines'),
     ]
 
     operations = [
