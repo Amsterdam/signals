7fbaa16a083cff5aa14bfa627bdbeb33d5d58a6a
David van Buiten <david.vanbuiten@gmail.com>
2021-03-03 08:03:19 +0100

    Moved imnports to the top of the file instead of importing them locally

--- api/app/signals/apps/email_integrations/utils.py
+++ api/app/signals/apps/email_integrations/utils.py
@@ -1,16 +1,12 @@
+from signals.apps.feedback.models import Feedback
+from signals.apps.feedback.utils import get_feedback_urls
 from signals.apps.signals.models import Signal
 
 
 def _create_feedback_and_mail_context(signal: Signal):
     """
     Util functions to create the feedback object and create the context needed for the mail
-
-    :param signal:
-    :return:
     """
-    from signals.apps.feedback.models import Feedback
-    from signals.apps.feedback.utils import get_feedback_urls
-
     feedback = Feedback.actions.request_feedback(signal)
     positive_feedback_url, negative_feedback_url = get_feedback_urls(feedback)
     return {
