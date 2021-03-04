b5531a70c804039cec0f0b793ae3566a546a79b0
CBuiVNG <info@iloki.nl>
2021-03-04 09:57:12 +0100

    add fake serializer to avoid assertions, filter out GEANNULEERD and VERZOEK_TOT_HEROPENEN in public map endpoint

--- api/app/signals/apps/api/v1/serializers/__init__.py
+++ api/app/signals/apps/api/v1/serializers/__init__.py
@@ -15,6 +15,7 @@ from signals.apps.api.v1.serializers.departments import (
     PrivateDepartmentSerializerDetail,
     PrivateDepartmentSerializerList
 )
+from signals.apps.api.v1.serializers.empty import PublicEmptySerializer
 from signals.apps.api.v1.serializers.expression import ExpressionContextSerializer
 from signals.apps.api.v1.serializers.question import PublicQuestionSerializerDetail
 from signals.apps.api.v1.serializers.signal import (
@@ -54,5 +55,6 @@ __all__ = [
     'PrivateCategorySerializer',
     'PublicQuestionSerializerDetail',
     'AbridgedChildSignalSerializer',
-    'ExpressionContextSerializer'
+    'ExpressionContextSerializer',
+    'PublicEmptySerializer'
 ]
---
+++ api/app/signals/apps/api/v1/serializers/empty.py
@@ -0,0 +1,5 @@
+from rest_framework import serializers
+
+
+class PublicEmptySerializer(serializers.Serializer):
+    pass
