1bccfaa907655cd6fef894b7001f66eb9adcb3dc
David van Buiten <david.vanbuiten@gmail.com>
2021-05-12 15:46:45 +0200

    Auto create child signals per selected container

---
+++ api/app/signals/apps/services/domain/auto_create_children.py
@@ -0,0 +1,115 @@
+# SPDX-License-Identifier: MPL-2.0
+# Copyright (C) 2021 Gemeente Amsterdam
+import logging
+
+from django.conf import settings
+
+from signals.apps.signals.models import Category, Signal
+from signals.apps.signals.workflow import GEMELD
+
+log = logging.getLogger(__name__)
+
+
+class AutoCreateChildrenService:
+    """
+    !!! This will be refactored when the "uitvraag" will be moved to the API !!!
+
+    Automatically create child signals if:
+        - The feature flag "AUTOMATICALLY_CREATE_CHILD_SIGNALS_PER_CONTAINER" is enabled
+        - A signal is not a parent or a child signal
+        - A signal has the status "GEMELD" ("m")
+        - A signal belongs to the sub category "container-is-vol", "container-voor-papier-is-vol", "container-voor-plastic-afval-is-vol" or "container-glas-vol"
+        - A signal must contain at least 2 or more containers
+        
+        For now this is only used to create child signals when multiple containers (more than 1) are selected.
+    """  # noqa
+
+    _type_2_category_slug = {
+        'rest': 'container-is-vol',
+        'papier': 'container-voor-papier-is-vol',
+        'plastic': 'container-voor-plastic-afval-is-vol',
+        'glas': 'container-glas-vol',
+        'default': 'container-is-vol',
+    }
+
+    @staticmethod
+    def _translate_type_2_category(container_type):
+        slug = AutoCreateChildrenService._type_2_category_slug['default']
+        if container_type.lower() in AutoCreateChildrenService._type_2_category_slug:
+            slug = AutoCreateChildrenService._type_2_category_slug[container_type.lower()]
+        return Category.objects.get(slug=slug)
+
+    @staticmethod
+    def _container_data(signal):
+        answers = []
+        if signal.extra_properties:
+            for extra_property in signal.extra_properties:
+                if extra_property.get('id', '').lower() == 'extra_container':
+                    for answer in extra_property['answer']:
+                        answers.append(answer)
+        return answers
+
+    @staticmethod
+    def _is_applicable(signal):
+        if signal.is_parent or signal.is_child or signal.status.state != GEMELD:
+            return False
+
+        category_slug = signal.category_assignment.category.slug
+        if category_slug not in set(AutoCreateChildrenService._type_2_category_slug.values()):
+            return False
+
+        if len(AutoCreateChildrenService._container_data(signal)) < 2:
+            return False
+
+        return True
+
+    @staticmethod
+    def _create_child(signal, container_data):
+        category = AutoCreateChildrenService._translate_type_2_category(container_data['type'])
+
+        signal_data = {
+            'text': signal.text,
+            'text_extra': signal.text_extra,
+            'incident_date_start': signal.incident_date_start,
+            'parent': signal,
+            'extra_properties': [
+                {
+                    'id': 'extra_container',
+                    'label': 'Container(s)',
+                    'answer': [
+                        container_data
+                    ],
+                    'category_url': category.get_absolute_url()
+                }
+            ]
+        }
+        location_data = {
+            'geometrie': signal.location.geometrie,  # TODO get the location of the selected object
+        }
+        status_data = {
+            'state': GEMELD,
+        }
+        category_data = {
+            'category': category,
+        }
+        reporter_data = {
+            'email': signal.reporter.email,
+            'phone': signal.reporter.phone,
+        }
+
+        # Create the child signal using the actions manager
+        Signal.actions.create_initial(signal_data, location_data, status_data, category_data, reporter_data)
+
+    @staticmethod
+    def run(signal_id):
+        if not settings.FEATURE_FLAGS.get('AUTOMATICALLY_CREATE_CHILD_SIGNALS_PER_CONTAINER', False):
+            return
+
+        signal = Signal.objects.get(pk=signal_id)
+
+        if not AutoCreateChildrenService._is_applicable(signal=signal):
+            return
+
+        containers_data = AutoCreateChildrenService._container_data(signal)
+        for container_data in containers_data:
+            AutoCreateChildrenService._create_child(signal, container_data)
---
+++ api/app/tests/apps/services/domain/auto_create_children.py
@@ -0,0 +1,114 @@
+# SPDX-License-Identifier: MPL-2.0
+# Copyright (C) 2021 Gemeente Amsterdam
+import copy
+
+from django.test import TestCase, override_settings
+
+from signals.apps.services.domain.auto_create_children import AutoCreateChildrenService
+from signals.apps.signals.factories import SignalFactory
+from signals.apps.signals.models import Category
+
+
+@override_settings(FEATURE_FLAGS={
+    'API_SEARCH_ENABLED': False,
+    'SEARCH_BUILD_INDEX': False,
+    'API_DETERMINE_STADSDEEL_ENABLED': False,
+    'API_FILTER_EXTRA_PROPERTIES': False,
+    'API_TRANSFORM_SOURCE_BASED_ON_REPORTER': False,
+    'API_TRANSFORM_SOURCE_IF_A_SIGNAL_IS_A_CHILD': False,
+    'TASK_UPDATE_CHILDREN_BASED_ON_PARENT': False,
+    'AUTOMATICALLY_CREATE_CHILD_SIGNALS_PER_CONTAINER': True,  # Interested in this functionality
+})
+class TestAutoCreateChildrenServiceService(TestCase):
+    def setUp(self):
+        self.category_container_is_vol = Category.objects.get(slug='container-is-vol', parent__isnull=False)
+        self.extra_properties = [{
+            "id": "extra_container",
+            "label": "Container(s)",
+            "answer": [
+                {"id": "ID001", "type": "Rest", "description": "Restafval container"},
+                {"id": "ID002", "type": "Plastic", "description": "Plasticafval container"},
+                {"id": "ID003", "type": "Papier", "description": "Papierafval container"},
+                {"id": "ID0004", "type": "Unknown", "description": "Unknown container"},
+            ],
+            "category_url": "/signals/v1/public/terms/categories/afval/sub_categories/container-is-vol"
+        }]
+
+    def test_create_children(self):
+        extra_properties = copy.deepcopy(self.extra_properties)
+        signal = SignalFactory.create(extra_properties=extra_properties,
+                                      category_assignment__category=self.category_container_is_vol)
+
+        AutoCreateChildrenService.run(signal_id=signal.pk)
+
+        signal.refresh_from_db()
+
+        self.assertTrue(signal.is_parent)
+        self.assertEqual(signal.children.count(), 4)
+
+    def test_no_containers_selected(self):
+        signal = SignalFactory.create(extra_properties=None,
+                                      category_assignment__category=self.category_container_is_vol)
+
+        AutoCreateChildrenService.run(signal_id=signal.pk)
+
+        signal.refresh_from_db()
+
+        self.assertFalse(signal.is_parent)
+        self.assertEqual(signal.children.count(), 0)
+
+    def test_one_container_selected(self):
+        extra_properties = copy.deepcopy(self.extra_properties)
+        extra_properties[0]['answer'] = [extra_properties[0]['answer'][0]]
+
+        signal = SignalFactory.create(extra_properties=None,
+                                      category_assignment__category=self.category_container_is_vol)
+
+        AutoCreateChildrenService.run(signal_id=signal.pk)
+
+        signal.refresh_from_db()
+
+        self.assertFalse(signal.is_parent)
+        self.assertEqual(signal.children.count(), 0)
+
+    def test_multiple_extra_properties_for_containers(self):
+        extra_properties = copy.deepcopy(self.extra_properties)
+        extra_properties.append(copy.deepcopy(extra_properties[0]))
+
+        signal = SignalFactory.create(extra_properties=extra_properties,
+                                      category_assignment__category=self.category_container_is_vol)
+
+        AutoCreateChildrenService.run(signal_id=signal.pk)
+
+        signal.refresh_from_db()
+
+        self.assertTrue(signal.is_parent)
+        self.assertEqual(signal.children.count(), 8)
+
+    def test_signal_wrong_state(self):
+        extra_properties = copy.deepcopy(self.extra_properties)
+        signal = SignalFactory.create(extra_properties=extra_properties,
+                                      category_assignment__category=self.category_container_is_vol,
+                                      status__state='i')
+
+        AutoCreateChildrenService.run(signal_id=signal.pk)
+
+        signal.refresh_from_db()
+
+        self.assertFalse(signal.is_parent)
+        self.assertEqual(signal.children.count(), 0)
+
+    @override_settings(FEATURE_FLAGS={
+        'AUTOMATICALLY_CREATE_CHILD_SIGNALS_PER_CONTAINER': False,  # Interested in this functionality
+    })
+    def test_signal_feature_disabled(self):
+        extra_properties = copy.deepcopy(self.extra_properties)
+        signal = SignalFactory.create(extra_properties=extra_properties,
+                                      category_assignment__category=self.category_container_is_vol)
+
+        AutoCreateChildrenService.run(signal_id=signal.pk)
+
+        signal.refresh_from_db()
+
+        self.assertFalse(signal.is_parent)
+        self.assertEqual(signal.children.count(), 0)
