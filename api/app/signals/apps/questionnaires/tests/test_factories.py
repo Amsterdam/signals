ea5d446b21f7fd5d113ff603369e9161d16ab380
Thijs Coenen <458483+tcoenen@users.noreply.github.com>
2021-05-31 16:51:49 +0200

    SIG-3769 updated models + simple factories

---
+++ api/app/signals/apps/questionnaires/tests/test_factories.py
@@ -0,0 +1,45 @@
+# SPDX-License-Identifier: MPL-2.0
+# Copyright (C) 2021 Gemeente Amsterdam
+from django.test import TestCase
+
+from signals.apps.questionnaires.factories import (
+    AnswerFactory,
+    QuestionFactory,
+    QuestionnaireFactory,
+    SessionFactory
+)
+from signals.apps.questionnaires.models import Answer, Question, Questionnaire, Session
+
+
+class TestFactories(TestCase):
+    def test_answer_factory(self):
+        AnswerFactory.create()
+
+        self.assertEqual(Answer.objects.count(), 1)
+        self.assertEqual(Session.objects.count(), 1)
+        self.assertEqual(Questionnaire.objects.count(), 1)
+        self.assertEqual(Question.objects.count(), 1)
+
+    def test_session_factory(self):
+        SessionFactory.create()
+
+        self.assertEqual(Answer.objects.count(), 0)
+        self.assertEqual(Session.objects.count(), 1)
+        self.assertEqual(Questionnaire.objects.count(), 1)
+        self.assertEqual(Question.objects.count(), 1)
+
+    def test_questionnaire_factory(self):
+        QuestionnaireFactory.create()
+
+        self.assertEqual(Answer.objects.count(), 0)
+        self.assertEqual(Session.objects.count(), 0)
+        self.assertEqual(Questionnaire.objects.count(), 1)
+        self.assertEqual(Question.objects.count(), 1)
+
+    def test_question_factory(self):
+        QuestionFactory.create()
+
+        self.assertEqual(Answer.objects.count(), 0)
+        self.assertEqual(Session.objects.count(), 0)
+        self.assertEqual(Questionnaire.objects.count(), 0)
+        self.assertEqual(Question.objects.count(), 1)
