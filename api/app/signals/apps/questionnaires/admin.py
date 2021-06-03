a5163ea20acdf584bb9d8b079701cc7d0b4feba8
David van Buiten <david.vanbuiten@gmail.com>
2021-06-03 16:15:59 +0200

    Some small admin changes

--- api/app/signals/apps/questionnaires/admin.py
+++ api/app/signals/apps/questionnaires/admin.py
@@ -6,8 +6,9 @@ from signals.apps.questionnaires.models import Question, Questionnaire, Session
 
 
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ('key', 'uuid', 'field_type', 'payload', 'required', 'created_at',)
+    fields = ('key', 'uuid', 'field_type', 'payload', 'required', 'root', 'created_at',)
     readonly_fields = ('uuid', 'created_at',)
+    raw_id_fields = ('root',)
     list_display = ('key', 'uuid', 'field_type', 'created_at',)
     ordering = ('-created_at',)
     list_per_page = 20
@@ -20,6 +21,7 @@ admin.site.register(Question, QuestionAdmin)
 class QuestionnaireAdmin(admin.ModelAdmin):
     fields = ('uuid', 'first_question', 'created_at',)
     readonly_fields = ('uuid', 'created_at',)
+    raw_id_fields = ('first_question',)
     list_display = ('uuid', 'created_at',)
     ordering = ('-created_at',)
     list_per_page = 20
@@ -30,14 +32,17 @@ admin.site.register(Questionnaire, QuestionnaireAdmin)
 
 
 class SessionAdmin(admin.ModelAdmin):
-    fields = ('uuid', 'started_at', 'submit_before', 'duration', 'created_at',)
-    readonly_fields = ('uuid', 'started_at', 'submit_before', 'duration', 'created_at',)
-    list_display = ('uuid', 'started_at', 'submit_before', 'created_at',)
+    fields = ('uuid', 'questionnaire', 'started_at', 'duration', 'submit_before', 'frozen', 'created_at',)
+    readonly_fields = ('uuid', 'started_at', 'frozen', 'created_at',)
+    raw_id_fields = ('questionnaire',)
+    list_display = ('uuid', 'questionnaire', 'started_at', 'submit_before', 'created_at',)
     ordering = ('-created_at',)
     list_per_page = 20
     list_select_related = True
 
     def has_delete_permission(self, request, obj=None):
+        if obj and not obj.started_at:
+            return True
         return False
 
 
