baa620e45fd0d58227705b1e550322309ed0fd8e
David van Buiten <david.vanbuiten@gmail.com>
2021-06-04 11:02:24 +0200

    Added 2 seperate swagger files, 1 for public and 1 for private endpoints of the Questionnaires app + added 2 url paths to access the swagger files

--- api/app/signals/apps/questionnaires/urls.py
+++ api/app/signals/apps/questionnaires/urls.py
@@ -1,6 +1,8 @@
 # SPDX-License-Identifier: MPL-2.0
 # Copyright (C) 2021 Gemeente Amsterdam
+import os
 from django.urls import include, path
+from django.views.generic.base import TemplateView
 
 from signals.apps.api.routers import SignalsRouterVersion1
 from signals.apps.questionnaires.views import (
@@ -23,4 +25,18 @@ router.register(r'public/qa/sessions', PublicSessionViewSet, basename='public-se
 
 urlpatterns = [
     path('', include((router.urls, 'signals.apps.questionnaires'), namespace='questionnaires')),
+
+    # Swagger documentation for the public endpoints
+    path('public/qa/swagger/openapi.yaml',
+         TemplateView.as_view(template_name='questionnaires/swagger/public_openapi.yaml',
+                              extra_context={'schema_url': 'openapi-schema',
+                                             'global_api_root': os.getenv('global_api_root', None)}),
+         name='swagger-ui'),
+
+    # Swagger documentation for the private endpoints
+    path('private/qa/swagger/openapi.yaml',
+         TemplateView.as_view(template_name='questionnaires/swagger/private_openapi.yaml',
+                              extra_context={'schema_url': 'openapi-schema',
+                                             'global_api_root': os.getenv('global_api_root', None)}),
+         name='swagger-ui'),
 ]
