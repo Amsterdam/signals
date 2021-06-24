feec5c794e4ec3623c8981cc2c81e9438168e73e
David van Buiten <david.vanbuiten@gmail.com>
2021-06-24 11:06:43 +0200

    Moved reporting tests to the reporting app + removed unused view/serializer

--- api/app/signals/apps/reporting/serializers/__init__.py
+++
@@ -1,7 +0,0 @@
-# SPDX-License-Identifier: MPL-2.0
-# Copyright (C) 2019 - 2021 Gemeente Amsterdam
-from signals.apps.reporting.serializers.horeca_csv import HorecaCSVExportSerializer
-
-__all__ = (
-    'HorecaCSVExportSerializer',
-)
--- api/app/signals/apps/reporting/views/__init__.py
+++
@@ -1,7 +0,0 @@
-# SPDX-License-Identifier: MPL-2.0
-# Copyright (C) 2019 - 2021 Gemeente Amsterdam
-from signals.apps.reporting.views.horeca_csv import HorecaCSVExportViewSet
-
-__all__ = (
-    'HorecaCSVExportViewSet',
-)
