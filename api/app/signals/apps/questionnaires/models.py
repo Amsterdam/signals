9465d8bc143241367fbe5fb4d5a91640e5aabaaa
Thijs Coenen <458483+tcoenen@users.noreply.github.com>
2021-07-21 08:56:44 +0200

    SIG-3904 missing files

--- api/app/signals/apps/questionnaires/models.py
+++ api/app/signals/apps/questionnaires/models.py
@@ -54,7 +54,7 @@ class Edge(models.Model):  # aka edge list / adjacency list
     question = models.ForeignKey('Question', on_delete=models.CASCADE, related_name='+')
     next_question = models.ForeignKey('Question', on_delete=models.CASCADE, related_name='+')
 
-    order = models.IntegerField(default=0)  # Default is not to care about order of edges.
+    order = models.IntegerField(default=0)  # Default is order of creation, can be overriden.
     payload = models.JSONField(null=True, blank=True)  # <- validate using question field when saving
 
     class Meta:
