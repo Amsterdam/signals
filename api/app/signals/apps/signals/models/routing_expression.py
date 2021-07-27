0f5de56b0cc3ad968a00bb90370efa5e1cd99b7a
ginokok1996 <ginokok1996@hotmail.com>
2021-07-27 15:47:04 +0200

    Added feature: automatic routing on employee level
    Added functionality to optionally select an employee when creating a routing expression.
    
    When an employee is selected this person will be assigned to the signal on creation

--- api/app/signals/apps/signals/models/routing_expression.py
+++ api/app/signals/apps/signals/models/routing_expression.py
@@ -1,8 +1,11 @@
 # SPDX-License-Identifier: MPL-2.0
 # Copyright (C) 2020 - 2021 Vereniging van Nederlandse Gemeenten, Gemeente Amsterdam
+from django.contrib.auth import get_user_model
 from django.contrib.gis.db import models
 
 from signals.apps.signals.models import Department, Expression
+
+User = get_user_model()
 
 
 class RoutingExpression(models.Model):
@@ -14,6 +17,7 @@ class RoutingExpression(models.Model):
         related_name='routing_department'
     )
     _department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    employee = models.ForeignKey(to=User, on_delete=models.SET_NULL, blank=True, null=True)
     order = models.PositiveIntegerField(default=0, db_index=True)
     is_active = models.BooleanField(default=False)
 
