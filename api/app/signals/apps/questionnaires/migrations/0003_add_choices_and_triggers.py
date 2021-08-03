1cebc40ff4a9db598f17bfba0708aae2cec37cc4
Thijs Coenen <458483+tcoenen@users.noreply.github.com>
2021-08-03 16:03:50 +0200

    SIG-3904 added choices (predefined answers) for questions and triggers to data model

---
+++ api/app/signals/apps/questionnaires/migrations/0003_add_choices_and_triggers.py
@@ -0,0 +1,85 @@
+# SPDX-License-Identifier: MPL-2.0
+# Copyright (C) 2021 Gemeente Amsterdam
+import django.db.models.deletion
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('questionnaires', '0002_auto_20210719_0131'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='question',
+            name='enforce_choices',
+            field=models.BooleanField(default=False),
+        ),
+        migrations.AlterField(
+            model_name='question',
+            name='field_type',
+            field=models.CharField(choices=[('integer', 'Integer'), ('plain_text', 'PlainText')], max_length=255),
+        ),
+        migrations.CreateModel(
+            name='InfoTrigger',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('payload', models.JSONField(blank=True, null=True)),
+                ('order', models.IntegerField(default=0)),
+                ('title', models.CharField(max_length=255)),
+                ('information', models.TextField()),
+                ('graph', models.ForeignKey(
+                    on_delete=django.db.models.deletion.CASCADE,
+                    related_name='info_triggers',
+                    to='questionnaires.questiongraph'
+                )),
+                ('question', models.ForeignKey(
+                    on_delete=django.db.models.deletion.CASCADE,
+                    related_name='+',
+                    to='questionnaires.question'
+                )),
+            ],
+            options={
+                'ordering': ['order', 'id'],
+            },
+        ),
+        migrations.CreateModel(
+            name='ActionTrigger',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('payload', models.JSONField(blank=True, null=True)),
+                ('order', models.IntegerField(default=0)),
+                ('action', models.CharField(choices=[('reopen signal', 'Melding heropenen')], max_length=255)),
+                ('arguments', models.JSONField(blank=True, null=True)),
+                ('graph', models.ForeignKey(
+                    on_delete=django.db.models.deletion.CASCADE,
+                    related_name='action_triggers',
+                    to='questionnaires.questiongraph'
+                )),
+                ('question', models.ForeignKey(
+                    on_delete=django.db.models.deletion.CASCADE,
+                    related_name='+',
+                    to='questionnaires.question'
+                )),
+            ],
+            options={
+                'ordering': ['order', 'id'],
+            },
+        ),
+        migrations.CreateModel(
+            name='Choice',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('payload', models.JSONField(blank=True, null=True)),
+                ('question', models.ForeignKey(
+                    on_delete=django.db.models.deletion.CASCADE,
+                    related_name='choices',
+                    to='questionnaires.question'
+                )),
+            ],
+            options={
+                'order_with_respect_to': 'question',
+            },
+        ),
+    ]
