dece4a6976bbd05d0d0d2b65294b89382aeededc
ginokok1996 <ginokok1996@hotmail.com>
2021-08-25 14:47:23 +0200

    update routing_expression modal

--- api/app/signals/apps/signals/models/routing_expression.py
+++ api/app/signals/apps/signals/models/routing_expression.py
@@ -1,17 +1,24 @@
 # SPDX-License-Identifier: MPL-2.0
 # Copyright (C) 2020 - 2021 Vereniging van Nederlandse Gemeenten, Gemeente Amsterdam
-from factory import Sequence, SubFactory
-from factory.django import DjangoModelFactory
+from django.contrib.auth import get_user_model
+from django.contrib.gis.db import models
 
-from signals.apps.signals.models import RoutingExpression
+from signals.apps.signals.models import Department, Expression
+
+User = get_user_model()
 
 
-class RoutingExpressionFactory(DjangoModelFactory):
-    class Meta:
-        model = RoutingExpression
+class RoutingExpression(models.Model):
+    # we only allow one department routing per expression
+    _expression = models.OneToOneField(
+        Expression,
+        on_delete=models.CASCADE,
+        unique=True,
+        related_name='routing_department'
+    )
+    _department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    _user = models.ForeignKey(to=User, on_delete=models.SET_NULL, blank=True, null=True)
+    order = models.PositiveIntegerField(default=0, db_index=True)
+    is_active = models.BooleanField(default=False)
 
-    _expression = SubFactory('signals.apps.signals.factories.expression.ExpressionFactory')
-    _department = SubFactory('signals.apps.signals.factories.expression.DepartmentFactory')
-    _user = SubFactory('signals.apps.users.factories.UserFactory')
-    order = Sequence(lambda n: n)
-    is_active = True
+    objects = models.Manager()
