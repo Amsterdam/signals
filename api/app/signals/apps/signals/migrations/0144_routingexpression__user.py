e15ff50e1d61d8acc0e7ae1ebd7c687c5155bdd1
ginokok1996 <ginokok1996@hotmail.com>
2021-08-25 14:52:57 +0200

    added migration

---
+++ api/app/signals/apps/signals/migrations/0144_routingexpression__user.py
@@ -0,0 +1,21 @@
+# SPDX-License-Identifier: MPL-2.0
+# Copyright (C) 2021 Gemeente Amsterdam
+from django.conf import settings
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('signals', '0143_storedsignalfilter_show_on_overview'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='routingexpression',
+            name='_user',
+            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, to=settings.AUTH_USER_MODEL),
+        ),
+    ]
