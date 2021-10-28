7bab1a1717b6a208ca54207f58e4d7dacbf6e67e
Boris Arkenaar <boris@arkenaar.com>
2021-10-28 15:17:43 +0200

    Revert incorrect change

--- api/app/signals/apps/api/app_settings.py
+++ api/app/signals/apps/api/app_settings.py
@@ -6,7 +6,7 @@ from signals.apps.signals import workflow
 
 SIGNALS_API_MAX_UPLOAD_SIZE = 8388608  # 8MB = 8*1024*1024
 SIGNALS_API_ATLAS_SEARCH_URL = settings.DATAPUNT_API_URL + 'atlas/search'
-SIGNALS_API_PDOK_API_URL = 'https://service.pdok.nl'
+SIGNALS_API_PDOK_API_URL = 'https://geodata.nationaalgeoregister.nl'
 
 SIGNALS_API_CLOSED_STATES = frozenset([
     workflow.AFGEHANDELD,
