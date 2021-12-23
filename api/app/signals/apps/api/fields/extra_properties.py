e333b21240df0e526a67ebf9a13f614fca1e9f6c
David van Buiten <david.vanbuiten@gmail.com>
2021-12-23 11:33:53 +0100

    WIP add extra_properties extracted from Sessions to the response of private signals

--- api/app/signals/apps/api/fields/extra_properties.py
+++ api/app/signals/apps/api/fields/extra_properties.py
@@ -3,6 +3,8 @@
 from django.conf import settings
 from rest_framework.fields import JSONField
 
+from signals.apps.questionnaires.services import SessionService
+
 
 class SignalExtraPropertiesField(JSONField):
     def __init__(self, *args, **kwargs):
@@ -11,12 +13,27 @@ class SignalExtraPropertiesField(JSONField):
 
     def get_attribute(self, instance):
         self.instance = instance
-        return instance.extra_properties
+        return self.to_representation(instance.extra_properties)
 
     def to_representation(self, value):
         representation = super().to_representation(value=value)
+        if representation is None:
+            representation = []
+
+        session_extra_properties = []
+        session_qs = self.instance.session_set.filter(questionnaire__category__isnull=False)
+        if session_qs.exists():
+            """
+            If there are connected sessions that have questionnaires belonging to categories we want to extract the
+            extra_properties and add them to the representation
+            """
+            for session in session_qs:
+                session_service = SessionService(session)
+                session_extra_properties += session_service.get_extra_properties()
+            representation = session_extra_properties
+
         if not settings.FEATURE_FLAGS.get('API_FILTER_EXTRA_PROPERTIES', False):
-            return representation
+            return representation + session_extra_properties
 
         category_url = self.instance.category_assignment.category.get_absolute_url()
         category_urls = [category_url, f'{category_url}/']
@@ -24,6 +41,6 @@ class SignalExtraPropertiesField(JSONField):
             parent_category_url = self.instance.category_assignment.category.parent.get_absolute_url()
             category_urls += [parent_category_url, f'{parent_category_url}/']
 
-        return filter(
+        return list(filter(
             lambda x: 'category_url' in x and x['category_url'] in category_urls, representation
-        )
+        )) + session_extra_properties
