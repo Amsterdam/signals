391fbb8002e9369ba99455fe7a92ef40a9a352ad
Nick Zaaijer <n.zaaijer@amsterdam.nl>
2022-04-26 13:31:32 +0200

    remove permissionset from the Filter endpint because any type of user is allowed to crud the filters.

--- api/app/signals/apps/api/views/stored_signal_filter.py
+++ api/app/signals/apps/api/views/stored_signal_filter.py
@@ -4,7 +4,6 @@ from datapunt_api.pagination import HALPagination
 from rest_framework import viewsets
 
 from signals.apps.api.generics import mixins
-from signals.apps.api.generics.permissions import SIAPermissions
 from signals.apps.api.serializers import StoredSignalFilterSerializer
 from signals.apps.signals.models import StoredSignalFilter
 from signals.auth.backend import JWTAuthBackend
@@ -14,7 +13,6 @@ class StoredSignalFilterViewSet(mixins.RetrieveModelMixin, mixins.ListModelMixin
                                 mixins.CreateModelMixin, mixins.UpdateModelMixin,
                                 mixins.DestroyModelMixin, viewsets.GenericViewSet):
     authentication_classes = (JWTAuthBackend,)
-    permission_classes = (SIAPermissions,)
 
     pagination_class = HALPagination
     serializer_class = StoredSignalFilterSerializer
