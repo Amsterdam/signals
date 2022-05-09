a6f14ab04c3aff671188e536d08138fff8e26b4d
Thijs Coenen <458483+tcoenen@users.noreply.github.com>
2022-05-09 10:14:58 +0200

    Add Location question field type to VNG style Question model

--- api/app/signals/apps/signals/models/question.py
+++ api/app/signals/apps/signals/models/question.py
@@ -12,6 +12,7 @@ class Question(models.Model):
     SELECT_INPUT = 'select_input'
     TEXT_AREA_INPUT = 'text_area_input'
     ASSET_SELECT = 'asset_select'
+    LOCATION = 'location'
 
     FIELD_TYPE_CHOICES = (
         (PLAIN_TEXT, 'PlainText'),
@@ -22,6 +23,7 @@ class Question(models.Model):
         (SELECT_INPUT, 'SelectInput'),
         (TEXT_AREA_INPUT, 'TextareaInput'),
         (ASSET_SELECT, 'AssetSelect'),
+        (LOCATION, 'Location')
     )
 
     key = models.CharField(max_length=255)
