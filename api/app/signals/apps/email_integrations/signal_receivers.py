270315b47546b9132a1fbe85b6339b913c52ff86
David van Buiten <david.vanbuiten@gmail.com>
2022-05-10 08:46:54 +0200

    Fixed the update_status_handler

--- api/app/signals/apps/email_integrations/signal_receivers.py
+++ api/app/signals/apps/email_integrations/signal_receivers.py
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: MPL-2.0
-# Copyright (C) 2019 - 2021 Gemeente Amsterdam
+# Copyright (C) 2019 - 2022 Gemeente Amsterdam
 from django.dispatch import receiver
 
 from signals.apps.email_integrations import tasks
@@ -7,5 +7,5 @@ from signals.apps.signals.managers import create_initial, update_status
 
 
 @receiver((create_initial, update_status), dispatch_uid='core_email_integrations_update_status')
-def update_status_handler(sender, signal_obj, status, prev_status, *args, **kwargs):
+def update_status_handler(sender, signal_obj, *args, **kwargs):
     tasks.send_mail_reporter.delay(pk=signal_obj.pk)
