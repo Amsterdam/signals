7eab2db90b6270466d77bb81ded0287a259dacca
Nick Zaaijer <n.zaaijer@amsterdam.nl>
2022-05-11 16:09:32 +0200

    remove duplicate key choice and add extra value text

--- api/app/signals/apps/email_integrations/models.py
+++ api/app/signals/apps/email_integrations/models.py
@@ -24,8 +24,7 @@ class EmailTemplate(CreatedUpdatedModel):
         (SIGNAL_STATUS_CHANGED_OPTIONAL, 'Send mail optional'),
         (SIGNAL_STATUS_CHANGED_REACTIE_GEVRAAGD, 'Send mail signal reaction requested'),
         (SIGNAL_STATUS_CHANGED_REACTIE_ONTVANGEN, 'Send mail signal reaction requested received'),
-        (SIGNAL_STATUS_CHANGED_REACTIE_ONTVANGEN, 'Send mail signal reaction requested received'),
-        (SIGNAL_STATUS_CHANGED_AFGEHANDELD_KTO_NEGATIVE_CONTACT, 'Send mail signal'),
+        (SIGNAL_STATUS_CHANGED_AFGEHANDELD_KTO_NEGATIVE_CONTACT, 'Send mail signal negative KTO contact'),
         (SIGNAL_FEEDBACK_RECEIVED, 'Send mail signal feedback received'),
     ]
 
