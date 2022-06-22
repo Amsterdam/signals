d491d858f98537153a8d454772c9d413e45cb72b
David van Buiten <david.vanbuiten@gmail.com>
2022-06-22 15:29:02 +0200

    Talked with Nick Zaaijer about the PR and we decied to only add the request to the HistoryLogService if a specific PUT, PATCH or POST request is made for a Category or a User. Will be refactored in SIG-4657 on the Amsterdam jira backlog.

--- api/app/signals/apps/history/middleware.py
+++ api/app/signals/apps/history/middleware.py
@@ -8,5 +8,13 @@ class HistoryMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        HistoryLogService.thread.request = request
+        paths = ['/signals/v1/private/categories/', '/signals/v1/private/users/', '/signals/admin/auth/user/',
+                 '/signals/admin/signals/category/', ]
+        if request.method in ['PUT', 'PATCH', 'POST'] and any(map(request.path.startswith, paths)):
+            # Only add the request to the thread when changing a Category OR a User instance
+            #
+            # Created ticket SIG-4657 on the Amsterdam JIRA backlog to find a better solution than using threading for
+            # this.
+            HistoryLogService.thread.request = request
+
         return self.get_response(request)
