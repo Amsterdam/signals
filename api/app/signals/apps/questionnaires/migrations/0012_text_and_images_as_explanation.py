3be8c1e80db92ff83a23e0737fe874e48e97f619
Thijs Coenen <458483+tcoenen@users.noreply.github.com>
2022-10-12 22:04:13 +0200

    Allow missing header and/or text for an AttachedSection

--- api/app/signals/apps/questionnaires/migrations/0012_text_and_images_as_explanation.py
+++ api/app/signals/apps/questionnaires/migrations/0012_text_and_images_as_explanation.py
@@ -32,8 +32,8 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('header', models.CharField(max_length=255)),
-                ('text', models.TextField()),
+                ('header', models.CharField(blank=True, max_length=255, null=True)),
+                ('text', models.TextField(blank=True, null=True)),
                 (
                     'illustrated_text',
                     models.ForeignKey(
