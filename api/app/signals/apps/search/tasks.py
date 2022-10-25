1d7411441c0bd510d81ce0ba3215b1810bcccdfc
David van Buiten <david.vanbuiten@gmail.com>
2022-10-25 09:08:54 +0200

    Added a celery task to the search app to index all Signals updated in a given date range. This functionality is copied from the elastic_index management command.

--- api/app/signals/apps/search/tasks.py
+++ api/app/signals/apps/search/tasks.py
@@ -2,6 +2,7 @@
 # Copyright (C) 2019 - 2021 Gemeente Amsterdam
 import logging
 
+from django.utils import timezone
 from elasticsearch import NotFoundError
 
 from signals.apps.search.documents.signal import SignalDocument
@@ -46,3 +47,40 @@ def delete_from_elastic(signal):
         signal_document.delete()
     except NotFoundError:
         log.warning(f'Signal {signal.id} not found in Elasticsearch')
+
+
+@app.task
+def index_signals_updated_in_date_range(from_date, to_date):
+    """
+    Index all Signals updated in the given date range (Copied from the elastic_index management command)
+    """
+    if not SignalDocument.ping():
+        raise Exception('Elastic cluster is unreachable')
+
+    if from_date:
+        from_date = timezone.make_aware(timezone.datetime.strptime(from_date, '%Y-%m-%d'))
+    else:
+        # Default is 2 days ago
+        from_date = timezone.now() - timezone.timedelta(days=2)
+
+    from_date = from_date.replace(hour=00, minute=00, second=00)  # Beginning of the day given in from_date
+
+    if to_date:
+        to_date = timezone.make_aware(timezone.datetime.strptime(to_date, '%Y-%m-%d'))
+    else:
+        # Default is today
+        to_date = timezone.now()
+
+    to_date = to_date + timezone.timedelta(days=1)
+    to_date = to_date.replace(hour=00, minute=00, second=00)  # Beginning of the day after the to_date
+
+    log.info(f'index_signals_updated_in_date_range - from {from_date}, to {to_date}')
+
+    if to_date < from_date:
+        log.warning('To date cannot be before the from date')
+        return
+
+    signal_qs = Signal.objects.filter(updated_at__range=[from_date, to_date]).order_by('-updated_at')
+    SignalDocument.index_documents(queryset=signal_qs)
+
+    log.info('index_signals_updated_in_date_range - done!')
