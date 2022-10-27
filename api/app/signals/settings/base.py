7b9646c1cb0bf2d92f51117e53936d9cf3d48bfc
Bart Jeukendrup <bart@jeukendrup.nl>
2022-10-27 23:42:05 +0200

    Set SECURE_SSL_REDIRECT to False

--- api/app/signals/settings/base.py
+++ api/app/signals/settings/base.py
@@ -140,7 +140,7 @@ CACHES = {
 }
 
 # Django security settings
-SECURE_SSL_REDIRECT = True
+SECURE_SSL_REDIRECT = False
 SECURE_REDIRECT_EXEMPT = [r'^status/', ]  # Allow health checks on localhost.
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SESSION_COOKIE_SECURE = True
