43744f4a9e392e477c7006429330de2ae2fd94e5
David van Buiten <david.vanbuiten@gmail.com>
2022-11-10 09:15:21 +0100

    Signal history for My Signals

--- api/app/signals/apps/my_signals/rest_framework/views/signals.py
+++ api/app/signals/apps/my_signals/rest_framework/views/signals.py
@@ -3,22 +3,36 @@
 from datapunt_api.pagination import HALPagination
 from datapunt_api.rest import DEFAULT_RENDERERS
 from dateutil.relativedelta import relativedelta
+from django.contrib.contenttypes.models import ContentType
+from django.db.models import Min, Q
 from django.utils import timezone
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.decorators import action
+from rest_framework.filters import OrderingFilter
 from rest_framework.response import Response
-from rest_framework.status import HTTP_501_NOT_IMPLEMENTED
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from rest_framework_extensions.mixins import DetailSerializerMixin
 
-from signals.apps.api.generics.filters import FieldMappingOrderingFilter
+from signals.apps.history.models import Log
 from signals.apps.my_signals.rest_framework.authentication import MySignalsTokenAuthentication
 from signals.apps.my_signals.rest_framework.filters.signals import MySignalFilterSet
 from signals.apps.my_signals.rest_framework.serializers.signals import (
+    HistoryLogHalSerializer,
     SignalDetailSerializer,
     SignalListSerializer
 )
-from signals.apps.signals.models import Signal
+from signals.apps.signals import workflow
+from signals.apps.signals.models import (
+    CategoryAssignment,
+    Location,
+    Note,
+    Priority,
+    Signal,
+    SignalDepartments,
+    SignalUser,
+    Status,
+    Type
+)
 
 
 class MySignalsViewSet(DetailSerializerMixin, ReadOnlyModelViewSet):
@@ -33,7 +47,7 @@ class MySignalsViewSet(DetailSerializerMixin, ReadOnlyModelViewSet):
     serializer_class = SignalListSerializer
     serializer_detail_class = SignalDetailSerializer
 
-    filter_backends = (DjangoFilterBackend, FieldMappingOrderingFilter,)
+    filter_backends = (DjangoFilterBackend, OrderingFilter,)
     filterset_class = MySignalFilterSet
 
     ordering = (
@@ -42,9 +56,6 @@ class MySignalsViewSet(DetailSerializerMixin, ReadOnlyModelViewSet):
     ordering_fields = (
         'created_at',
     )
-    ordering_field_mappings = {
-        'created_at': 'created_at',
-    }
 
     def get_queryset(self, *args, **kwargs):
         """
@@ -62,6 +73,67 @@ class MySignalsViewSet(DetailSerializerMixin, ReadOnlyModelViewSet):
     @action(detail=True, url_path=r'history/?$')
     def history(self, *args, **kwargs):
         """
-        Placeholder for the history view of a signal, this will be implemented when ticket SIG-4764 has been refined
-        """
-        return Response(status=HTTP_501_NOT_IMPLEMENTED)
+        The public history view of a signal  
+
+        This will return the history log of a Signal for the "My Signal" flow.  
+        Not all history will be returned to the reporter.  
+
+        The following rules apply:  
+        - Do not return logs about SignalDepartments (Department assignment)  
+        - Do not return logs about SignalUser (User assignment)  
+        - Do not return logs about Note  
+        - Do not return logs about Priority  
+        - Do not return logs about Status changes to LEEG, AFWACHTING, ON_HOLD, GEANNULEERD, GESPLITST, 
+          VERZOEK_TOT_AFHANDELING, INGEPLAND, VERZOEK_TOT_HEROPENEN, TE_VERZENDEN, VERZONDEN, VERZENDEN_MISLUKT, 
+          AFGEHANDELD_EXTERN  
+        - Status changes are translated to a more reporter friendly name  
+        -- GEANNULEERD, AFGEHANDELD -> Afgesloten    
+        -- HEROPEND -> Heropend  
+        -- REACTIE_GEVRAAGD -> Vraag aan u verstuurd  
+        -- REACTIE_ONTVANGEN -> Antwoord van u ontvangen    
+        -- All other statusses -> Open  
+        - Do not return the first occurrences of logs about CategoryAssignment, Location and Type    
+        """  # noqa
+        signal = self.get_object()
+
+        # Note and Priority ContentTypes are always excluded
+        excluded_content_types = ContentType.objects.get_for_models(Note, Priority, SignalDepartments, SignalUser)
+        excluded_q = Q(content_type__in=excluded_content_types.values())
+
+        # Some Status transactions are excluded
+        status_type = ContentType.objects.get_for_model(Status)
+        excluded_q |= Q(Q(content_type=status_type) & Q(extra__in=[workflow.LEEG, workflow.AFWACHTING, workflow.ON_HOLD,
+                                                                   workflow.GEANNULEERD, workflow.GESPLITST,
+                                                                   workflow.VERZOEK_TOT_AFHANDELING, workflow.INGEPLAND,
+                                                                   workflow.VERZOEK_TOT_HEROPENEN,
+                                                                   workflow.TE_VERZENDEN, workflow.VERZONDEN,
+                                                                   workflow.VERZENDEN_MISLUKT,
+                                                                   workflow.AFGEHANDELD_EXTERN, ]))
+
+        # The first occurrence in the history log of a Location, CategoryAssignment or Type transition are excluded
+        exclude_first_occurrence_content_types = ContentType.objects.get_for_models(Location, CategoryAssignment, Type)
+        excluded_q |= Q(id__in=signal.history_log.values(
+            'content_type_id'
+        ).filter(
+            content_type_id__in=exclude_first_occurrence_content_types.values()
+        ).annotate(
+            id=Min('id')
+        ).values_list(
+            'id',
+            flat=True
+        ).order_by())
+
+        history_log_qs = signal.history_log.exclude(Q(excluded_q))
+
+        what = self.request.query_params.get('what', None)
+        if what:
+            _action = Log.translate_what_to_action(what)
+            _content_type = Log.translate_what_to_content_type(what)
+            history_log_qs = history_log_qs.filter(action__iexact=_action, content_type__model__iexact=_content_type)
+
+        ordering = self.request.query_params.get('ordering', None)
+        if ordering and (ordering == 'created_at' or ordering == '-created_at'):
+            history_log_qs = history_log_qs.order_by(ordering)
+
+        serializer = HistoryLogHalSerializer(history_log_qs, many=True)
+        return Response(serializer.data)
--- api/app/signals/apps/my_signals/tests/rest_framework/views/test_my_signals_history.py
+++ api/app/signals/apps/my_signals/tests/rest_framework/views/test_my_signals_history.py
@@ -1,13 +1,31 @@
 # SPDX-License-Identifier: MPL-2.0
 # Copyright (C) 2022 Gemeente Amsterdam
+import copy
+from io import StringIO
+
+from django.conf import settings
+from django.core.management import call_command
 from django.test import override_settings
 from django.urls import include, path
-from rest_framework.status import HTTP_404_NOT_FOUND, HTTP_501_NOT_IMPLEMENTED
+from django.utils import timezone
+from freezegun import freeze_time
+from rest_framework.status import HTTP_200_OK, HTTP_401_UNAUTHORIZED, HTTP_404_NOT_FOUND
 from rest_framework.test import APITestCase
 
 from signals.apps.api.views import NamespaceView
 from signals.apps.my_signals.models import Token
-from signals.apps.signals.factories import SignalFactoryWithImage
+from signals.apps.signals import workflow
+from signals.apps.signals.factories import (
+    CategoryAssignmentFactory,
+    LocationFactory,
+    NoteFactory,
+    PriorityFactory,
+    ReporterFactory,
+    SignalFactoryWithImage,
+    StatusFactory,
+    TypeFactory
+)
+from signals.apps.signals.models import Signal
 
 urlpatterns = [
     path('v1/relations/', NamespaceView.as_view(), name='signal-namespace'),
@@ -28,13 +46,218 @@ class TestMySignalsHistoryEndpoint(APITestCase):
     endpoint = '/my/signals'
 
     def setUp(self):
-        self.signal = SignalFactoryWithImage.create(reporter__email='my-signals-test-reporter@example.com')
+        self.feature_flags = copy.deepcopy(settings.FEATURE_FLAGS)
+        self.feature_flags.update({'SIGNAL_HISTORY_LOG_ENABLED': True})
+
         token = Token.objects.create(reporter_email='my-signals-test-reporter@example.com')
         self.request_headers = {'HTTP_AUTHORIZATION': f'Token {token.key}'}
 
-    def test_my_signals_history_not_implemented(self):
-        response = self.client.get(f'{self.endpoint}/{self.signal.uuid}/history', **self.request_headers)
-        self.assertEqual(response.status_code, HTTP_501_NOT_IMPLEMENTED)
+    def _create_initial_signal(self):
+        now = timezone.now()
+        with freeze_time(now - timezone.timedelta(minutes=7)):
+            signal = Signal.objects.create(text='Test melding (My Signals)', incident_date_start=now)
+
+        with freeze_time(now - timezone.timedelta(minutes=6)):
+            status = StatusFactory.create(_signal=signal)
+
+        with freeze_time(now - timezone.timedelta(minutes=5)):
+            location = LocationFactory.create(_signal=signal)
+
+        with freeze_time(now - timezone.timedelta(minutes=4)):
+            category_assignment = CategoryAssignmentFactory.create(_signal=signal)
+
+        with freeze_time(now - timezone.timedelta(minutes=3)):
+            reporter = ReporterFactory.create(email='my-signals-test-reporter@example.com')
+
+        with freeze_time(now - timezone.timedelta(minutes=2)):
+            priority = PriorityFactory.create(_signal=signal)
+
+        with freeze_time(now - timezone.timedelta(minutes=1)):
+            _type = TypeFactory.create(_signal=signal)
+
+        signal.status = status
+        signal.location = location
+        signal.category_assignment = category_assignment
+        signal.reporter = reporter
+        signal.priority = priority
+        signal.type_assignment = _type
+
+        signal.save()
+
+        # A bit hacky but this makes sure the history is present
+        call_command('migrate_signals_history', stdout=StringIO())
+
+        return signal
+
+    def _create_signal_with_history(self):
+        now = timezone.now()
+        with freeze_time(now - timezone.timedelta(minutes=113)):
+            # Initial Signal is created 2 hours ago
+            signal = self._create_initial_signal()
+
+        # Simulate workflow
+        with freeze_time(now - timezone.timedelta(minutes=110)):
+            status = StatusFactory(state=workflow.BEHANDELING, text=None, _signal=signal)
+            signal.status = status
+            signal.save()
+
+        with freeze_time(now - timezone.timedelta(minutes=105)):
+            status = StatusFactory(state=workflow.REACTIE_GEVRAAGD, text='Weet u meer?', _signal=signal)
+            signal.status = status
+            signal.save()
+
+        with freeze_time(now - timezone.timedelta(minutes=60)):
+            status = StatusFactory(state=workflow.REACTIE_ONTVANGEN, text='Ik heb alles gemeld', _signal=signal)
+            signal.status = status
+            signal.save()
+
+        with freeze_time(now - timezone.timedelta(minutes=50)):
+            category_assignment = CategoryAssignmentFactory(_signal=signal)
+            priority = PriorityFactory(priority='high', _signal=signal)
+            NoteFactory(text='Intern contact gehad en category aangepast + prio naar hoog', _signal=signal)
+            status = StatusFactory(state=workflow.INGEPLAND, text=None, _signal=signal)
+            signal.category_assignment = category_assignment
+            signal.priority = priority
+            signal.status = status
+            signal.save()
+
+        with freeze_time(now - timezone.timedelta(minutes=45)):
+            priority = PriorityFactory(priority='normal', _signal=signal)
+            NoteFactory(text='Prio weer naar normaal', _signal=signal)
+            signal.priority = priority
+            signal.save()
+
+        with freeze_time(now - timezone.timedelta(minutes=30)):
+            status = StatusFactory(state=workflow.AFGEHANDELD, text=None, _signal=signal)
+            signal.status = status
+            signal.save()
+
+        with freeze_time(now - timezone.timedelta(minutes=20)):
+            status = StatusFactory(state=workflow.HEROPEND, text='Melding nog niet AFGEHANDELD', _signal=signal)
+            signal.status = status
+            signal.save()
+
+        with freeze_time(now - timezone.timedelta(minutes=15)):
+            status = StatusFactory(state=workflow.REACTIE_GEVRAAGD, text='Toch nog 1 vraag, ok?', _signal=signal)
+            signal.status = status
+            signal.save()
+
+        with freeze_time(now - timezone.timedelta(minutes=5)):
+            status = StatusFactory(state=workflow.AFGEHANDELD, text='Melder heeft gebeld, medling AFGEHANDELD',
+                                   _signal=signal)
+            signal.status = status
+            signal.save()
+
+        # A bit hacky but this makes sure the history is present
+        call_command('migrate_signals_history', stdout=StringIO())
+
+        return signal
+
+    def test_history_signal_created(self):
+        signal = self._create_initial_signal()
+
+        response = self.client.get(f'{self.endpoint}/{signal.uuid}/history', **self.request_headers)
+        self.assertEqual(response.status_code, HTTP_200_OK)
+
+        response_json = response.json()
+        self.assertEqual(len(response_json), 2)
+
+        self.assertEqual(response_json[0]['what'], 'UPDATE_SLA')
+        self.assertEqual(response_json[0]['action'], 'Servicebelofte:')
+        self.assertEqual(response_json[0]['description'], f'{signal.category_assignment.stored_handling_message}')
+        self.assertEqual(response_json[0]['_signal'], f'{signal.uuid}')
+
+        self.assertEqual(response_json[1]['what'], 'UPDATE_STATUS')
+        self.assertEqual(response_json[1]['action'], 'Status gewijzigd naar: Open')
+        self.assertEqual(response_json[1]['description'], f'{signal.status.text}')
+        self.assertEqual(response_json[1]['_signal'], f'{signal.uuid}')
+
+        # Reverse order
+        response = self.client.get(f'{self.endpoint}/{signal.uuid}/history', data={'ordering': 'created_at'},
+                                   **self.request_headers)
+        self.assertEqual(response.status_code, HTTP_200_OK)
+
+        response_json = response.json()
+        self.assertEqual(len(response_json), 2)
+
+        self.assertEqual(response_json[0]['what'], 'UPDATE_STATUS')
+        self.assertEqual(response_json[0]['action'], 'Status gewijzigd naar: Open')
+        self.assertEqual(response_json[0]['description'], f'{signal.status.text}')
+        self.assertEqual(response_json[0]['_signal'], f'{signal.uuid}')
+
+        self.assertEqual(response_json[1]['what'], 'UPDATE_SLA')
+        self.assertEqual(response_json[1]['action'], 'Servicebelofte:')
+        self.assertEqual(response_json[1]['description'], f'{signal.category_assignment.stored_handling_message}')
+        self.assertEqual(response_json[1]['_signal'], f'{signal.uuid}')
+
+        # Filter on UPDATE_STATUS
+        response = self.client.get(f'{self.endpoint}/{signal.uuid}/history', data={'what': 'UPDATE_STATUS'},
+                                   **self.request_headers)
+        self.assertEqual(response.status_code, HTTP_200_OK)
+
+        response_json = response.json()
+        self.assertEqual(len(response_json), 1)
+
+        self.assertEqual(response_json[0]['what'], 'UPDATE_STATUS')
+        self.assertEqual(response_json[0]['action'], 'Status gewijzigd naar: Open')
+        self.assertEqual(response_json[0]['description'], f'{signal.status.text}')
+        self.assertEqual(response_json[0]['_signal'], f'{signal.uuid}')
+
+        # Filter on UPDATE_SLA
+        response = self.client.get(f'{self.endpoint}/{signal.uuid}/history', data={'what': 'UPDATE_SLA'},
+                                   **self.request_headers)
+        self.assertEqual(response.status_code, HTTP_200_OK)
+
+        response_json = response.json()
+        self.assertEqual(len(response_json), 1)
+
+        self.assertEqual(response_json[0]['what'], 'UPDATE_SLA')
+        self.assertEqual(response_json[0]['action'], 'Servicebelofte:')
+        self.assertEqual(response_json[0]['description'], f'{signal.category_assignment.stored_handling_message}')
+        self.assertEqual(response_json[0]['_signal'], f'{signal.uuid}')
+
+    def assert_what_in_data(self, what, data, pos=None):
+        if pos:
+            self.assertEqual(data[pos]['what'], what)
+        else:
+            self.assertIn(what, [item['what'] for item in data])
+
+    def assert_action_in_data(self, action, data, pos=None):
+        if pos:
+            self.assertEqual(data[pos]['action'], action)
+        else:
+            self.assertIn(action, [item['action'] for item in data])
+
+    def assert_description_in_data(self, description, data, pos=None):
+        if pos:
+            self.assertEqual(data[pos]['description'], description)
+        else:
+            self.assertIn(description, [item['description'] for item in data])
+
+    def test_history_signal_flow(self):
+        signal = self._create_signal_with_history()
+
+        response = self.client.get(f'{self.endpoint}/{signal.uuid}/history', data={'ordering': 'created_at'},
+                                   **self.request_headers)
+        self.assertEqual(response.status_code, HTTP_200_OK)
+
+        response_json = response.json()
+        self.assertEqual(len(response_json), 9)
+
+        self.assert_what_in_data('UPDATE_STATUS', response_json, 0)
+        self.assert_what_in_data('UPDATE_SLA', response_json, 1)
+        self.assert_what_in_data('UPDATE_STATUS', response_json, 2)
+        self.assert_what_in_data('UPDATE_STATUS', response_json, 3)
+        self.assert_what_in_data('UPDATE_STATUS', response_json, 4)
+        self.assert_what_in_data('UPDATE_STATUS', response_json, 5)
+        self.assert_what_in_data('UPDATE_STATUS', response_json, 6)
+        self.assert_what_in_data('UPDATE_STATUS', response_json, 7)
+        self.assert_what_in_data('UPDATE_STATUS', response_json, 8)
+
+    def test_history_not_my_signal(self):
+        not_my_signal = SignalFactoryWithImage.create(reporter__email='not-me@example.com')
+        response = self.client.get(f'{self.endpoint}/{not_my_signal.uuid}/history', **self.request_headers)
+        self.assertEqual(response.status_code, HTTP_401_UNAUTHORIZED)
 
 
 class TestMySignalsHistoryEndpointDisabled(APITestCase):
