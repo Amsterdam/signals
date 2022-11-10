f5ad38fa648ebd76e3031ad1a111fc775303e96e
Thijs Coenen <458483+tcoenen@users.noreply.github.com>
2022-11-10 12:34:36 +0100

    Fixes after rebase

--- api/app/signals/apps/questionnaires/tests/rest_framework/views/public/test_public_questions_endpoint.py
+++ api/app/signals/apps/questionnaires/tests/rest_framework/views/public/test_public_questions_endpoint.py
@@ -56,19 +56,6 @@ class TestPublicQuestionEndpoint(ValidateJsonSchemaMixin, APITestCase):
             os.path.join(THIS_DIR, '../../json_schema/public_post_question_answer_response.json')
         )
 
-        # set up explanatory text + images
-        illustrated_text, section_1, section_2, attached_file_1, attached_file_2 = create_illustrated_text()
-        self.illustrated_text = illustrated_text
-        self.section_1 = section_1
-        self.section_2 = section_2
-        self.attached_file_1 = attached_file_1
-        self.attached_file_2 = attached_file_2
-        self.explanation = illustrated_text
-        self.explanation = IllustratedText.objects.create(title='Questionnaire title')
-
-        self.question.explanation = self.explanation
-        self.question.save()
-
     def test_question_list(self):
         response = self.client.get(f'{self.base_endpoint}')
         self.assertEqual(response.status_code, 404)
