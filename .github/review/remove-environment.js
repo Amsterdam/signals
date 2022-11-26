e59f3ac7f87a9a6dce522f8d34aad8f015147b4a
Bart Jeukendrup <bart@jeukendrup.nl>
2022-11-26 22:03:13 +0100

    Add ability to remove environment in GitHub

---
+++ .github/review/remove-environment.js
@@ -0,0 +1,20 @@
+const { Octokit } = require("@octokit/action");
+
+const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
+const environmentName = process.env.ENVIRONMENT_NAME;
+
+removeEnvironment(environmentName);
+
+async function removeEnvironment(environmentName) {
+    const octokit = new Octokit();
+
+    const { data } = await octokit.request(
+        "DELETE /repos/{owner}/{repo}/environments/{environmentName}", {
+            owner,
+            repo,
+            environmentName,
+        }
+    )
+
+    console.log("Removed environment", data);
+}
