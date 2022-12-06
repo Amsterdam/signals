9557e1db8c78410f99b9a7cc1afeb2311acaefd1
Bart Jeukendrup <bart@jeukendrup.nl>
2022-12-06 11:38:52 +0100

    Use _required_call_kwargs pass arguments using kwargs

--- api/app/signals/apps/email_integrations/actions/assigned_action.py
+++ api/app/signals/apps/email_integrations/actions/assigned_action.py
@@ -11,8 +11,7 @@ from django.template import Context, Template, loader
 from signals.apps.email_integrations.actions.abstract import AbstractSystemAction
 from signals.apps.email_integrations.exceptions import URLEncodedCharsFoundInText
 from signals.apps.email_integrations.models import EmailTemplate
-from signals.apps.email_integrations.utils import make_email_context
-from signals.apps.signals.models import Department, Signal
+from signals.apps.signals.models import Department
 
 logger = logging.getLogger(__name__)
 
@@ -25,6 +24,8 @@ class AssignedAction(AbstractSystemAction):
     user or the users' department
     """
 
+    _required_call_kwargs = ('recipient', 'assigned_to')
+
     key = EmailTemplate.SIGNAL_ASSIGNED
     subject = (
         "Melding {{ formatted_signal_id }} is toegewezen aan "
@@ -33,16 +34,21 @@ class AssignedAction(AbstractSystemAction):
 
     note = None
 
-    def __call__(self, signal, dry_run=False, recipient=None, assigned_to=None):
-        if self.rule(signal):
-            if dry_run:
-                return True
+    def get_additional_context(self, signal, dry_run=False):
+        recipient = self.kwargs['recipient']
+        assigned_to = self.kwargs['assigned_to']
 
-            if self.send_mail(signal, recipient=recipient, assigned_to=assigned_to):
-                self.add_note(signal)
-                return True
+        return {
+            'signal_url': f'{settings.FRONTEND_URL}/manage/incident/{signal.id}',
+            'recipient_full_name': recipient.get_full_name(),
+            'assigned_to_user': assigned_to if isinstance(assigned_to, User) else None,
+            'assigned_to_department': assigned_to if isinstance(assigned_to, Department) else None
+        }
 
-        return False
+    def get_recipient_list(self, signal):
+        return [
+            formataddr((self.kwargs['recipient'].get_full_name(), self.kwargs['recipient'].email))
+        ]
 
     def render_mail_data(self, context):
         """
@@ -68,46 +74,18 @@ class AssignedAction(AbstractSystemAction):
 
         return subject, message, html_message
 
-    def get_context(self, signal, dry_run=False, recipient=None, assigned_to=None):
-        """
-        Email context
-        """
-        context = make_email_context(
-            signal,
-            self.get_additional_context(signal, dry_run, recipient, assigned_to),
-            dry_run
-        )
-
-        return context
-
-    def get_additional_context(self, signal, dry_run=False, recipient=None, assigned_to=None):
-        return {
-            'signal_url': f'{settings.FRONTEND_URL}/manage/incident/{signal.id}',
-            'recipient_full_name': recipient.get_full_name(),
-            'assigned_to_user': assigned_to if isinstance(assigned_to, User) else None,
-            'assigned_to_department': assigned_to if isinstance(assigned_to, Department) else None
-        }
-
-    def send_mail(self, signal, dry_run=False, recipient=None, assigned_to=None):
+    def send_mail(self, signal, dry_run=False):
         """
         Send the email to the reporter
         """
         try:
-            context = self.get_context(signal, dry_run, recipient, assigned_to)
+            context = self.get_context(signal, dry_run)
         except URLEncodedCharsFoundInText:
-            # Log a warning and add a note  to the Signal that the email could not be sent
+            # Log a warning that the email could not be sent
             logger.warning(f'URL encoded text found in Signal {signal.id}')
-            Signal.actions.create_note(
-                {'text':
-                 'E-mail is niet verzonden omdat er verdachte tekens in de meldtekst staan.'},
-                signal=signal
-            )
             return 0  # No mail sent, return 0. Same behaviour as send_mail()
+
         subject, message, html_message = self.render_mail_data(context)
 
-        recipient_list = [
-            formataddr((recipient.get_full_name(), recipient.email))
-        ]
-
         return send_mail(subject=subject, message=message, from_email=self.from_email,
-                         recipient_list=recipient_list, html_message=html_message)
+                         recipient_list=self.get_recipient_list(signal), html_message=html_message)
