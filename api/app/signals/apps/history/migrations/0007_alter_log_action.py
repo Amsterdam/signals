d694eb6dd74fa38a6a5704b781d0eb907dc1957b
Thijs Coenen <458483+tcoenen@users.noreply.github.com>
2022-12-21 12:21:54 +0100

    ACTION_NOT_RECEIVED -> NOT_RECEIVED

---
+++ api/app/signals/apps/history/migrations/0007_alter_log_action.py
@@ -0,0 +1,30 @@
+# SPDX-License-Identifier: MPL-2.0
+# Copyright (C) 2022 Vereniging van Nederlandse Gemeenten
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('history', '0006_alter_log_action'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='log',
+            name='action',
+            field=models.CharField(
+                choices=[
+                    ('UNKNOWN', 'Unknown'),
+                    ('CREATE', 'Created'),
+                    ('UPDATE', 'Updated'),
+                    ('DELETE', 'Deleted'),
+                    ('RECEIVE', 'Received'),
+                    ('NOT_RECEIVED', 'Not received')
+                ],
+                default='UNKNOWN',
+                editable=False,
+                max_length=20
+            ),
+        ),
+    ]
