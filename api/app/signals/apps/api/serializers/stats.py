6a7de7fcf154ce076aa9dd5b7d5d925d8e80742a
Youri Westerman <y.westerman@amsterdam.nl>
2023-01-30 13:25:16 +0100

    Add serializer for the "high urgency completion" statistics

--- api/app/signals/apps/api/serializers/stats.py
+++ api/app/signals/apps/api/serializers/stats.py
@@ -1,9 +1,19 @@
-from rest_framework.serializers import IntegerField
+from rest_framework.fields import DateField, IntegerField
 from rest_framework.serializers import Serializer
 
 
 class TotalSerializer(Serializer):
     total = IntegerField()
+
+    def update(self, instance, validated_data):
+        pass
+
+    def create(self, validated_data):
+        pass
+
+class HighUrgencyCompletionSerializer(Serializer):
+    date = DateField()
+    amount = IntegerField()
 
     def update(self, instance, validated_data):
         pass
