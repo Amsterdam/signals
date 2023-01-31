2b455c95f2ded4cee3680c0e14a6ea460f19be6f
David van Buiten <david.vanbuiten@gmail.com>
2023-01-31 08:19:57 +0100

    Bumped API version to 2.13.0

--- api/app/signals/__init__.py
+++ api/app/signals/__init__.py
@@ -28,7 +28,7 @@ datastructures.EmptyResultSet = EmptyResultSet
 
 
 # Application version (Major, minor, patch)
-VERSION = (2, 13, '0-beta.01')
+VERSION = (2, 13, '0')
 
 API_VERSIONS = {
     'v0': VERSION,
