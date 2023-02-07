500bfec0c3c6d33bac53975b007e026a7a12c262
Youri Westerman <y.westerman@amsterdam.nl>
2023-02-07 12:52:36 +0100

    Add additional context for re-openend email

--- api/app/signals/apps/email_integrations/actions/signal_reopened.py
+++ api/app/signals/apps/email_integrations/actions/signal_reopened.py
@@ -16,3 +16,27 @@ class SignalReopenedAction(AbstractAction):
     subject = 'Meer over uw melding {formatted_signal_id}'
 
     note = 'Automatische e-mail bij heropenen is verzonden aan de melder.'
+
+    def get_additional_context(self, signal, dry_run=False):
+        feedback_qs = signal.feedback.filter(submitted_at__isnull=False)
+        feedback_received = feedback_qs.exists()
+
+        if feedback_received:
+            last_received_feedback = feedback_qs.order_by('submitted_at').last()
+            feedback_is_satisfied = last_received_feedback.is_satisfied
+            feedback_text = last_received_feedback.text
+            feedback_text_extra = last_received_feedback.text_extra
+            feedback_text_list = last_received_feedback.text_list
+        else:
+            feedback_is_satisfied = None
+            feedback_text = None
+            feedback_text_extra = None
+            feedback_text_list = None
+
+        return {
+            'feedback_received': feedback_received,
+            'feedback_is_satisfied': feedback_is_satisfied,
+            'feedback_text': feedback_text,
+            'feedback_text_extra': feedback_text_extra,
+            'feedback_text_list': feedback_text_list,
+        }
