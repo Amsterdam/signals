2c6b093a1efc4070a89e66dbbf810115bd998dcc
Youri Westerman <y.westerman@amsterdam.nl>
2023-02-21 14:10:16 +0100

    Add test cases for MimeTypeFromFilenameResolver

---
+++ app/signals/apps/services/tests/domain/test_mimetypes.py
@@ -0,0 +1,35 @@
+# SPDX-License-Identifier: MPL-2.0
+# Copyright (C) 2023 Gemeente Amsterdam
+import pytest
+
+from signals.apps.services.domain.mimetypes import MimeTypeFromFilenameResolver
+
+
+class TestMimeTypeFromFilenameResolver:
+    @pytest.mark.parametrize(',filename,expected', [
+        ('test.svg', 'image/svg+xml'),
+        ('TEST.SVG', 'image/svg+xml'),
+        ('TEst.SvG', 'image/svg+xml'),
+        ('teST.sVg', 'image/svg+xml'),
+        ('test.jpeg', 'image/jpeg'),
+        ('TEST.JPEG', 'image/jpeg'),
+        ('TEst.jpEG', 'image/jpeg'),
+        ('teST.JPeg', 'image/jpeg'),
+        ('test.jpg', 'image/jpeg'),
+        ('test.JPG', 'image/jpeg'),
+        ('test.jPg', 'image/jpeg'),
+        ('test.jpG', 'image/jpeg'),
+        ('test.png', 'image/png'),
+        ('TEST.PNG', 'image/png'),
+        ('test.pnG', 'image/png'),
+        ('test.gif', 'image/gif'),
+        ('test.GIF', 'image/gif'),
+        ('test.Gif', 'image/gif'),
+        ('test.pdf', 'application/pdf'),
+        ('TEST.PDF', 'application/pdf'),
+        ('TEST.pDF', 'application/pdf'),
+        ('test.Pdf', 'application/pdf'),
+    ])
+    def test_resolving(self, filename, expected):
+        resolver = MimeTypeFromFilenameResolver(filename)
+        assert expected == resolver()
