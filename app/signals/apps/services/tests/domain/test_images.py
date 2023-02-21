31f42ea892d1f30ab2e8cab3e2a2208ff318aa49
Youri Westerman <y.westerman@amsterdam.nl>
2023-02-21 15:53:41 +0100

    Add IsPdfChecker

--- app/signals/apps/services/tests/domain/test_images.py
+++ app/signals/apps/services/tests/domain/test_images.py
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: MPL-2.0
-# Copyright (C) 2021 Gemeente Amsterdam
+# Copyright (C) 2023 Gemeente Amsterdam
 import os
 from io import BytesIO
 from unittest.mock import MagicMock
