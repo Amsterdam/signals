36f787f849ee9720364a83165626c58375d76d6d
David van Buiten <david.vanbuiten@gmail.com>
2023-02-22 11:28:51 +0100

    Bumped API version to 2.15.5

--- app/signals/__init__.py
+++ app/signals/__init__.py
@@ -15,7 +15,7 @@ __all__ = ['celery_app', 'VERSION', 'API_VERSIONS', ]
 # they are now all the same.
 
 # Application version (Major, minor, patch)
-VERSION = (2, 15, 4)
+VERSION = (2, 15, 5)
 
 API_VERSIONS = {
     'v1': VERSION,
