f01f2bf75253313f87ae276ee7bc9342c9fe7e75
Youri Westerman <y.westerman@amsterdam.nl>
2023-02-22 13:29:31 +0100

    Remove UploadScannerService

--- app/signals/apps/api/tests/test_filescanner.py
+++ app/signals/apps/api/tests/test_filescanner.py
@@ -1,24 +1,15 @@
 # SPDX-License-Identifier: MPL-2.0
-# Copyright (C) 2021 Gemeente Amsterdam
+# Copyright (C) 2023 Gemeente Amsterdam
 """
 Test uploads, specifically test the filetype sniffing. Note we are not testing
 the permissions system of SIA/Signalen, hence the usage of superusers to write
 to private endpoints
 """
 import os
-from itertools import chain
 from unittest.mock import patch
 
 from django.core.files.uploadedfile import SimpleUploadedFile
-from django.test import TestCase
-from PIL.Image import DecompressionBombError
 
-from signals.apps.services.domain.filescanner import (
-    FileRejectedError,
-    FileTypeExtensionMismatch,
-    FileTypeNotSupported,
-    UploadScannerService
-)
 from signals.apps.signals.factories import SignalFactory
 from signals.test.utils import SignalsBaseApiTestCase
 
@@ -144,98 +135,3 @@ class TestUploadedFileScannerViaAPI(SignalsBaseApiTestCase):
             self.assertIn('file', response_json)
             self.assertEqual(1, len(response_json['file']))
             self.assertIn('decompression bomb DOS attack', response_json['file'][0])
-
-
-class TestUploadScannerService(TestCase):
-    def test__get_mime_from_extension(self):
-        for filename, content_type in chain(ALLOWED, DISALLOWED):
-            with open(filename, 'rb') as disallowed_file:
-                suf = SimpleUploadedFile(filename, disallowed_file.read(), content_type=content_type)
-                mime_type = UploadScannerService._get_mime_from_extension(suf)
-
-                self.assertEqual(content_type, mime_type)
-
-    def test__get_mime_from_extension_with_no_extension(self):
-        with open(GIF_FILE, 'rb') as file:
-            suf = SimpleUploadedFile('no-extension', file.read(), content_type='image/gif')
-            mime_type = UploadScannerService._get_mime_from_extension(suf)
-            self.assertEqual(mime_type, None)
-
-    def test__get_mime_from_content(self):
-        for filename, content_type in chain(ALLOWED, DISALLOWED):
-            with open(filename, 'rb') as file:
-                suf = SimpleUploadedFile(filename, file.read(), content_type=content_type)
-                mime_type = UploadScannerService._get_mime_from_content(suf)
-
-                self.assertEqual(mime_type, content_type)
-
-    def test__get_mime_from_content_zero_length(self):
-        suf = SimpleUploadedFile('empty.jpg', b'', content_type='image/jpg')
-        mime_type = UploadScannerService._get_mime_from_content(suf)
-
-        self.assertEqual(mime_type, 'application/x-empty')
-
-    def test_scan_file_allowed(self):
-        for filename, content_type in ALLOWED:
-            with open(filename, 'rb') as file:
-                suf = SimpleUploadedFile(filename, file.read(), content_type=content_type)
-                UploadScannerService.scan_file(suf)
-
-    def test_scan_file_disallowed(self):
-        for filename, content_type in DISALLOWED:
-            with open(filename, 'rb') as file:
-                suf = SimpleUploadedFile(filename, file.read(), content_type=content_type)
-                with self.assertRaises(FileRejectedError):
-                    UploadScannerService.scan_file(suf)
-
-    def test_scan_file_disallowed_content_does_not_match_extension_i(self):
-        with open(DOC_FILE, 'rb') as disallowed_file:
-            suf = SimpleUploadedFile('not-actual.jpg', disallowed_file.read(), content_type='image/jpg')
-            with self.assertRaises(FileTypeExtensionMismatch):
-                UploadScannerService.scan_file(suf)
-
-    def test_scan_file_disallowed_content_does_not_match_extension_ii(self):
-        with open(GIF_FILE, 'rb') as allowed_file:
-            suf = SimpleUploadedFile('not-actual.jpg', allowed_file.read(), content_type='image/jpg')
-            with self.assertRaises(FileTypeExtensionMismatch):
-                UploadScannerService.scan_file(suf)
-
-    def test_scan_file_disallowed_raises_filetype_not_supported(self):
-        with open(DOC_FILE, 'rb') as disallowed_file:
-            suf = SimpleUploadedFile(DOC_FILE, disallowed_file.read(), content_type='application/msword')
-            with self.assertRaises(FileTypeNotSupported):
-                UploadScannerService.scan_file(suf)
-
-    def test_scan_file_disallowed_no_extension(self):
-        # Note: a file without extension will get a mimetype of None in the
-        # _get_mime_from_extension method. The _get_mime_from_content will
-        # itself derive a non-None extension and the error will be content does
-        # not match extension. (Just to document the behavior.)
-        with open(GIF_FILE, 'rb') as allowed_file:
-            suf = SimpleUploadedFile('not-extension', allowed_file.read(), content_type='image/gif')
-            with self.assertRaises(FileTypeExtensionMismatch):
-                UploadScannerService.scan_file(suf)
-
-    def test_scan_file_disallowed_empty(self):
-        # Note: empty files are labeled: 'application/x-empty' by python-magic
-        # hence this case looks like a file with incorrect content given the
-        # extension. (Just to document the behavior.)
-        suf = SimpleUploadedFile('image.jpg', b'', content_type='image/jpg')
-        with self.assertRaises(FileTypeExtensionMismatch):
-            UploadScannerService.scan_file(suf)
-
-    @patch('PIL.Image.MAX_IMAGE_PIXELS', 1)
-    def test_pil_DecompressionBombError(self):
-        """
-        When an image exceeds a the MAX_IMAGE_PIXELS PIL will protect against potential DOS attacks caused by
-        “decompression bombs”. When the MAX_IMAGE_PIXELS been exceeded PIL will log a DecompressionBombWarning, if the
-        image pixels exceed twice the MAX_IMAGE_PIXELS PIL will raise a DecompressionBombError.
-
-        More information can be found on:
-            https://pillow.readthedocs.io/en/stable/reference/Image.html?highlight=MAX_IMAGE_PIXELS#PIL.Image.open
-        """
-        for filename, content_type in ALLOWED:
-            with open(filename, 'rb') as file:
-                suf = SimpleUploadedFile(filename, file.read(), content_type=content_type)
-                with self.assertRaises(DecompressionBombError):
-                    UploadScannerService.scan_file(suf)
