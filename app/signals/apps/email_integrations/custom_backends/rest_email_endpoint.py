87ee56b7e880fc1b2623eda0d32f685afdcd0248
Bart Jeukendrup <bart@jeukendrup.nl>
2023-03-01 20:01:23 +0100

    Only forward e-mail part of mailbox format in from, to, cc, bcc and reply_to to REST API

--- app/signals/apps/email_integrations/custom_backends/rest_email_endpoint.py
+++ app/signals/apps/email_integrations/custom_backends/rest_email_endpoint.py
@@ -1,12 +1,14 @@
 # SPDX-License-Identifier: MPL-2.0
 # Copyright (C) 2020 - 2021 Vereniging van Nederlandse Gemeenten, Gemeente Amsterdam
+import base64
 import json
 import threading
+from email.mime.base import MIMEBase
+from email.utils import parseaddr
 
 from django.conf import settings
+from django.core.mail import EmailMessage
 from django.core.mail.backends.base import BaseEmailBackend
-
-from djcelery_email.utils import email_to_dict
 from requests import RequestException, Session
 
 
@@ -67,8 +69,48 @@ class RestEmailBackend(BaseEmailBackend):
                 return 0
             num_sent = 0
             for message in email_messages:
-                if self._send_email_rest_api(email_to_dict(message)):
+                if self._send_email_rest_api(self._email_to_dict(message)):
                     num_sent += 1
             if session_created:
                 self.close()
         return num_sent
+
+    def _email_to_dict(self, message):
+        if isinstance(message, dict):
+            return message
+
+        message_dict = {
+            'subject': message.subject,
+            'body': message.body,
+            'from_email': parseaddr(message.from_email)[1],
+            'to': [parseaddr(addr)[1] for addr in message.to],
+            'bcc': [parseaddr(addr)[1] for addr in message.bcc],
+            # ignore connection
+            'attachments': [],
+            'headers': message.extra_headers,
+            'cc': [parseaddr(addr)[1] for addr in message.cc],
+            'reply_to': [parseaddr(addr)[1] for addr in message.reply_to]
+        }
+
+        if hasattr(message, 'alternatives'):
+            message_dict['alternatives'] = message.alternatives
+        if message.content_subtype != EmailMessage.content_subtype:
+            message_dict["content_subtype"] = message.content_subtype
+        if message.mixed_subtype != EmailMessage.mixed_subtype:
+            message_dict["mixed_subtype"] = message.mixed_subtype
+
+        attachments = message.attachments
+        for attachment in attachments:
+            if isinstance(attachment, MIMEBase):
+                filename = attachment.get_filename('')
+                binary_contents = attachment.get_payload(decode=True)
+                mimetype = attachment.get_content_type()
+            else:
+                filename, binary_contents, mimetype = attachment
+                # For a mimetype starting with text/, content is expected to be a string.
+                if isinstance(binary_contents, str):
+                    binary_contents = binary_contents.encode()
+            contents = base64.b64encode(binary_contents).decode('ascii')
+            message_dict['attachments'].append((filename, contents, mimetype))
+
+        return message_dict
--- app/signals/apps/email_integrations/tests/rest_email_backend/test_rest_email_backend.py
+++ app/signals/apps/email_integrations/tests/rest_email_backend/test_rest_email_backend.py
@@ -1,10 +1,12 @@
 # SPDX-License-Identifier: MPL-2.0
 # Copyright (C) 2020 - 2021 Vereniging van Nederlandse Gemeenten, Gemeente Amsterdam
+import json
 from unittest import mock
 
 from django.core.mail import EmailMessage
 from django.core.mail import send_mail as django_send_mail
 from django.test import TestCase
+from requests import Session
 
 from signals.apps.email_integrations.custom_backends.rest_email_endpoint import RestEmailBackend
 
@@ -15,8 +17,8 @@ class TestRestEmailBackend(TestCase):
             'Hello',
             'Body goes here',
             'from@example.com',
-            ['to1@example.com', 'to2@example.com'],
-            ['bcc@example.com'],
+            ['to1@example.com', 'Example <to2@example.com>'],
+            ['bcc1@example.com', 'Example <bcc2@example.com>'],
             reply_to=['another@example.com'],
             headers={'Message-ID': 'foo'},
         )
@@ -29,6 +31,33 @@ class TestRestEmailBackend(TestCase):
                 messages_sent += backend.send_messages([self.email])
         self.assertEqual(messages_sent, 1)
         mocked_send.assert_called_once()
+
+    @mock.patch.object(Session, 'post')
+    def test_rest_email_backend_post_request(self, mocked_session_post):
+        with self.settings(CELERY_EMAIL_MESSAGE_EXTRA_ATTRIBUTES=None):
+            with RestEmailBackend() as backend:
+                backend.send_messages([self.email])
+
+        expected_data = {
+            'subject': 'Hello',
+            'body': 'Body goes here',
+            'from_email': 'from@example.com',
+            'to': ['to1@example.com', 'to2@example.com'],
+            'bcc': ['bcc1@example.com', 'bcc2@example.com'],
+            'attachments': [],
+            'headers': {
+                'Message-ID': 'foo'
+            },
+            'cc': [],
+            'reply_to': ['another@example.com']
+        }
+
+        mocked_session_post.assert_called_once_with(
+            url=None,
+            headers={'Content-type': 'application/json', 'Accept': 'text/plain'},
+            data=json.dumps(expected_data),
+            timeout=5
+        )
 
     @mock.patch('signals.apps.email_integrations.custom_backends.rest_email_endpoint.RestEmailBackend._send_email_rest_api') # noqa
     def test_rest_email_backend_send_multiple_message(self, mocked_send):
