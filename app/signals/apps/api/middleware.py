f01160d4c50bd8a6d02def8a447e1e06f00480bc
Youri Westerman <y.westerman@amsterdam.nl>
2023-03-20 15:11:25 +0100

    Update copyright year

--- app/signals/apps/api/middleware.py
+++ app/signals/apps/api/middleware.py
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: MPL-2.0
-# Copyright (C) 2018 - 2021 Gemeente Amsterdam
+# Copyright (C) 2018 - 2023 Gemeente Amsterdam
 from typing import Callable
 
 from django.conf import settings
