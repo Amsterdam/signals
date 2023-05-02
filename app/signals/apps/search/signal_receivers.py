660950de3a4f3035d70618d3e5f5c44f1526f3de
Youri Westerman <y.westerman@amsterdam.nl>
2023-05-02 17:15:26 +0200

    Delete elasticsearch document when the status message is deleted from the database

--- app/signals/apps/search/signal_receivers.py
+++ app/signals/apps/search/signal_receivers.py
@@ -1,8 +1,9 @@
 # SPDX-License-Identifier: MPL-2.0
 # Copyright (C) 2023 Gemeente Amsterdam
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
+from signals.apps.search.documents.status_message import StatusMessage as StatusMessageDocument
 from signals.apps.search.tasks import index_status_message, save_to_elastic
 from signals.apps.signals.managers import (
     create_child,
@@ -40,3 +41,20 @@ def status_message_post_save_receiver(sender: str, instance: StatusMessageModel,
         The instance of the StatusMessage model that was saved to the database.
     """
     index_status_message.delay(status_message_id=instance.id)
+
+
+@receiver(post_delete, sender=StatusMessageModel)
+def status_message_post_delete_receiver(sender: str, instance: StatusMessageModel, **kwargs):
+    """Django signal receiver used to delete status message documents from the
+    elasticsearch index after they have been deleted from the database.
+
+    Parameters
+    ----------
+    sender : str
+        The classname of the model. This should always be the classname of the
+        StatusMessage model, which is defined in the @receiver decorator above.
+    instance : StatusMessageModel
+        The instance of the StatusMessage model that was saved to the database.
+    """
+    document = StatusMessageDocument.get(instance.id)
+    document.delete()
