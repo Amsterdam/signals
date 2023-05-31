285dda3e46522becc278218160e407edeeb17167
Youri Westerman <y.westerman@amsterdam.nl>
2023-05-31 15:33:16 +0200

    Change `state` field type to keyword so it can be used for filtering

--- app/signals/apps/search/documents/status_message.py
+++ app/signals/apps/search/documents/status_message.py
@@ -1,13 +1,13 @@
 # SPDX-License-Identifier: MPL-2.0
 # Copyright (C) 2023 Gemeente Amsterdam
-from elasticsearch_dsl import Boolean, Document, Integer, Text
+from elasticsearch_dsl import Boolean, Document, Integer, Keyword, Text
 
 
 class StatusMessage(Document):
     id = Integer()
     title = Text(analyzer='dutch')
     text = Text(analyzer='dutch')
-    state = Text()
+    state = Keyword()
     active = Boolean()
 
     class Index:
