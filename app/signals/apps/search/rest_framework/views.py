c94e74b61edc503e59e809464e2a5c07d2ae6c61
Youri Westerman <y.westerman@amsterdam.nl>
2023-06-07 13:34:41 +0200

    Extract nested class

--- app/signals/apps/search/rest_framework/views.py
+++ app/signals/apps/search/rest_framework/views.py
@@ -58,6 +58,33 @@ class SearchView(DatapuntViewSet):
         return super().list(request=request, *args, **kwargs)
 
 
+class StatusMessagesSearch(FacetedSearch):
+    """The Elasticsearch DSL library requires us to subclass FacetedSearch in order to
+    configure a faceted search, which allows us to use filters and provides us with
+    counts for each possible filter option.
+    """
+    index = 'status_messages'
+    doc_types = (StatusMessage,)
+    fields = ('title', 'text',)
+    facets = {
+        'state': TermsFacet(field='state', size=20, min_doc_count=0),
+        'active': TermsFacet(field='active', min_doc_count=0),
+    }
+
+    def query(self, search: Search, query: str):
+        """Overridden query method in order to set the fuzziness of the query and
+        to provide the zero_terms_query option in order to get (all) results when
+        no query term is provided.
+        """
+        return search.query(
+            'multi_match',
+            query=query,
+            fields=self.fields,
+            fuzziness='AUTO',
+            zero_terms_query='all',
+        )
+
+
 class StatusMessageSearchView(APIView):
     """View providing support for searching for status messages using elasticsearch."""
     authentication_classes = (JWTAuthBackend,)
@@ -77,32 +104,6 @@ class StatusMessageSearchView(APIView):
         In the results there is a 'facets' section available, which provides counts for every
         filter option.
         """
-        class StatusMessagesSearch(FacetedSearch):
-            """The Elasticsearch DSL library requires us to subclass FacetedSearch in order to
-            configure a faceted search, which allows us to use filters and provides us with
-            counts for each possible filter option.
-            """
-            index = 'status_messages'
-            doc_types = (StatusMessage,)
-            fields = ('title', 'text',)
-            facets = {
-                'state': TermsFacet(field='state', size=20, min_doc_count=0),
-                'active': TermsFacet(field='active', min_doc_count=0),
-            }
-
-            def query(self, search: Search, query: str):
-                """Overridden query method in order to set the fuzziness of the query and
-                to provide the zero_terms_query option in order to get (all) results when
-                no query term is provided.
-                """
-                return search.query(
-                    'multi_match',
-                    query=query,
-                    fields=self.fields,
-                    fuzziness='AUTO',
-                    zero_terms_query='all',
-                )
-
         q = ''
         if 'q' in request.query_params:
             q = request.query_params['q']
