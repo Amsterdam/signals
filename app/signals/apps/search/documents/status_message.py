5764e752b016a152cc58ce987006fd9297ba777f
Youri Westerman <y.westerman@amsterdam.nl>
2023-06-08 16:23:33 +0200

    Add suggest field to status message document and index autocomplete suggestions

--- app/signals/apps/search/documents/status_message.py
+++ app/signals/apps/search/documents/status_message.py
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: MPL-2.0
 # Copyright (C) 2023 Gemeente Amsterdam
-from elasticsearch_dsl import Boolean, Document, Integer, Keyword, Text
+from elasticsearch_dsl import Boolean, Completion, Document, Integer, Keyword, Text
 
 
 class StatusMessage(Document):
@@ -9,6 +9,10 @@ class StatusMessage(Document):
     text = Text(analyzer='dutch')
     state = Keyword()
     active = Boolean()
+    suggest = Completion(analyzer='dutch')
+
+    def clean(self):
+        self.suggest = {'input': self.text}
 
     class Index:
         name = 'status_messages'
