4fd2cd2eaebad3a2b2cfdbf76b94c975809275d1
Youri Westerman <y.westerman@amsterdam.nl>
2023-06-28 16:40:23 +0200

    Use import alias to prevent redefinition issues in flake8

--- app/signals/apps/email_integrations/templatetags/markdown_filters.py
+++ app/signals/apps/email_integrations/templatetags/markdown_filters.py
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: MPL-2.0
 # Copyright (C) 2020 - 2021 Vereniging van Nederlandse Gemeenten, Gemeente Amsterdam
-import markdown
+import markdown as md
 from django import template
 from django.utils.safestring import mark_safe
 
@@ -10,10 +10,10 @@ register = template.Library()
 
 
 @register.filter
-def markdown(value):
-    return mark_safe(markdown.markdown(value, extensions=('legacy_em', )))
+def markdown(value: str) -> str:
+    return mark_safe(md.markdown(value, extensions=('legacy_em', )))
 
 
 @register.filter(is_safe=True)
-def plaintext(value):
-    return strip_markdown_html(markdown.markdown(value, extensions=('legacy_em', )))
+def plaintext(value: str) -> str:
+    return strip_markdown_html(md.markdown(value, extensions=('legacy_em', )))
