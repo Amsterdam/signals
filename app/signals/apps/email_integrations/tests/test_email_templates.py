a9276117cfdfb52a4091cc9cb44b8ed732b26b35
Youri Westerman <y.westerman@amsterdam.nl>
2023-07-04 16:01:07 +0200

    Add signal repopened test case

--- app/signals/apps/email_integrations/tests/test_email_templates.py
+++ app/signals/apps/email_integrations/tests/test_email_templates.py
@@ -639,3 +639,69 @@ Voor vragen over uw melding kunt u bellen met telefoonnummer 14 020, maandag tot
 </body>
 </html>
 """ # noqa
+
+    def test_signal_reopened(self):
+        with freeze_time('2023-07-04 13:37'):
+            signal = SignalFactory.create(
+                reporter__email='test@example.com',
+                reporter__phone='0123456789',
+                status__state=workflow.HEROPEND,
+            )
+
+        MailService.status_mail(signal=signal)
+
+        assert mail.outbox[0].body == f"""Geachte melder,
+
+U bent niet tevreden over wat wij met uw melding hebben gedaan. Dat spijt ons. Wij gaan opnieuw aan het werk.
+
+Wat wij nu gaan doen
+{signal.status.text}
+
+U liet ons het volgende weten
+Bent u tevreden met de afhandeling van uw melding?
+Nee, Ik ben niet tevreden met de afhandeling van mijn melding
+
+Waarom bent u niet tevreden?
+
+Gegevens van uw melding
+Nummer: SIG-{signal.id}
+Gemeld op: 4 juli 2023, 15.37 uur
+Plaats: Sesamstraat 666, 1011 AA Ergens
+
+Meer weten?
+Voor vragen over uw melding kunt u bellen met telefoonnummer 14 020, maandag tot en met vrijdag van 08.00 tot 18.00. Geef dan ook het nummer van uw melding door: SIG-{signal.id}.
+
+Met vriendelijke groet,
+
+Gemeente Amsterdam""" # noqa
+
+        body, mime_type = mail.outbox[0].alternatives[0]
+        self.assertEqual(mime_type, 'text/html')
+
+        assert body == f"""
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <title>Uw melding {signal.id}</title>
+</head>
+<body>
+    <p>Geachte melder,</p>
+<p>U bent niet tevreden over wat wij met uw melding hebben gedaan. Dat spijt ons. Wij gaan opnieuw aan het werk.</p>
+<p><strong>Wat wij nu gaan doen</strong><br />
+{signal.status.text}</p>
+<p><strong>U liet ons het volgende weten</strong><br />
+Bent u tevreden met de afhandeling van uw melding?<br />
+<em>Nee, Ik ben niet tevreden met de afhandeling van mijn melding</em></p>
+<p><strong>Waarom bent u niet tevreden?</strong></p>
+<p><strong>Gegevens van uw melding</strong><br />
+Nummer: SIG-{signal.id}<br />
+Gemeld op: 4 juli 2023, 15.37 uur<br />
+Plaats: Sesamstraat 666, 1011 AA Ergens</p>
+<p><strong>Meer weten?</strong><br />
+Voor vragen over uw melding kunt u bellen met telefoonnummer 14 020, maandag tot en met vrijdag van 08.00 tot 18.00. Geef dan ook het nummer van uw melding door: SIG-{signal.id}.</p>
+<p>Met vriendelijke groet,</p>
+<p>Gemeente Amsterdam</p>
+</body>
+</html>
+""" # noqa
