0794bec1e17db4631f9825d11fa45a3220c174ff
Youri Westerman <y.westerman@amsterdam.nl>
2023-07-11 14:24:08 +0200

    Use token generator to generate secure token for "my signals" access

--- app/signals/apps/my_signals/models.py
+++ app/signals/apps/my_signals/models.py
@@ -1,30 +1,30 @@
 # SPDX-License-Identifier: MPL-2.0
-# Copyright (C) 2022 Gemeente Amsterdam
-import binascii
-import os
+# Copyright (C) 2022 - 2023 Gemeente Amsterdam
 from datetime import timedelta
 
 from django.db import models
 from django.utils.timezone import now
 
 from signals.apps.my_signals.app_settings import MY_SIGNALS_TOKEN_EXPIRES_SECOND
+from signals.apps.signals.tokens.token_generator import TokenGenerator
 
 
 class Token(models.Model):
     """
     Based on the AuthToken implementation of rest-framework
     """
-    key = models.CharField(max_length=40, primary_key=True)
+    key = models.CharField(max_length=120, primary_key=True)
     reporter_email = models.EmailField()
     expires_at = models.DateTimeField(editable=False)
     created_at = models.DateTimeField(editable=False, auto_now_add=True)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.key
 
     @classmethod
-    def generate_key(cls):
-        return binascii.hexlify(os.urandom(20)).decode()
+    def generate_key(cls) -> str:
+        generate = TokenGenerator()
+        return generate()
 
     def save(self, *args, **kwargs):
         if not self.key:
