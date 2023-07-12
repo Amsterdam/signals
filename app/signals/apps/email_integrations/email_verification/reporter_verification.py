5b81afde1106427162d9a8b1e5a2a7026d7565d5
Youri Westerman <y.westerman@amsterdam.nl>
2023-07-12 17:15:56 +0200

    Send verification email (#1281)
    * Add key for verification email template
    
    * Implement sending of verification email

---
+++ app/signals/apps/email_integrations/email_verification/reporter_verification.py
@@ -0,0 +1,62 @@
+# SPDX-License-Identifier: MPL-2.0
+# Copyright (C) 2023 Gemeente Amsterdam
+from django.core.mail import send_mail
+
+from signals import settings
+from signals.apps.email_integrations.models import EmailTemplate
+from signals.apps.email_integrations.renderers.email_template_renderer import EmailTemplateRenderer
+from signals.apps.signals.models import Reporter
+from signals.apps.signals.tokens.token_generator import TokenGenerator
+
+
+class FailedToSendVerificationMailException(Exception):
+    """This exception is raised when there is a failure with sending the verification email."""
+
+
+class ReporterVerifier:
+    """This class attempts to send a verification email to a reporter.
+    The verification email contains a link that can be clicked by the reporter to verify their
+    email address."""
+    from_email: str
+    _render_email_template: EmailTemplateRenderer
+    _generate_token: TokenGenerator
+
+    def __init__(
+            self,
+            email_template_renderer: EmailTemplateRenderer,
+            token_generator: TokenGenerator,
+            from_email: str = settings.DEFAULT_FROM_EMAIL,
+    ):
+        self._render_email_template = email_template_renderer
+        self._generate_token = token_generator
+        self.from_email = from_email
+
+    def __call__(self, reporter: Reporter) -> None:
+        """Sends the actual verification email or raises `FailedToSendVerificationMailException`
+        if it is not possible to send the email.
+
+        Parameters
+        ----------
+        reporter: Reporter
+            The reporter to send the verification email to. Obviously the reporter needs to have
+            an email address.
+        """
+        if not reporter.email:
+            raise FailedToSendVerificationMailException('Reporter has no email address!')
+
+        context = {
+            'verification_url': f'{settings.FRONTEND_URL}/verify_email/{self._generate_token()}',
+            'ORGANIZATION_NAME': settings.ORGANIZATION_NAME
+        }
+        subject, message, html_message = self._render_email_template(
+            EmailTemplate.VERIFY_EMAIL_REPORTER,
+            context
+        )
+
+        send_mail(
+            from_email=self.from_email,
+            recipient_list=[reporter.email],
+            subject=subject,
+            message=message,
+            html_message=html_message,
+        )
---
+++ app/signals/apps/email_integrations/tests/email_verification/test_reporter_verfication.py
@@ -0,0 +1,52 @@
+# SPDX-License-Identifier: MPL-2.0
+# Copyright (C) 2023 Gemeente Amsterdam
+import typing
+from unittest.mock import MagicMock, Mock, patch
+
+import pytest
+
+from signals.apps.email_integrations.email_verification.reporter_verification import (
+    FailedToSendVerificationMailException,
+    ReporterVerifier
+)
+from signals.apps.signals.models import Reporter
+
+
+class TestReporterVerifier:
+    FROM_EMAIL: typing.Final[str] = 'noreply@example.com'
+    EMAIL: typing.Final[str] = 'someone@example.com'
+    SUBJECT: typing.Final[str] = 'subject'
+    MESSAGE: typing.Final[str] = 'message'
+    HTML_MESSAGE: typing.Final[str] = 'html_message'
+
+    @patch('signals.apps.email_integrations.email_verification.reporter_verification.send_mail')
+    def test_send_verification_mail(self, send_mail: MagicMock):
+        send_mail.return_value = 1
+        renderer = Mock(return_value=(self.SUBJECT, self.MESSAGE, self.HTML_MESSAGE))
+        token_generator = Mock()
+        reporter_verifier = ReporterVerifier(renderer, token_generator, self.FROM_EMAIL)
+
+        reporter = Reporter()
+        reporter.email = self.EMAIL
+
+        reporter_verifier(reporter)
+
+        token_generator.assert_called_once()
+        renderer.assert_called_once()
+        send_mail.assert_called_once_with(
+            from_email=self.FROM_EMAIL,
+            recipient_list=[self.EMAIL],
+            subject=self.SUBJECT,
+            message=self.MESSAGE,
+            html_message=self.HTML_MESSAGE,
+        )
+
+    def test_raises_exception_when_reporter_has_no_email(self):
+        renderer = Mock(return_value=(self.SUBJECT, self.MESSAGE, self.HTML_MESSAGE))
+        token_generator = Mock()
+        reporter_verifier = ReporterVerifier(renderer, token_generator)
+
+        with pytest.raises(FailedToSendVerificationMailException) as e_info:
+            reporter_verifier(Reporter())
+
+        assert str(e_info.value) == 'Reporter has no email address!'
