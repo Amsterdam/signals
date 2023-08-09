7a08c9862bda48ff889ad1043a887bf1cb835e45
Youri Westerman <y.westerman@amsterdam.nl>
2023-08-09 14:39:46 +0200

    Allow cancelling the reporter

--- app/signals/apps/api/views/signals/private/signal_reporters.py
+++ app/signals/apps/api/views/signals/private/signal_reporters.py
@@ -1,7 +1,12 @@
 # SPDX-License-Identifier: MPL-2.0
 # Copyright (C) 2023 Gemeente Amsterdam
+from django.db import transaction
+from django_fsm import TransitionNotAllowed
+from rest_framework import status
 from rest_framework.exceptions import NotFound
 from rest_framework.mixins import CreateModelMixin, ListModelMixin
+from rest_framework.request import Request
+from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 from rest_framework_extensions.mixins import NestedViewSetMixin
 
@@ -32,3 +37,26 @@ class PrivateSignalReporterViewSet(CreateModelMixin, ListModelMixin, NestedViewS
             raise NotFound()
 
         return reporter._signal
+
+    def destroy(self, request: Request, *args, **kwargs):
+        """
+        Cancel a reporter, this allows cancelling a reporter update.
+        Cancelling is allowed, when the state of the reporter is "new" or "verification_email_sent"
+        and the reporter is not the original/first reporter of the signal.
+        """
+        instance = self.get_object()
+
+        try:
+            self.perform_destroy(instance)
+        except TransitionNotAllowed:
+            return Response(
+                data={'non_field_errors': ['Cancelling this reporter is not possible.']},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    @transaction.atomic()
+    def perform_destroy(self, instance: Reporter) -> None:
+        instance.cancel()
+        instance.save()
