567a3add0c37001bd543907e30e786996ea91fa0
Youri Westerman <y.westerman@amsterdam.nl>
2023-08-14 13:45:23 +0200

    Satisfy isort

--- app/signals/apps/api/tests/test_private_signal_reporters.py
+++ app/signals/apps/api/tests/test_private_signal_reporters.py
@@ -9,7 +9,7 @@ from rest_framework.status import (
     HTTP_200_OK,
     HTTP_201_CREATED,
     HTTP_400_BAD_REQUEST,
-    HTTP_401_UNAUTHORIZED,
+    HTTP_401_UNAUTHORIZED
 )
 from rest_framework.test import APITestCase
 
