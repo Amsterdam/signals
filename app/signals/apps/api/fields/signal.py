d7e89520a3fcb68be6bcb9a7e54e177c07da66bd
Youri Westerman <y.westerman@amsterdam.nl>
2023-10-23 13:25:50 +0200

    Assert that we're getting the objects that we're expecting

--- app/signals/apps/api/fields/signal.py
+++ app/signals/apps/api/fields/signal.py
@@ -115,6 +115,7 @@ class PrivateSignalLinksFieldWithArchives(serializers.HyperlinkedIdentityField):
         ])
 
         if value.is_child:
+            assert value.parent is not None
             result.update({
                 'sia:parent':
                 dict(href=self.get_url(value.parent, "private-signals-detail", request, None))
@@ -148,6 +149,7 @@ class PrivateSignalLinksField(serializers.HyperlinkedIdentityField):
 
     def to_representation(self, value: Signal) -> OrderedDict:
         request = self.context.get('request')
+        assert isinstance(request, Request)
 
         result = OrderedDict([
             ('self', dict(href=self.get_url(value, "private-signals-detail", request, None))),
@@ -205,6 +207,7 @@ class PrivateSignalWithContextLinksField(serializers.HyperlinkedIdentityField):
 
     def to_representation(self, value: Signal) -> OrderedDict:
         request = self.context.get('request')
+        assert isinstance(request, Request)
 
         result = OrderedDict([
             ('curies', dict(name='sia', href=self.reverse('signal-namespace', request=request))),
@@ -223,6 +226,7 @@ class PublicSignalLinksField(serializers.HyperlinkedIdentityField):
 
     def to_representation(self, value: Signal) -> OrderedDict:
         request = self.context.get('request')
+        assert isinstance(request, Request)
 
         result = OrderedDict([
             ('self', dict(href=self.get_url(value, "public-signals-detail", request, None))),
