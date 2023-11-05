b0c470d51b706a3780f43ee32818c13033456e81
Bart Jeukendrup <bart@jeukendrup.nl>
2023-11-05 20:21:53 +0100

    fix: always use FileSystemStorage for DWH

--- app/signals/apps/reporting/utils.py
+++ app/signals/apps/reporting/utils.py
@@ -8,7 +8,7 @@ from django.core.files.storage import FileSystemStorage
 from storages.backends.azure_storage import AzureStorage
 
 
-def _get_storage_backend(using: str) -> Union[AzureStorage, FileSystemStorage]:
+def _get_storage_backend(using: str) -> FileSystemStorage:
     """
     Returns one of the following storages:
         - AzureStorage, the "using" must be present in the AZURE_CONTAINERS setting.
@@ -17,12 +17,4 @@ def _get_storage_backend(using: str) -> Union[AzureStorage, FileSystemStorage]:
     :param using:
     :returns: AzureStorage or FileSystemStorage
     """
-    if settings.AZURE_STORAGE_ENABLED:
-        if not hasattr(settings, 'AZURE_CONTAINERS'):
-            raise ImproperlyConfigured('AZURE_CONTAINERS settings must be set!')
-        if using not in settings.AZURE_CONTAINERS.keys():
-            raise ImproperlyConfigured(f'{using} not present in the AZURE_CONTAINERS settings')
-
-        return AzureStorage(**settings.AZURE_CONTAINERS.get(using, {}))
-    else:
-        return FileSystemStorage(location=settings.DWH_MEDIA_ROOT)
+    return FileSystemStorage(location=settings.DWH_MEDIA_ROOT)
