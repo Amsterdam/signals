7c15860beeecc45b841469eb7911b597b112ee9c
Youri Westerman <y.westerman@amsterdam.nl>
2023-11-22 14:43:57 +0100

    Have StoredSignalFilterLinksField extend LinksField instead of HyperLinkedIdentityField

--- app/signals/apps/api/fields/stored_signal_filter.py
+++ app/signals/apps/api/fields/stored_signal_filter.py
@@ -1,23 +1,26 @@
 # SPDX-License-Identifier: MPL-2.0
-# Copyright (C) 2019 - 2021 Gemeente Amsterdam
+# Copyright (C) 2019 - 2023 Gemeente Amsterdam
 from collections import OrderedDict
 
-from rest_framework import serializers
+from datapunt_api.serializers import LinksField
+from rest_framework.reverse import reverse
+
+from signals.apps.signals.models import StoredSignalFilter
 
 
-class StoredSignalFilterLinksField(serializers.HyperlinkedIdentityField):
-    def to_representation(self, stored_signal_filter):
+class StoredSignalFilterLinksField(LinksField):
+    def to_representation(self, stored_signal_filter: StoredSignalFilter) -> OrderedDict:
         request = self.context.get('request')
 
-        filter_url = self.reverse('private-signals-list', request=request)
+        filter_url = reverse('private-signals-list', request=request)
         url_safe_options = stored_signal_filter.url_safe_options()
         if url_safe_options:
             filter_url = '{}?{}'.format(filter_url, url_safe_options)
 
         result = OrderedDict([
-            ('curies', dict(name='sia', href=self.reverse('signal-namespace', request=request))),
-            ('self', dict(href=self.get_url(stored_signal_filter, 'stored-signal-filters-detail', request, None))),  # noqa
-            ('sia:filter', dict(href=filter_url)),
+            ('curies', {'name': 'sia', 'href': reverse('signal-namespace', request=request)}),
+            ('self', {'href': self.get_url(stored_signal_filter, 'stored-signal-filters-detail', request, None)}),
+            ('sia:filter', {'href': filter_url}),
         ])
 
         return result
