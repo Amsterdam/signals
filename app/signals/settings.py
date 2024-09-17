d603cd8eb08ec0214de326ccec5a73500274b905
ramonavic <r.hofmeister@amsterdam.nl>
2024-09-17 11:03:57 +0200

    fixes isort

--- app/signals/settings.py
+++ app/signals/settings.py
@@ -3,12 +3,6 @@
 import os
 from typing import Any, Callable
 
-# Export modules to Azure Application Insights
-from azure.monitor.opentelemetry.exporter import (
-    AzureMonitorLogExporter,
-    AzureMonitorTraceExporter,
-)
-
 # Opentelemetry modules needed for logging and tracing
 from opentelemetry import trace
 from opentelemetry.instrumentation.django import DjangoInstrumentor
@@ -18,6 +12,9 @@ from opentelemetry.sdk._logs.export import BatchLogRecordProcessor
 from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+# Export modules to Azure Application Insights
+from azure.monitor.opentelemetry.exporter import AzureMonitorLogExporter, AzureMonitorTraceExporter
 
 from signals import __version__
 
