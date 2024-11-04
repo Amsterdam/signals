6886a608ee63837c78da5ab327c4fc918f388743
Bart Jeukendrup <bart@delta10.nl>
2024-11-04 11:40:05 +0100

    fix: linting errors

---
+++ app/signals/apps/media/__init__.py
@@ -0,0 +1,2 @@
+# SPDX-License-Identifier: MPL-2.0
+# Copyright (C) 2024 Delta10 B.V.
--- app/signals/apps/media/apps.py
+++ app/signals/apps/media/apps.py
@@ -1,3 +1,5 @@
+# SPDX-License-Identifier: MPL-2.0
+# Copyright (C) 2024 Delta10 B.V.
 from django.apps import AppConfig
 
 
