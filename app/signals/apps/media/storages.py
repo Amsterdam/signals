f5ce0316f8a6661a65dafef01991ab1bf9e8de41
Bart Jeukendrup <bart@delta10.nl>
2024-11-04 10:48:48 +0100

    feat: add ability to protect media folder

--- app/signals/apps/media/storages.py
+++ app/signals/apps/media/storages.py
@@ -12,11 +12,11 @@ signer = signing.TimestampSigner()
 class ProtectedFileSystemStorage(FileSystemStorage):
     def url(self, name):
         if self.base_url is None:
-            raise ValueError("This file is not accessible via a URL.")
+            raise ValueError('This file is not accessible via a URL.')
 
         url = filepath_to_uri(name)
         if url is not None:
-            url = url.lstrip("/")
+            url = url.lstrip('/')
 
         signature = signer.sign(url).split(':')
 
