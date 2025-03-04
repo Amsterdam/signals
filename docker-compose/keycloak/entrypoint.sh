1b8f6f6648860a89a628fdce55c7075b50efc7fb
Youri Westerman <y.westerman@amsterdam.nl>
2025-03-04 16:27:55 +0100

    Make sure keycloak imports the realm at startup

--- docker-compose/keycloak/entrypoint.sh
+++ docker-compose/keycloak/entrypoint.sh
@@ -2,8 +2,8 @@
 
 set -eux
 
-#if [ "$1" = "/opt/keycloak/bin/kc.sh" ]; then
-#	/opt/keycloak/bin/kc.sh import --file /import/realm-export.json
-#fi
+if [ "$1" = "/opt/keycloak/bin/kc.sh" ]; then
+	/opt/keycloak/bin/kc.sh import --file /import/realm-export.json
+fi
 
 exec "$@"
