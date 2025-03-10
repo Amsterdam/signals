1d677bc3fe639e65756357607dceedda1cde641d
Youri Westerman <y.westerman@amsterdam.nl>
2025-03-10 14:45:31 +0100

    Replace tests

---
+++ app/signals/auth/tests/test_backend.py
@@ -0,0 +1,31 @@
+# SPDX-License-Identifier: MPL-2.0
+# Copyright (C) 2025 Gemeente Amsterdam
+from unittest.mock import Mock
+
+import pytest
+from django.contrib.auth.backends import BaseBackend
+from django.test import TestCase
+from rest_framework.exceptions import AuthenticationFailed
+from rest_framework.request import Request
+
+from signals.apps.users.factories import SuperUserFactory
+from signals.auth.backend import JWTAuthBackend
+
+
+class TestJWTAuthBackend(TestCase):
+    def setUp(self):
+        self.backend = JWTAuthBackend(backend=Mock(BaseBackend))
+
+    def test_authenticate_header(self) -> None:
+        assert self.backend.authenticate_header(Mock(Request)) == 'Bearer realm="signals"'
+
+    def test_authenticate_always_ok_user_not_found(self) -> None:
+        with pytest.raises(AuthenticationFailed):
+            self.backend.authenticate(Mock(Request))
+
+    def test_authenticate_always_ok(self) -> None:
+        super_user = SuperUserFactory.create(email='signals.admin@example.com')
+
+        user, _ = self.backend.authenticate(Mock(Request))
+
+        assert user == super_user
