cd806dabc84ddd8e867f68acc84d35b97c2bd453
Bart Jeukendrup <bart@delta10.nl>
2025-03-14 19:27:04 +0100

    feat: add bulk category and status change

--- app/signals/apps/signals/admin/signal.py
+++ app/signals/apps/signals/admin/signal.py
@@ -1,27 +1,47 @@
 # SPDX-License-Identifier: MPL-2.0
 # Copyright (C) 2022 - 2023 Gemeente Amsterdam
+from django import forms
 from django.contrib import admin
+from django.core.exceptions import ValidationError
 from django.db import transaction
 
 from signals.apps.signals import workflow
-from signals.apps.signals.models import Status
+from signals.apps.signals.models import Category, Status, Signal
+
+
+class CreateReportForm(admin.helpers.ActionForm):
+    new_category = forms.ModelChoiceField(
+        queryset=Category.objects.filter(parent__isnull=False),
+        label='Nieuwe categorie',
+        empty_label='Selecteer een categorie',
+        required=False
+    )
 
 
 class SignalAdmin(admin.ModelAdmin):
     """
     signals.Signal model admin, allows some maintenance tasks.
     """
+    action_form = CreateReportForm
     fields = ('id', 'created_at', 'updated_at', 'get_status_display', 'get_category')
     list_display = ('id', 'created_at', 'updated_at', 'get_status_display', 'get_category')
     list_display_links = None  # change page not relevant
     ordering = ('-id',)
     list_per_page = 20
     list_select_related = True
+
+    list_filter = ['created_at', 'status__state', 'category_assignment__category__name']
     search_fields = ['id__exact']  # we do not want to page through 400k or more signals
 
     # Add an action that frees signals stuck between SIA and CityControl. These
     # signals need to be in workflow.VERZONDEN state.
-    actions = ['free_signals']
+    actions = [
+        'free_signals',
+        'change_category',
+        'change_status_reopened',
+        'change_status_cancelled',
+        'change_status_handled'
+    ]
 
     @admin.action(description='Free SIA signals (meldingen) stuck in state VERZONDEN.')
     def free_signals(self, request, queryset):
@@ -49,6 +69,56 @@ class SignalAdmin(admin.ModelAdmin):
                 msg = 'No IDs were freed.'
 
             transaction.on_commit(lambda: self.message_user(request, msg))
+
+    @admin.action(description='Zet categorie op')
+    def change_category(self, request, queryset):
+        if not request.POST['new_category']:
+            return self.message_user(request, 'Kies een nieuwe categorie.')
+
+        new_category = Category.objects.get(id=request.POST['new_category'])
+
+        for signal in queryset:
+            Signal.actions.update_category_assignment({
+                'category': new_category,
+            }, signal)
+
+        self.message_user(request, f'Categorieën zijn aangepast naar {new_category.name}.')
+
+    @admin.action(description='Zet status op heropend')
+    def change_status_handled(self, request, queryset):
+        for signal in queryset:
+            Signal.actions.update_status(
+                data={'text': 'De melding is heropend.', 'state': workflow.HEROPEND},
+                signal=signal
+            )
+
+        self.message_user(request, 'Statussen zijn aangepast naar heropend.')
+
+    @admin.action(description='Zet status op afgehandeld')
+    def change_status_handled(self, request, queryset):
+        for signal in queryset:
+            try:
+                Signal.actions.update_status(
+                    data={'text': 'De melding is afgehandeld.', 'state': workflow.AFGEHANDELD},
+                    signal=signal
+                )
+            except ValidationError as e:
+                return self.message_user(request, f'Fout bij het afhandelen van melding {signal.id}: {e}')
+
+        self.message_user(request, 'Statussen zijn aangepast naar afgehandeld.')
+
+    @admin.action(description='Zet status op geannuleerd')
+    def change_status_cancelled(self, request, queryset):
+        for signal in queryset:
+            try:
+                Signal.actions.update_status(
+                    data={'text': 'In bulk geannuleerd.', 'state': workflow.GEANNULEERD},
+                    signal=signal
+                )
+            except ValidationError as e:
+                return self.message_user(request, f'Fout bij het annuleren van melding {signal.id}: {e}')
+
+        self.message_user(request, 'Statussen zijn aangepast naar geannuleerd.')
 
     # Get the human-readable status and category:
     @admin.display(description='status')
