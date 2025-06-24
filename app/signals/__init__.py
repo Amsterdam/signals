ebe64564dc88c1209392c8d296ff3b9491ec26ae
Youri Westerman <y.westerman@amsterdam.nl>
2025-06-24 13:41:20 +0200

    Bump version

--- app/signals/__init__.py
+++ app/signals/__init__.py
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: MPL-2.0
-# Copyright (C) 2018 - 2023 Gemeente Amsterdam
+# Copyright (C) 2018 - 2025 Gemeente Amsterdam
 from signals.celery import app as celery_app
 
-VERSION = (2, 43, '5')
+VERSION = (2, 43, '6')
 __version__ = '.'.join(str(part) for part in VERSION)
 __all__ = ['celery_app', 'VERSION', '__version__', ]
