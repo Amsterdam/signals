1145af01bed8259da93b3b2f3ede52eeb0ddbd3c
ramonavic <r.hofmeister@amsterdam.nl>
2026-01-23 09:43:19 +0100

    bump version

--- app/signals/__init__.py
+++ app/signals/__init__.py
@@ -2,7 +2,7 @@
 # Copyright (C) 2018 - 2025 Gemeente Amsterdam
 from signals.celery import app as celery_app
 
-VERSION = (2, 50, "3")
+VERSION = (2, 50, "4")
 __version__ = ".".join(str(part) for part in VERSION)
 __all__ = [
     "celery_app",
