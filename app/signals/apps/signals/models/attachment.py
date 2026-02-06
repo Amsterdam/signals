cd9c1d29cc27d67eaf471dcf492d6cf2ff9ab654
ramonavic <r.hofmeister@amsterdam.nl>
2026-02-06 10:27:39 +0100

    removes pdf from accepted mime types

--- app/signals/apps/signals/models/attachment.py
+++ app/signals/apps/signals/models/attachment.py
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: MPL-2.0
-# Copyright (C) 2019 - 2021 Gemeente Amsterdam
+# Copyright (C) 2019 - 2026 Gemeente Amsterdam
 import logging
 
 from django.conf import settings
