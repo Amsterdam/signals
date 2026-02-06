089ce0d391da011f5e55434b42b313435a2a6fb7
ramonavic <r.hofmeister@amsterdam.nl>
2026-02-06 10:44:13 +0100

    update copyright

--- app/signals/apps/api/tests/test_attachment_validation.py
+++ app/signals/apps/api/tests/test_attachment_validation.py
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: MPL-2.0
-# Copyright (C) 2023 Gemeente Amsterdam
+# Copyright (C) 2026 Gemeente Amsterdam
 """
 Test uploads, specifically test the filetype sniffing. Note we are not testing
 the permissions system of SIA/Signalen, hence the usage of superusers to write
