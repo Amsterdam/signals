b7feccb1097868f82d3cfce9cf0f71d65ee7e5aa
Joey <joey.hendriks@amsterdam.nl>
2026-04-20 15:16:05 +0200

    fix test suite

--- app/signals/apps/classification/tests/test_training_set.py
+++ app/signals/apps/classification/tests/test_training_set.py
@@ -1,3 +1,5 @@
+import tempfile
+
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase
 
@@ -6,18 +8,21 @@ from signals.apps.classification.models import TrainingSet
 
 class TrainingSetTestCase(TestCase):
     def setUp(self):
-        self.test_file = SimpleUploadedFile('test_data_set.xlsx', '', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        self.tmp_dir = tempfile.mkdtemp()
+        self.test_file = SimpleUploadedFile('test_data_set.xlsx', b'', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
 
     def test_create_training_set(self):
-        training_set = TrainingSet.objects.create(
-            name='test_training_set',
-            file=self.test_file,
-        )
+        with self.settings(DWH_MEDIA_ROOT=self.tmp_dir):
+            training_set = TrainingSet.objects.create(
+                name='test_training_set',
+                file=self.test_file,
+            )
 
-        self.assertEqual(TrainingSet.objects.count(), 1)
-        self.assertEqual(training_set.name, 'test_training_set')
-        self.assertIsNotNone(training_set.file)
+            self.assertEqual(TrainingSet.objects.count(), 1)
+            self.assertEqual(training_set.name, 'test_training_set')
+            self.assertIsNotNone(training_set.file)
 
     def tearDown(self):
-        for training_set in TrainingSet.objects.all():
-            training_set.file.delete()
+        with self.settings(DWH_MEDIA_ROOT=self.tmp_dir):
+            for training_set in TrainingSet.objects.all():
+                training_set.file.delete()
